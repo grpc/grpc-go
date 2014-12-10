42989a83fff7f3e3212febb83a15feb37c6b13ac
ejona <ejona@google.com>
2014-12-09 18:03:24 -0800

    Allow killing test server/client
    mvn exec:exec doesn't kill the child process when Maven is killed. Now
    we just run ourselves, but have Maven do as much heavy lifting as able.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=81736090

--- run-test-client.sh
+++ run-test-client.sh
@@ -3,6 +3,8 @@ TARGET='Test Service Client'
 TARGET_CLASS='com.google.net.stubby.testing.integration.TestServiceClient'
 TARGET_ARGS="$@"
 
-cd "`dirname "$0"`"/integration-testing
+cd "$(dirname "$0")"
+mvn -q -nsu -pl integration-testing -am package -Dcheckstyle.skip=true -DskipTests
+. integration-testing/target/bootclasspath.properties
 echo "[INFO] Running: $TARGET ($TARGET_CLASS $TARGET_ARGS)"
-exec mvn -q -nsu compile exec:exec -Dcheckstyle.skip=true -DexampleClass="$TARGET_CLASS" -DargLine.example="$TARGET_ARGS"
+exec java "$bootclasspath" -cp "$jar" "$TARGET_CLASS" $TARGET_ARGS
