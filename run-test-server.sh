00a7192f73d641e0dc91b9f830e1c1ade1cecd0a
Eric Anderson <ejona@google.com>
2015-05-01 21:37:06 -0700

    Use CreateStartScripts for integration-testing
    This makes it easy to run our test client/server without running gradle
    every time. For the moment we run 'installDist' in our script since that
    is not performed as part of 'build' or 'assemble'. Once we fix our
    scripts and improve the documentation, we can remove running installdist
    from our script.

--- run-test-server.sh
+++ run-test-server.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -e
-TARGET='Test Service Server'
-TARGET_CLASS='io.grpc.testing.integration.TestServiceServer'
-
-TARGET_ARGS=''
-for i in "$@"; do 
-    TARGET_ARGS="$TARGET_ARGS, '$i'"
-done
-TARGET_ARGS="${TARGET_ARGS:2}"
-
 cd "$(dirname "$0")"
-echo "[INFO] Running: $TARGET ($TARGET_CLASS $TARGET_ARGS)"
-./gradlew -PmainClass="$TARGET_CLASS" -PappArgs="[$TARGET_ARGS]" :grpc-integration-testing:execute
+./gradlew :grpc-integration-testing:installDist
+./integration-testing/build/install/grpc-integration-testing/bin/test-server "$@"
