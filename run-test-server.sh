05f5a4fb3bb21600adbc89c8534f8419e4df9b43
Eric Anderson <ejona@google.com>
2015-07-01 13:20:19 -0700

    Suggest -PskipCodegen in run-test-{client,server}
    Just helps new users wanting to try things out from thinking they need
    to deal with compiling our protoc plugin. As seen in #581

--- run-test-server.sh
+++ run-test-server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 cd "$(dirname "$0")"
 cat >&2 <<EOF
-Gradle is no longer run automatically. Make sure to run './gradlew installDist' or
-'./gradlew :grpc-interop-testing:installDist' after any changes.
+Gradle is no longer run automatically. Make sure to run
+'./gradlew installDist -PskipCodegen=true' or
+'./gradlew :grpc-interop-testing:installDist -PskipCodegen' after any changes.
+-PskipCodegen=true is optional, but requires less setup.
 EOF
 exec ./interop-testing/build/install/grpc-interop-testing/bin/test-server "$@"
