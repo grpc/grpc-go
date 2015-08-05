fc85a4085aad746aed9b3350a47b7dff6f49aa5b
Kun Zhang <zhangkun@google.com>
2015-08-05 16:48:15 -0700

    Add more documentation for transports.
    - Add package descriptions for transport, netty and okhttp.
    - Describe transports (netty, okhttp and inprocess) in README

---
+++ okhttp/src/main/java/io/grpc/transport/okhttp/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A lightweight transport based on
+ * <a target="_blank" href="http://square.github.io/okhttp/">OkHttp</a>, mainly for use on Android
+ * (client-only).
+ */
+package io.grpc.transport.okhttp;
