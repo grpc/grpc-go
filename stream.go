18c359f6a9e2a00ec8615526de6fd597736a108f
Sameer Ajmani <sameer@golang.org>
2015-08-09 23:01:01 -0400

    grpc: rename request trace family from "Sent" to "grpc.Sent".

--- stream.go
+++ stream.go
@@ -107,7 +107,7 @@ func NewClientStream(ctx context.Context, desc *StreamDesc, cc *ClientConn, meth
 		tracing: EnableTracing,
 	}
 	if cs.tracing {
-		cs.traceInfo.tr = trace.New("Sent."+methodFamily(method), method)
+		cs.traceInfo.tr = trace.New("grpc.Sent."+methodFamily(method), method)
 		cs.traceInfo.firstLine.client = true
 		if deadline, ok := ctx.Deadline(); ok {
 			cs.traceInfo.firstLine.deadline = deadline.Sub(time.Now())
