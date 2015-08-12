6eb790420c8cbcde4a61ee833279ad62598849eb
yangzhouhan <yangzhouhan@gmail.com>
2015-08-12 12:49:51 -0700

    fix merge conflict

--- interop/client/client.go
+++ interop/client/client.go
@@ -277,11 +277,7 @@ func doTimeoutOnSleepingServer(tc testpb.TestServiceClient) {
 		grpclog.Fatalf("%v.Send(%v) = %v", stream, req, err)
 	}
 	if _, err := stream.Recv(); grpc.Code(err) != codes.DeadlineExceeded {
-<<<<<<< HEAD
 		grpclog.Fatalf("%v.Recv() = _, %v, want error code %d", stream, err, codes.DeadlineExceeded)
-=======
-		grpclog.Fatalf("%v got the error %v, want error code: %d", stream, err, codes.DeadlineExceeded)
->>>>>>> 2a4442cda2d99dad88ccbcaf39f8975fcf85747c
 	}
 	grpclog.Println("TimeoutOnSleepingServer done")
 }
