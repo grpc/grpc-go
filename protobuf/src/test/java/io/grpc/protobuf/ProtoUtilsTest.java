0b57f56bf001a409d471c77f37c43b55cf5a80fc
nmittler <nathanmittler@google.com>
2015-08-24 07:42:51 -0700

    Reducing the size of the "large" proto in ProtoUtilsTest.
    Hoping to eliminate flakes on Travis.
    
    Fixes #859

--- protobuf/src/test/java/io/grpc/protobuf/ProtoUtilsTest.java
+++ protobuf/src/test/java/io/grpc/protobuf/ProtoUtilsTest.java
@@ -77,7 +77,8 @@ public class ProtoUtilsTest {
 
   @Test
   public void marshallerShouldNotLimitProtoSize() throws Exception {
-    byte[] bigName = new byte[100 * 1024 * 1024];
+    // The default limit is 64MB. Using a larger proto to verify that the limit is not enforced.
+    byte[] bigName = new byte[70 * 1024 * 1024];
     new Random().nextBytes(bigName);
 
     proto = Type.newBuilder().setNameBytes(ByteString.copyFrom(bigName)).build();
