345773504c24e3e52b1b1e586ad8e509ac53425c
Carl Mastrangelo <notcarl@google.com>
2015-08-24 10:57:24 -0700

    Quick work around for Rst bug

--- netty/src/main/java/io/grpc/netty/NettyClientHandler.java
+++ netty/src/main/java/io/grpc/netty/NettyClientHandler.java
@@ -226,7 +226,11 @@ class NettyClientHandler extends Http2ConnectionHandler {
   private void onRstStreamRead(int streamId, long errorCode) throws Http2Exception {
     NettyClientStream stream = clientStream(requireHttp2Stream(streamId));
     Status status = GrpcUtil.Http2Error.statusForCode((int) errorCode);
-    stream.transportReportStatus(status, false, new Metadata());
+    // TODO(carl-mastrangelo): This is a hack!  Currently, due to a bug in the deframer, the stream
+    // listener might not be closed if the stream is stopped while in the middle of a recieving a
+    // message.   This is a quick work around to get things working again, but should be changed
+    // back to not stopping delivery once a proper, thought-out fix is in place in the Deframer.
+    stream.transportReportStatus(status, true /*stop delivery*/, new Metadata());
   }
 
   @Override
