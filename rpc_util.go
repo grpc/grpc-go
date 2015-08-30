08ce04a73b36f376dcca6837ea6cdb720297838f
Ammar Aijazi <aaijazi@google.com>
2015-08-29 18:24:51 -0700

    Add IsRPCErr to test for server application errors

--- rpc_util.go
+++ rpc_util.go
@@ -205,6 +205,13 @@ func (e rpcError) Error() string {
 	return fmt.Sprintf("rpc error: code = %d desc = %q", e.code, e.desc)
 }
 
+// IsRPCError returns true for err if it was produced by the rpc system.
+// Useful for checking if a particular error was produced by the server applications.
+func IsRPCError(err) bool {
+	_, ok := err.(rpcError)
+	return ok
+}
+
 // Code returns the error code for err if it was produced by the rpc system.
 // Otherwise, it returns codes.Unknown.
 func Code(err error) codes.Code {
