579e65cb979fbdc878f04b6e88d05d1299287494
Eric Anderson <ejona@google.com>
2015-10-22 18:14:00 -0700

    Simplify Jenkins configuration on Windows
    Without the attributes, fc loses its mind a bit when comparing the
    codegen output against the golden; the golden would have DOS newlines
    (assuming default git configuration) and the generated code would have
    Unix newlines.
    
    The jenkins-pre script just reduces the amount of code is backed into
    the jenkins project.

---
+++ buildscripts/jenkins-pre.bat
@@ -0,0 +1,17 @@
+if NOT EXIST grpc-java-helper mkdir grpc-java-helper
+cd grpc-java-helper
+
+REM check if 7za is available
+7za > NUL
+if ERRORLEVEL 1 call gsutil cp gs://grpc-jenkins-helpers/java/7za.exe .
+
+call "%VS120COMNTOOLS%\vsvars32.bat"
+call "%WORKSPACE%\buildscripts\make_dependencies.bat"
+
+cd "%WORKSPACE%"
+
+set ESCWORKSPACE=%WORKSPACE:\=\\%
+
+echo targetArch=x86_32> gradle.properties
+echo vcProtobufLibs=%ESCWORKSPACE%\\grpc-java-helper\\protobuf-3.0.0-beta-1\\cmake\\build\\Release>> gradle.properties
+echo vcProtobufInclude=%ESCWORKSPACE%\\grpc-java-helper\\protobuf-3.0.0-beta-1\\cmake\\build\\include>> gradle.properties
