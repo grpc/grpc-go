c1800943ffeb8ad38ecf08bfb4691c0a4f242dab
Carl Mastrangelo <notcarl@google.com>
2015-12-15 14:19:25 -0800

    Bump test coverage

---
+++ core/src/test/java/io/grpc/ForwardingServerCallListenerTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ *    * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.grpc;
+
+import static org.mockito.Mockito.verify;
+
+import io.grpc.ForwardingServerCallListener.SimpleForwardingServerCallListener;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(JUnit4.class)
+public class ForwardingServerCallListenerTest {
+
+  @Mock private ServerCall.Listener<Void> serverCallListener;
+  private ForwardingServerCallListener<Void> forwarder;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    forwarder = new SimpleForwardingServerCallListener<Void>(serverCallListener) {};
+  }
+
+  @Test
+  public void onMessage() {
+    forwarder.onMessage(null);
+
+    verify(serverCallListener).onMessage(null);
+  }
+
+  @Test
+  public void onHalfClose() {
+    forwarder.onHalfClose();
+
+    verify(serverCallListener).onHalfClose();
+  }
+
+  @Test
+  public void onCancel() {
+    forwarder.onCancel();
+
+    verify(serverCallListener).onCancel();
+  }
+
+  @Test
+  public void onComplete() {
+    forwarder.onComplete();
+
+    verify(serverCallListener).onComplete();
+  }
+
+  @Test
+  public void onReady() {
+    forwarder.onReady();
+
+    verify(serverCallListener).onReady();
+  }
+}
+
