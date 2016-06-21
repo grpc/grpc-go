920d8a1bfc2e9434aa968b27bbca4d72af9814e8
Kun Zhang <zhangkun@google.com>
2016-06-21 15:56:15 -0700

    auth: revert ClientAuthInterceptor to its original implementation.
    This partially reverts commit def237d9606cc226f6b523f0818ae2ae664645cf.
    
    This is to avoid breaking internal tests that specifically verify the
    behavior of the original implementation.

--- auth/src/main/java/io/grpc/auth/ClientAuthInterceptor.java
+++ auth/src/main/java/io/grpc/auth/ClientAuthInterceptor.java
@@ -38,8 +38,17 @@ import io.grpc.CallOptions;
 import io.grpc.Channel;
 import io.grpc.ClientCall;
 import io.grpc.ClientInterceptor;
+import io.grpc.ClientInterceptors.CheckedForwardingClientCall;
+import io.grpc.Metadata;
 import io.grpc.MethodDescriptor;
+import io.grpc.Status;
+import io.grpc.StatusException;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 /**
@@ -56,14 +65,104 @@ public final class ClientAuthInterceptor implements ClientInterceptor {
 
   private final Credentials credentials;
 
+  private Metadata cached;
+  private Map<String, List<String>> lastMetadata;
+
   public ClientAuthInterceptor(
       Credentials credentials, @SuppressWarnings("unused") Executor executor) {
     this.credentials = Preconditions.checkNotNull(credentials);
+    // TODO(louiscryan): refresh token asynchronously with this executor.
   }
 
   @Override
   public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
       final MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, final Channel next) {
-    return next.newCall(method, callOptions.withCredentials(MoreCallCredentials.from(credentials)));
+    // TODO(ejona86): If the call fails for Auth reasons, this does not properly propagate info that
+    // would be in WWW-Authenticate, because it does not yet have access to the header.
+    return new CheckedForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
+      @Override
+      protected void checkedStart(Listener<RespT> responseListener, Metadata headers)
+          throws StatusException {
+        Metadata cachedSaved;
+        URI uri = serviceUri(next, method);
+        synchronized (ClientAuthInterceptor.this) {
+          // TODO(louiscryan): This is icky but the current auth library stores the same
+          // metadata map until the next refresh cycle. This will be fixed once
+          // https://github.com/google/google-auth-library-java/issues/3
+          // is resolved.
+          // getRequestMetadata() may return a different map based on the provided URI, i.e., for
+          // JWT. However, today it does not cache JWT and so we won't bother tring to cache its
+          // return value based on the URI.
+          Map<String, List<String>> latestMetadata = getRequestMetadata(uri);
+          if (lastMetadata == null || lastMetadata != latestMetadata) {
+            lastMetadata = latestMetadata;
+            cached = toHeaders(lastMetadata);
+          }
+          cachedSaved = cached;
+        }
+        headers.merge(cachedSaved);
+        delegate().start(responseListener, headers);
+      }
+    };
+  }
+
+  /**
+   * Generate a JWT-specific service URI. The URI is simply an identifier with enough information
+   * for a service to know that the JWT was intended for it. The URI will commonly be verified with
+   * a simple string equality check.
+   */
+  private URI serviceUri(Channel channel, MethodDescriptor<?, ?> method) throws StatusException {
+    String authority = channel.authority();
+    if (authority == null) {
+      throw Status.UNAUTHENTICATED.withDescription("Channel has no authority").asException();
+    }
+    // Always use HTTPS, by definition.
+    final String scheme = "https";
+    final int defaultPort = 443;
+    String path = "/" + MethodDescriptor.extractFullServiceName(method.getFullMethodName());
+    URI uri;
+    try {
+      uri = new URI(scheme, authority, path, null, null);
+    } catch (URISyntaxException e) {
+      throw Status.UNAUTHENTICATED.withDescription("Unable to construct service URI for auth")
+          .withCause(e).asException();
+    }
+    // The default port must not be present. Alternative ports should be present.
+    if (uri.getPort() == defaultPort) {
+      uri = removePort(uri);
+    }
+    return uri;
+  }
+
+  private URI removePort(URI uri) throws StatusException {
+    try {
+      return new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), -1 /* port */,
+          uri.getPath(), uri.getQuery(), uri.getFragment());
+    } catch (URISyntaxException e) {
+      throw Status.UNAUTHENTICATED.withDescription(
+            "Unable to construct service URI after removing port")
+          .withCause(e).asException();
+    }
+  }
+
+  private Map<String, List<String>> getRequestMetadata(URI uri) throws StatusException {
+    try {
+      return credentials.getRequestMetadata(uri);
+    } catch (IOException e) {
+      throw Status.UNAUTHENTICATED.withCause(e).asException();
+    }
+  }
+
+  private static final Metadata toHeaders(Map<String, List<String>> metadata) {
+    Metadata headers = new Metadata();
+    if (metadata != null) {
+      for (String key : metadata.keySet()) {
+        Metadata.Key<String> headerKey = Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER);
+        for (String value : metadata.get(key)) {
+          headers.put(headerKey, value);
+        }
+      }
+    }
+    return headers;
   }
 }
