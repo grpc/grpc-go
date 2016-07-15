07bf1082901dfc3513a50f59b0fa0d1f8baee6b4
iamqizhao <toqizhao@gmail.com>
2016-07-15 16:02:21 -0700

    fix Invoke to accommodate the change

--- call.go
+++ call.go
@@ -90,7 +90,10 @@ func sendRequest(ctx context.Context, codec Codec, compressor Compressor, callHd
 		return nil, transport.StreamErrorf(codes.Internal, "grpc: %v", err)
 	}
 	err = t.Write(stream, outBuf, opts)
-	if err != nil {
+	// NewStream could lead to an early rejection of the RPC (e.g., the service/method
+	// does not exist.) so that t.Write could get io.EOF from wait(...). Leave the following
+	// recvResponse to get the final status.
+	if err != nil || err != io.EOF {
 		return nil, err
 	}
 	// Sent successfully.
