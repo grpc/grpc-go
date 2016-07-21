5502cde5280527283a6c97be44bda37485376e12
Menghan Li <menghanl@google.com>
2016-07-21 10:46:48 -0700

    Reconstruct grpc encoding tests

--- transport/http_util_test.go
+++ transport/http_util_test.go
@@ -109,27 +109,36 @@ func TestValidContentType(t *testing.T) {
 }
 
 func TestGrpcMessageEncode(t *testing.T) {
-	testGrpcMessageEncode(t, "my favorite character is \u0000", "my favorite character is %00")
-	testGrpcMessageEncode(t, "my favorite character is %", "my favorite character is %25")
-}
-
-func TestGrpcMessageDecode(t *testing.T) {
-	testGrpcMessageDecode(t, "Hello", "Hello")
-	testGrpcMessageDecode(t, "H%61o", "Hao")
-	testGrpcMessageDecode(t, "H%6", "H%6")
-	testGrpcMessageDecode(t, "%G0", "%G0")
-}
-
-func testGrpcMessageEncode(t *testing.T, input string, expected string) {
-	actual := grpcMessageEncode(input)
-	if expected != actual {
-		t.Errorf("grpcMessageEncode(%v) = %v, want %v", input, actual, expected)
+	for _, tt := range []struct {
+		input    string
+		expected string
+	}{
+		{"", ""},
+		{"Hello", "Hello"},
+		{"my favorite character is \u0000", "my favorite character is %00"},
+		{"my favorite character is %", "my favorite character is %25"},
+	} {
+		actual := grpcMessageEncode(tt.input)
+		if tt.expected != actual {
+			t.Errorf("grpcMessageEncode(%v) = %v, want %v", tt.input, actual, tt.expected)
+		}
 	}
 }
 
-func testGrpcMessageDecode(t *testing.T, input string, expected string) {
-	actual := grpcMessageDecode(input)
-	if expected != actual {
-		t.Errorf("grpcMessageDncode(%v) = %v, want %v", input, actual, expected)
+func TestGrpcMessageDecode(t *testing.T) {
+	for _, tt := range []struct {
+		input    string
+		expected string
+	}{
+		{"", ""},
+		{"Hello", "Hello"},
+		{"H%61o", "Hao"},
+		{"H%6", "H%6"},
+		{"%G0", "%G0"},
+	} {
+		actual := grpcMessageDecode(tt.input)
+		if tt.expected != actual {
+			t.Errorf("grpcMessageDncode(%v) = %v, want %v", tt.input, actual, tt.expected)
+		}
 	}
 }
