343f6d95a7f6b30967ed50ff14d1478c61909b5e
Mak Mukhi <mmukhi@google.com>
2016-08-10 14:00:28 -0700

    lint updates and removing rand logic

--- backoff_test.go
+++ backoff_test.go
@@ -1,10 +1,6 @@
 package grpc
 
-import ( 
-	"testing"
-	"math/rand"
-	"time"
-)
+import "testing"
 
 func TestBackoffConfigDefaults(t *testing.T) {
 	b := BackoffConfig{}
@@ -15,16 +11,12 @@ func TestBackoffConfigDefaults(t *testing.T) {
 }
 
 func TestBackoffWitDifferentNumberOfRetries(t *testing.T) {
-	const MAX_RETRIES = 10
-	randSrc := rand.NewSource(time.Now().UnixNano())
-	randGen := rand.New(randSrc)
-	for i := 0; i < 5; i++ {
-		// generate a randon number, between 0 and MAX_RETRIES, to be used as number of retries
-		retries := randGen.Intn(MAX_RETRIES)
+	const maxRetries = 10
+	for retries := 0; retries < maxRetries; retries++ {
 		b := BackoffConfig{}
 		setDefaults(&b)
 		backoffTime := b.backoff(retries)
-		// backoff time should be between basedelay and max delay
+		// Backoff time should be between basedelay and max delay.
 		if backoffTime < b.baseDelay || backoffTime > b.MaxDelay {
 			t.Fatalf("expected backoff time: %v to be between basedelay: %v and maxdelay: %v",backoffTime,b.baseDelay,b.MaxDelay)
 		}
@@ -32,14 +24,14 @@ func TestBackoffWitDifferentNumberOfRetries(t *testing.T) {
 }
 
 func TestBackOffTimeIncreasesWithRetries(t *testing.T) {
-	const MAX_RETRIES = 10
+	const maxRetries = 10
 	b := BackoffConfig{}
 	setDefaults(&b)
-	// base delay
+	// Base delay.
 	lastBackOffTime := b.backoff(0)
-	for retries := 1; retries <= MAX_RETRIES; retries++ {
+	for retries := 1; retries <= maxRetries; retries++ {
 		backoffTime := b.backoff(retries)
-		// backoff time should increase as the number of retries increase
+		// Backoff time should increase as the number of retries increase.
 		if backoffTime <= lastBackOffTime {
 			t.Fatalf("backoffTime for %v retries : %v is smaller than backoffTime for %v retries: %v",retries,backoffTime,retries-1,lastBackOffTime)
 		}
