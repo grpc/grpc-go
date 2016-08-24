f254fadfbef1beb131e35b58e69079d440aeb94b
nmittler <nathanmittler@google.com>
2016-08-24 09:54:26 -0700

    Add RunWith annotation to AutoWindowSizingOnTest

--- interop-testing/src/test/java/io/grpc/testing/integration/AutoWindowSizingOnTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/AutoWindowSizingOnTest.java
@@ -39,7 +39,10 @@ import io.grpc.netty.NettyServerBuilder;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class AutoWindowSizingOnTest extends AbstractInteropTest {
 
   @BeforeClass
