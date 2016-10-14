483662e24dd29c105584717914fb0f75d4609c70
Eric Anderson <ejona@google.com>
2016-10-13 17:06:52 -0700

    interop-testing: -PskipCodegen requires =true
    Otherwise it has no effect.

--- run-test-client.sh
+++ run-test-client.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 cat >&2 <<EOF
 Gradle is no longer run automatically. Make sure to run
 './gradlew installDist -PskipCodegen=true' or
-'./gradlew :grpc-interop-testing:installDist -PskipCodegen' after any changes.
--PskipCodegen=true is optional, but requires less setup.
+'./gradlew :grpc-interop-testing:installDist -PskipCodegen=true' after any
+changes. -PskipCodegen=true is optional, but requires less setup.
 EOF
 exec ./interop-testing/build/install/grpc-interop-testing/bin/test-client "$@"
