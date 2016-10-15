46e46eb8bd06d58753ce9e68566eac2d68fb74b1
Jakob Buchgraber <bucjac@gmail.com>
2016-10-15 10:45:00 +0200

    benchmarks: Reduce histogram precision. (#2343)
    It greatly reduces memory usage (from 130KiB to 30KiB per histogram).

--- benchmarks/src/main/java/io/grpc/benchmarks/Utils.java
+++ benchmarks/src/main/java/io/grpc/benchmarks/Utils.java
@@ -87,8 +87,9 @@ public final class Utils {
 
   // The histogram can record values between 1 microsecond and 1 min.
   public static final long HISTOGRAM_MAX_VALUE = 60000000L;
-  // Value quantization will be no larger than 1/10^3 = 0.1%.
-  public static final int HISTOGRAM_PRECISION = 3;
+
+  // Value quantization will be no more than 1%. See the README of HdrHistogram for more details.
+  public static final int HISTOGRAM_PRECISION = 2;
 
   public static final int DEFAULT_FLOW_CONTROL_WINDOW =
       NettyChannelBuilder.DEFAULT_FLOW_CONTROL_WINDOW;
