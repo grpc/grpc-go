77cec023d1dead74678db16a0b51c275a9cdd519
Mahak Mukhi <mmukhi@google.com>
2017-01-10 17:07:04 -0800

    fix

--- credentials/credentials_test.go
+++ credentials/credentials_test.go
@@ -147,7 +147,6 @@ func launchServer(t *testing.T, hs serverHandshake, done chan AuthInfo) net.List
 // Is run in a seperate goroutine.
 func serverHandle(t *testing.T, hs serverHandshake, done chan AuthInfo, lis net.Listener) {
 	serverRawConn, err := lis.Accept()
-	defer serverRawConn.Close()
 	if err != nil {
 		t.Errorf("Server failed to accept connection: %v", err)
 		close(done)
@@ -156,6 +155,7 @@ func serverHandle(t *testing.T, hs serverHandshake, done chan AuthInfo, lis net.
 	serverAuthInfo, err := hs(serverRawConn)
 	if err != nil {
 		t.Errorf("Server failed while handshake. Error: %v", err)
+		serverRawConn.Close()
 		close(done)
 		return
 	}
