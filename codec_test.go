c8c475cb51e650550212e46b43bd0a202610917e
Alexander Polcyn <apolcyn@google.com>
2017-02-13 15:38:17 -0800

    update Fatalf -> Errorf in tests

--- codec_test.go
+++ codec_test.go
@@ -48,15 +48,15 @@ func marshalAndUnmarshal(t *testing.T, protoCodec Codec, expectedBody []byte) {
 
 	marshalledBytes, err := protoCodec.Marshal(p)
 	if err != nil {
-		t.Fatalf("protoCodec.Marshal(_) returned an error")
+		t.Errorf("protoCodec.Marshal(_) returned an error")
 	}
 
 	if err := protoCodec.Unmarshal(marshalledBytes, p); err != nil {
-		t.Fatalf("protoCodec.Unmarshal(_) returned an error")
+		t.Errorf("protoCodec.Unmarshal(_) returned an error")
 	}
 
 	if bytes.Compare(p.GetBody(), expectedBody) != 0 {
-		t.Fatalf("Unexpected body; got %v; want %v", p.GetBody(), expectedBody)
+		t.Errorf("Unexpected body; got %v; want %v", p.GetBody(), expectedBody)
 	}
 }
 
@@ -112,19 +112,19 @@ func TestStaggeredMarshalAndUnmarshalUsingSamePool(t *testing.T) {
 	var err error
 
 	if m1, err = codec1.Marshal(&proto1); err != nil {
-		t.Fatalf("protoCodec.Marshal(%v) failed", proto1)
+		t.Errorf("protoCodec.Marshal(%v) failed", proto1)
 	}
 
 	if m2, err = codec2.Marshal(&proto2); err != nil {
-		t.Fatalf("protoCodec.Marshal(%v) failed", proto2)
+		t.Errorf("protoCodec.Marshal(%v) failed", proto2)
 	}
 
 	if err = codec1.Unmarshal(m1, &proto1); err != nil {
-		t.Fatalf("protoCodec.Unmarshal(%v) failed", m1)
+		t.Errorf("protoCodec.Unmarshal(%v) failed", m1)
 	}
 
 	if err = codec2.Unmarshal(m2, &proto2); err != nil {
-		t.Fatalf("protoCodec.Unmarshal(%v) failed", m2)
+		t.Errorf("protoCodec.Unmarshal(%v) failed", m2)
 	}
 
 	b1 := proto1.GetBody()
@@ -132,13 +132,13 @@ func TestStaggeredMarshalAndUnmarshalUsingSamePool(t *testing.T) {
 
 	for i, v := range b1 {
 		if expectedBody1[i] != v {
-			t.Fatalf("expected %v at index %v but got %v", i, expectedBody1[i], v)
+			t.Errorf("expected %v at index %v but got %v", i, expectedBody1[i], v)
 		}
 	}
 
 	for i, v := range b2 {
 		if expectedBody2[i] != v {
-			t.Fatalf("expected %v at index %v but got %v", i, expectedBody2[i], v)
+			t.Errorf("expected %v at index %v but got %v", i, expectedBody2[i], v)
 		}
 	}
 }
