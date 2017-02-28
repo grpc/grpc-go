d570bc359050a8c725aa0ad20b0e6a0099f66a74
Alexander Polcyn <apolcyn@google.com>
2017-02-27 20:49:33 -0800

    opt in to frame reuse on the framer to reduce garbage

--- transport/http_util.go
+++ transport/http_util.go
@@ -379,6 +379,9 @@ func newFramer(conn net.Conn) *framer {
 		writer: bufio.NewWriterSize(conn, http2IOBufSize),
 	}
 	f.fr = http2.NewFramer(f.writer, f.reader)
+	// Opt-in to Frame reuse API on framer to reduce garbage.
+	// Frames aren't safe to read from after a subsequent call to ReadFrame.
+	f.fr.SetReuseFrames()
 	f.fr.ReadMetaHeaders = hpack.NewDecoder(http2InitHeaderTableSize, nil)
 	return f
 }
