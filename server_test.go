ba23d25e3a10a341be227a75202deb6d2c99bf41
Menghan Li <menghanl@google.com>
2017-03-02 15:21:41 -0800

    relax the checking on error string

--- server_test.go
+++ server_test.go
@@ -60,7 +60,7 @@ func TestStopBeforeServe(t *testing.T) {
 	// server.Serve is responsible for closing the listener, even if the
 	// server was already stopped.
 	err = lis.Close()
-	if got, want := ErrorDesc(err), "use of closed network connection"; !strings.Contains(got, want) {
+	if got, want := ErrorDesc(err), "use of closed"; !strings.Contains(got, want) {
 		t.Errorf("Close() error = %q, want %q", got, want)
 	}
 }
