135247d85c4519e31f5e1b0459658147ab1a6781
Eric Drechsel <ericdrex@gmail.com>
2017-05-15 12:41:55 -0700

    fix server panic trying to send on stream as client disconnects #1111 (#1115)

--- transport/handler_server.go
+++ transport/handler_server.go
@@ -179,11 +179,18 @@ func (a strAddr) String() string { return string(a) }
 
 // do runs fn in the ServeHTTP goroutine.
 func (ht *serverHandlerTransport) do(fn func()) error {
+	// Avoid a panic writing to closed channel. Imperfect but maybe good enough.
 	select {
-	case ht.writes <- fn:
-		return nil
 	case <-ht.closedCh:
 		return ErrConnClosing
+	default:
+		select {
+		case ht.writes <- fn:
+			return nil
+		case <-ht.closedCh:
+			return ErrConnClosing
+		}
+
 	}
 }
 
