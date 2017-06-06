b4ef78daba5676f5fa19c24d657f2043231c46ab
ZhouyihaiDing <ddyihai@google.com>
2017-06-06 13:59:48 -0700

    benchmark17_test: reformat the scenario loop

--- benchmark/benchmark17_test.go
+++ benchmark/benchmark17_test.go
@@ -5,17 +5,20 @@ package benchmark
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/benchmark/stats"
 )
 
-func BenchClient(b *testing.B, maxConcurrentCalls, reqSize, respSize int, enableTrace, unaryMode bool) {
-	if enableTrace {
+func BenchClient(b *testing.B, maxConcurrentCalls, reqSize, respSize int, runMode, enableTrace string) {
+	if strings.EqualFold(enableTrace, "Trace") {
 		grpc.EnableTracing = true
+	} else {
+		grpc.EnableTracing = false
 	}
-	if unaryMode {
+	if strings.EqualFold(runMode, "Unary") {
 		runUnary(b, maxConcurrentCalls, reqSize, respSize)
 	} else {
 		runStream(b, maxConcurrentCalls, reqSize, respSize)
@@ -23,28 +26,27 @@ func BenchClient(b *testing.B, maxConcurrentCalls, reqSize, respSize int, enable
 }
 
 func BenchmarkClient(b *testing.B) {
+	mode_trace := []struct {
+		runMode     string
+		enableTrace string
+	}{
+		{"Unary", "Trace"},
+		{"Unary", "noTrace"},
+		{"Stream", "Trace"},
+		{"Stream", "noTrace"},
+	}
 	maxConcurrentCalls := []int{1, 8, 64, 512}
 	reqSize := []int{1, 1024}
 	respSize := []int{1, 1024}
-	for _, maxC := range maxConcurrentCalls {
-		for _, reqS := range reqSize {
-			for _, respS := range respSize {
-				b.Run(fmt.Sprintf("Unary-Trace-maxConcurrentCalls_"+
-					"%#v-reqSize_%#v-respSize_%#v", maxC, reqS, respS), func(b *testing.B) {
-					BenchClient(b, maxC, reqS, respS, true, true)
-				})
-				b.Run(fmt.Sprintf("Unary-noTrace-maxConcurrentCalls_"+
-					"%#v-reqSize_%#v-respSize_%#v", maxC, reqS, respS), func(b *testing.B) {
-					BenchClient(b, maxC, reqS, respS, false, true)
-				})
-				b.Run(fmt.Sprintf("Stream-Trace-maxConcurrentCalls_"+
-					"%#v-reqSize_%#v-respSize_%#v", maxC, reqS, respS), func(b *testing.B) {
-					BenchClient(b, maxC, reqS, respS, true, false)
-				})
-				b.Run(fmt.Sprintf("Stream-noTrace-maxConcurrentCalls_"+
-					"%#v-reqSize_%#v-respSize_%#v", maxC, reqS, respS), func(b *testing.B) {
-					BenchClient(b, maxC, reqS, respS, false, false)
-				})
+	for _, mt := range mode_trace {
+		for _, maxC := range maxConcurrentCalls {
+			for _, reqS := range reqSize {
+				for _, respS := range respSize {
+					b.Run(fmt.Sprintf("%s-%s-maxConcurrentCalls_"+
+						"%#v-reqSize_%#v-respSize_%#v", mt.runMode, mt.enableTrace, maxC, reqS, respS), func(b *testing.B) {
+						BenchClient(b, maxC, reqS, respS, mt.runMode, mt.enableTrace)
+					})
+				}
 			}
 		}
 	}
