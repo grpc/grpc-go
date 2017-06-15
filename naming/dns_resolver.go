ee4811a50262f6aa311e500cf6f543748aecb661
Yuxuan Li <yuxuanli@google.com>
2017-06-14 18:15:16 -0700

    fix comments: unexport dnsResolver and dnsWatcher, add chan to exit Next(), add freq to control polling frequency

--- naming/dns_resolver.go
+++ naming/dns_resolver.go
@@ -1,8 +1,8 @@
 package naming
 
 import (
+	"errors"
 	"net"
-	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -11,38 +11,52 @@ import (
 	"google.golang.org/grpc/grpclog"
 )
 
-// DNSResolver handles name resolution for names following the DNS scheme
-type DNSResolver struct {
+const (
+	defaultFreq = time.Minute * 30
+)
+
+// NewDNSResolver creates a DNS Resolver that can resolve DNS names.
+func NewDNSResolver() (Resolver, error) {
+	return &dnsResolver{}, nil
 }
 
-// Resolve creates a watcher that watches the resolution of the targeted DNS name.
-func (r *DNSResolver) Resolve(target string) (DNSWatcher, error) {
-	// try to separate name and port if the target is in name:port format
-	port := "443"
-	name := target
-	re := regexp.MustCompile("(:[0-9]+){1}$")
-	if p := re.FindString(target); p != "" {
-		// target in the format of {ip|target}:port
-		name = target[:re.FindStringIndex(target)[0]]
-		port = p[1:]
+// dnsResolver handles name resolution for names following the DNS scheme
+type dnsResolver struct {
+}
+
+// Resolve creates a watcher that watches the name resolution of the target.
+func (r *dnsResolver) Resolve(target string) (Watcher, error) {
+	host, port, err := net.SplitHostPort(target)
+	if err != nil {
+		if err.(*net.AddrError).Err != "missing port in address" {
+			return &dnsWatcher{}, err
+		} else {
+			host = target
+			port = "443"
+		}
 	}
-	return DNSWatcher{
+	return &dnsWatcher{
 		target: target,
-		name:   name,
+		host:   host,
 		port:   port,
+		done:   make(chan struct{}),
+		freq:   defaultFreq,
 	}, nil
 }
 
-// DNSWatcher watches for the name resolution update for a specific target
-type DNSWatcher struct {
+// dnsWatcher watches for the name resolution update for a specific target
+type dnsWatcher struct {
+	dnsResolver
 	// target to watch address Update. TODO(yuxuanli): delete this? since its info is redundant
 	target string
-	// hostname
-	name string
-	// port number
-	port string
+	host   string
+	port   string
 	// The latest resolved address list
 	curAddrs []*Update
+	// done channel is closed to notify Next() to exit.
+	done chan struct{}
+	// frequency of polling the DNS server.
+	freq time.Duration
 }
 
 // AddressType indicates the address type returned by name resolution.
@@ -65,6 +79,8 @@ type AddrMetadataGRPCLB struct {
 	ServerName string
 }
 
+// compileUpdate compares the old resolved addresses and newly resolved addresses,
+// and generates an update list
 func compileUpdate(oldAddrs []*Update, newAddrs []*Update) []*Update {
 	result := make([]*Update, 0, len(oldAddrs)+len(newAddrs))
 	idx1, idx2 := 0, 0
@@ -114,52 +130,78 @@ func compileUpdate(oldAddrs []*Update, newAddrs []*Update) []*Update {
 	return result
 }
 
-// Next returns the resolved address update(delta) for the target. If there's no
-// change, it will sleep for 30 mins and try to resolve again after that.
-func (w *DNSWatcher) Next() ([]*Update, error) {
-	for {
-		_, srvs, err := net.LookupSRV("grpclb", "tcp", w.name)
-		if err != nil {
-			grpclog.Printf("grpc: failed dns SRV record lookup due to %v.\n", err)
-		}
-
-		newAddrs := make([]*Update, 0, 1000 /* TODO: decide the number here*/)
-
-		if len(srvs) > 0 {
-			// target has SRV records associated with it
-			for _, r := range srvs {
-				lbAddrs, err := net.LookupHost(r.Target)
-				if err != nil {
-					grpclog.Printf("grpc: failed dns srv load banlacer address lookup due to %v.\n", err)
-				}
-				for _, a := range lbAddrs {
-					newAddrs = append(newAddrs, &Update{Addr: a + ":" + strconv.Itoa(int(r.Port)),
-						Metadata: AddrMetadataGRPCLB{AddrType: GRPCLB, ServerName: r.Target},
-					})
-				}
-			}
-			sortSlice(newAddrs)
-		} else {
-			// If target doesn't have SRV records associated with it, return any A record info available.
-			addrs, err := net.LookupHost(w.name)
-			if err != nil {
-				grpclog.Printf("grpc: failed dns A record lookup due to %v.\n", err)
-			}
-			sort.Strings(addrs)
-			for _, a := range addrs {
-				newAddrs = append(newAddrs, &Update{Addr: a + ":" + w.port})
-			}
-		}
-		result := compileUpdate(w.curAddrs, newAddrs)
-		w.curAddrs = newAddrs
-		if len(result) > 0 {
-			return result, nil
-		}
-		time.Sleep(30 * time.Minute)
-		continue
-	}
+// Set the frequency at which the watcher polls the DNS server.
+func (w *dnsWatcher) SetFreq(d time.Duration) {
+	w.freq = d
 }
 
-// Close is a Noop for DNSWatcher
-func (w *DNSWatcher) Close() {
+func (w *dnsWatcher) lookup() ([]*Update, error) {
+	_, srvs, err := net.LookupSRV("grpclb", "tcp", w.host)
+	if err != nil {
+		grpclog.Printf("grpc: failed dns SRV record lookup due to %v.\n", err)
+	}
+
+	newAddrs := make([]*Update, 0, 100 /* TODO: decide the number here*/)
+	if len(srvs) > 0 {
+		// target has SRV records associated with it
+		for _, r := range srvs {
+			lbAddrs, err := net.LookupHost(r.Target)
+			if err != nil {
+				// TODO(yuxuanli): For service that doesn't use service config, this will
+				// result in a certain amount of unncessary logs.
+				grpclog.Printf("grpc: failed dns srv load banlacer address lookup due to %v.\n", err)
+			}
+			for _, a := range lbAddrs {
+				newAddrs = append(newAddrs, &Update{Addr: a + ":" + strconv.Itoa(int(r.Port)),
+					Metadata: AddrMetadataGRPCLB{AddrType: GRPCLB, ServerName: r.Target},
+				})
+			}
+		}
+		sortSlice(newAddrs)
+	} else {
+		// If target doesn't have SRV records associated with it, return any A record info available.
+		addrs, err := net.LookupHost(w.host)
+		if err != nil {
+			grpclog.Printf("grpc: failed dns A record lookup due to %v.\n", err)
+		}
+		sort.Strings(addrs)
+		for _, a := range addrs {
+			newAddrs = append(newAddrs, &Update{Addr: a + ":" + w.port})
+		}
+	}
+	result := compileUpdate(w.curAddrs, newAddrs)
+	w.curAddrs = newAddrs
+	return result, nil
+}
+
+// Next returns the resolved address update(delta) for the target. If there's no
+// change, it will sleep for 30 mins and try to resolve again after that.
+func (w *dnsWatcher) Next() ([]*Update, error) {
+	result, err := w.lookup()
+	if err != nil {
+		return nil, err
+	}
+	if len(result) > 0 {
+		return result, nil
+	}
+	ticker := time.NewTicker(w.freq)
+	for {
+		select {
+		case <-w.done:
+			return nil, errors.New("watcher has been closed")
+		case <-ticker.C:
+			result, err := w.lookup()
+			if err != nil {
+				return nil, err
+			}
+			if len(result) > 0 {
+				return result, nil
+			}
+		}
+	}
+
+}
+
+func (w *dnsWatcher) Close() {
+	close(w.done)
 }
--- naming/dns_resolver_test.go
+++ naming/dns_resolver_test.go
@@ -1,8 +1,9 @@
 package naming
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
+	"time"
 )
 
 type testcase struct {
@@ -15,25 +16,6 @@ func newUpdate(op Operation, addr string) *Update {
 		Op:   op,
 		Addr: addr,
 	}
-}
-
-func checkEquality(a []*Update, b []*Update) bool {
-	if a == nil && b == nil {
-		return true
-	}
-	if a == nil || b == nil {
-		return false
-	}
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i, bitem := range a {
-		if a[i] != bitem {
-			return false
-		}
-	}
-	return true
 }
 
 var updateTestcases = []testcase{
@@ -71,7 +53,7 @@ var updateResult = [][]*Update{
 	{newUpdate(Add, "1.0.0.1")},
 	{},
 	{newUpdate(Delete, "1.0.0.0"), newUpdate(Add, "1.0.0.1")},
-	{newUpdate(Add, "1.0.0.0"), newUpdate(Add, "1.0.0.1")},
+	{newUpdate(Add, "1.0.0.0"), newUpdate(Delete, "1.0.0.1")},
 	{newUpdate(Add, "1.0.0.2"), newUpdate(Add, "1.0.0.3")},
 	{newUpdate(Add, "1.0.0.0"), newUpdate(Delete, "1.0.0.1"), newUpdate(Delete, "1.0.0.2"), newUpdate(Delete, "1.0.0.3")},
 	{newUpdate(Delete, "1.0.0.1"), newUpdate(Add, "1.0.0.2"), newUpdate(Delete, "1.0.0.5"), newUpdate(Add, "1.0.0.6")},
@@ -79,7 +61,6 @@ var updateResult = [][]*Update{
 
 func TestCompileUpdate(t *testing.T) {
 	for i, c := range updateTestcases {
-		fmt.Printf("%d: ", i)
 		oldUpdates := make([]*Update, len(c.oldAddrs))
 		newUpdates := make([]*Update, len(c.newAddrs))
 		for i, a := range c.oldAddrs {
@@ -89,50 +70,75 @@ func TestCompileUpdate(t *testing.T) {
 			newUpdates[i] = &Update{Addr: a}
 		}
 		r := compileUpdate(oldUpdates, newUpdates)
-		if !checkEquality(updateResult[i], r) {
+		if !reflect.DeepEqual(updateResult[i], r) {
 			t.Errorf("Wrong update generated. idx: %d\n", i)
 		}
-		for _, u := range r {
-			if u.Op == Add {
-				fmt.Print("Add ")
-			} else {
-				fmt.Print("Delete ")
-			}
-			fmt.Printf("%s\t", u.Addr)
+	}
+}
+
+var testAddrs = map[string]bool{
+	// TODO(yuxuanli): More false cases?
+	"www.google.com":            true,
+	"foo.bar:12345":             true,
+	"127.0.0.1":                 true,
+	"127.0.0.1:12345":           true,
+	"[::1]:80":                  true,
+	"[2001:db8:a0b:12f0::1]:21": true,
+	"[2001:db8:a0b:12f0::1":     false,
+	"127.0.0...1:12345":         true, // SplitHostPort doesn't seem to check ipv4 format.
+}
+
+func TestResolveFunc(t *testing.T) {
+	r, err := NewDNSResolver()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	for k, v := range testAddrs {
+		_, err := r.Resolve(k)
+		if (err == nil) != v {
+			t.Errorf("%s is a %v address while it is returned as a %v address", k, v, err == nil)
 		}
-		fmt.Println("")
 	}
 }
 
 var addrToResolve = []string{
-// "www.google.com",
+	"localhost",
 }
 
 var addrResolved = [][]*Update{
-// {newUpdate(Add, "216.58.194.196:443"), newUpdate(Add, "2607:f8b0:4005:808::2004:443")},
+	{newUpdate(Add, "127.0.0.1:443"), newUpdate(Add, "::1:443")},
 }
 
 func TestResolver(t *testing.T) {
 	for i, a := range addrToResolve {
-		r := DNSResolver{}
+		r, err := NewDNSResolver()
+		if err != nil {
+			t.Errorf("%v\n", err)
+		}
 		w, err := r.Resolve(a)
 		if err != nil {
 			t.Errorf("%v\n", err)
 		}
-		updates, err := w.Next()
+		defer w.Close()
+		var updates []*Update
+		w.(*dnsWatcher).SetFreq(time.Millisecond * 500)
+		go func() {
+			for {
+				u, err := w.Next()
+				if err != nil {
+					return
+				}
+				updates = u
+			}
+		}()
 		if err != nil {
 			t.Errorf("%v\n", err)
 		}
-		if !checkEquality(addrResolved[i], updates) {
+		// Sleep for sometime to let watcher do more than one lookup
+		time.Sleep(time.Second * 1)
+
+		if !reflect.DeepEqual(addrResolved[i], updates) {
 			t.Errorf("Wrong resolved update , idx: %d, name: %s\n", i, a)
-		}
-		for _, u := range updates {
-			if u.Op == Add {
-				fmt.Print("Add ")
-			} else {
-				fmt.Print("Delete ")
-			}
-			fmt.Printf("%s\t", u.Addr)
 		}
 	}
 }
