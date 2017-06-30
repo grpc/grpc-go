3e9d86629e819a52589a561853b3d93733e3c7bf
Tao Li <lita@google.com>
2017-06-29 19:19:36 -0700

    Implement the mTLS for gRPC service, and extract client side ID out

--- examples/helloworld/greeter_server/main.go
+++ examples/helloworld/greeter_server/main.go
@@ -22,8 +22,15 @@ import (
 	"log"
 	"net"
 
+	"crypto/tls"
+	"crypto/x509"
+
+	"io/ioutil"
+
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/reflection"
 )
@@ -37,15 +44,47 @@ type server struct{}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+	log.Printf("Greeting: %s", ctx)
+        peer, _ := peer.FromContext(ctx)
+
+        tlsInfo := peer.AuthInfo.(credentials.TLSInfo);
+	log.Printf("\nGreeting: %s", tlsInfo.State.VerifiedChains[0][0].Subject.CommonName)
+	return &pb.HelloReply{Message: "Heiillo " + in.Name}, nil
 }
 
 func main() {
+	certificate, err := tls.LoadX509KeyPair(
+                "server.cert.pem",
+	        "server.key.pem",
+	)
+	certPool := x509.NewCertPool()
+	bs, err := ioutil.ReadFile("ca.cert.pem")
+	if err != nil {
+		  log.Fatalf("failed to read client ca cert: %s", err)
+	}
+
+	ok := certPool.AppendCertsFromPEM(bs)
+	if !ok {
+		  log.Fatal("failed to append client certs")
+	  }
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	tlsConfig := &tls.Config{
+                ClientAuth:   tls.RequireAndVerifyClientCert,
+	        Certificates: []tls.Certificate{certificate},
+	        ClientCAs:    certPool,
+        }
+	// Create the TLS credentials
+	serverOption := grpc.Creds(credentials.NewTLS(tlsConfig))
+	s := grpc.NewServer(serverOption)
+	// creds, err := credentials.NewServerTLSFromFile("server.crt", "server.key")
+	// if err != nil {
+	//        log.Fatalf("could not load TLS keys: %s", err)
+	// }
+
+	// s := grpc.NewServer(grpc.Creds(creds))
 	pb.RegisterGreeterServer(s, &server{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
