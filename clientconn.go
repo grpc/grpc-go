aa5b2f7368b8f78de2f1e7377d9d8164d284221e
mmukhi <mmukhi@google.com>
2017-07-28 09:37:53 -0700

    Enabling client process multiple GoAways (#1393)

--- clientconn.go
+++ clientconn.go
@@ -932,12 +932,8 @@ func (ac *addrConn) resetTransport(drain bool) error {
 	t := ac.transport
 	ac.transport = nil
 	ac.mu.Unlock()
-	if t != nil {
-		if drain {
-			t.GracefulClose()
-		} else {
-			t.Close()
-		}
+	if t != nil && !drain {
+		t.Close()
 	}
 	ac.cc.mu.RLock()
 	ac.dopts.copts.KeepaliveParams = ac.cc.mkp
