1f48cf28657e1ac914ae6c7d5cda6f22c0b463ac
xuechen <xuechen@mogujie.com>
2017-08-14 14:49:47 +0800

    put z back to pool

--- rpc_util.go
+++ rpc_util.go
@@ -63,6 +63,9 @@ func NewGZIPCompressor() Compressor {
 
 func (c *gzipCompressor) Do(w io.Writer, p []byte) error {
 	z := c.pool.Get().(*gzip.Writer)
+	defer func() {
+		c.pool.Put(z)
+	}()
 	z.Reset(w)
 	if _, err := z.Write(p); err != nil {
 		return err
