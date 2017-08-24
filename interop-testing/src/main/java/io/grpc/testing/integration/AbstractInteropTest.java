23dfc84ccf9193e269aba730321f5fa49d3a1735
ZHANG Dapeng <zdapeng@google.com>
2017-08-24 14:27:49 -0700

    interop-testing: fix dealineExceeded test flakiness by increasing time interval
    Should resolve #1399

--- interop-testing/src/main/java/io/grpc/testing/integration/AbstractInteropTest.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/AbstractInteropTest.java
@@ -994,7 +994,7 @@ public abstract class AbstractInteropTest {
                 .build()).next();
   }
 
-  @Test(timeout = 10000)
+  @Test(timeout = 25000)
   public void deadlineExceeded() throws Exception {
     // warm up the channel and JVM
     blockingStub.emptyCall(Empty.getDefaultInstance());
@@ -1002,7 +1002,7 @@ public abstract class AbstractInteropTest {
         blockingStub.withDeadlineAfter(10, TimeUnit.MILLISECONDS);
     StreamingOutputCallRequest request = StreamingOutputCallRequest.newBuilder()
         .addResponseParameters(ResponseParameters.newBuilder()
-            .setIntervalUs(20000))
+            .setIntervalUs((int) TimeUnit.SECONDS.toMicros(20)))
         .build();
     try {
       stub.streamingOutputCall(request).next();
