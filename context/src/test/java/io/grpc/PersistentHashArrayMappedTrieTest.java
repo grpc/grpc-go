812e65a5ca109610422177b6e62eddd8465d01f9
Eric Gribkoff <ericgribkoff@google.com>
2017-08-28 12:11:02 -0700

    context: add RunWith annotation

--- context/src/test/java/io/grpc/PersistentHashArrayMappedTrieTest.java
+++ context/src/test/java/io/grpc/PersistentHashArrayMappedTrieTest.java
@@ -25,7 +25,10 @@ import io.grpc.PersistentHashArrayMappedTrie.CompressedIndex;
 import io.grpc.PersistentHashArrayMappedTrie.Leaf;
 import io.grpc.PersistentHashArrayMappedTrie.Node;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class PersistentHashArrayMappedTrieTest {
   @Test
   public void leaf_replace() {
