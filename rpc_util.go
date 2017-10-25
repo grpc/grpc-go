61d37c5d657a47e4404fd6823bd598341a2595de
Menghan Li <menghanl@google.com>
2017-10-25 15:03:47 -0700

    Change version to 1.7.1

--- rpc_util.go
+++ rpc_util.go
@@ -567,6 +567,6 @@ const SupportPackageIsVersion3 = true
 const SupportPackageIsVersion4 = true
 
 // Version is the current grpc version.
-const Version = "1.7.1-dev"
+const Version = "1.7.1"
 
 const grpcUA = "grpc-go/" + Version
