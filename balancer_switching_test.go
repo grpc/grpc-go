7d7a8e37f02021d344d0896d0aeddd79cbf23d68
Yuxuan Li <yuxuanli@google.com>
2017-11-13 15:21:28 -0800

    policy name with underscore

--- balancer_switching_test.go
+++ balancer_switching_test.go
@@ -121,12 +121,12 @@ func TestSwitchBalancer(t *testing.T) {
 		t.Fatalf("check pickfirst returned non-nil error: %v", err)
 	}
 	// Switch to roundrobin.
-	cc.handleServiceConfig(`{"loadBalancingPolicy": "roundrobin"}`)
+	cc.handleServiceConfig(`{"loadBalancingPolicy": "round_robin"}`)
 	if err := checkRoundRobin(cc, servers); err != nil {
 		t.Fatalf("check roundrobin returned non-nil error: %v", err)
 	}
 	// Switch to pickfirst.
-	cc.handleServiceConfig(`{"loadBalancingPolicy": "pickfirst"}`)
+	cc.handleServiceConfig(`{"loadBalancingPolicy": "pick_first"}`)
 	if err := checkPickFirst(cc, servers); err != nil {
 		t.Fatalf("check pickfirst returned non-nil error: %v", err)
 	}
