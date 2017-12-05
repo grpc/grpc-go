43efd4de1a633fec8dc11c1a227f0e496b03da7c
Menghan Li <menghanl@google.com>
2017-12-05 13:48:16 -0800

    Add WithResolverUserOptions for custom resolver build options

--- resolver_conn_wrapper.go
+++ resolver_conn_wrapper.go
@@ -83,7 +83,9 @@ func newCCResolverWrapper(cc *ClientConn) (*ccResolverWrapper, error) {
 	}
 
 	var err error
-	ccr.resolver, err = rb.Build(cc.parsedTarget, ccr, resolver.BuildOption{})
+	ccr.resolver, err = rb.Build(cc.parsedTarget, ccr, resolver.BuildOption{
+		UserOptions: cc.dopts.resolverBuildUserOptions,
+	})
 	if err != nil {
 		return nil, err
 	}
