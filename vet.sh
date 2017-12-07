27f2cb37a091c6cdb8db49bdaa8815bbb9cc3dbe
Menghan Li <menghanl@google.com>
2017-12-06 16:04:41 -0800

    Revert "Always run go generate. Revert this!!!"
    This reverts commit a58ba7d8408f3a09699385c261733878065a101c.

--- vet.sh
+++ vet.sh
@@ -15,9 +15,6 @@ if [[ "$TRAVIS" != "true" || "$TRAVIS_EVENT_TYPE" = "cron" ]]; then
   check_proto="true"
 fi
 
-# Remove this!!!
-check_proto="true"
-
 if [ "$1" = "-install" ]; then
   go get -d \
     google.golang.org/grpc/...
