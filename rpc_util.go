fb57512bcf559adb14c19326b46a54cf0bbdefb0
Menghan Li <menghanl@google.com>
2017-12-18 15:27:04 -0800

    Change version to 1.7.6-dev

--- rpc_util.go
+++ rpc_util.go
@@ -567,6 +567,6 @@ const SupportPackageIsVersion3 = true
 const SupportPackageIsVersion4 = true
 
 // Version is the current grpc version.
-const Version = "1.7.5"
+const Version = "1.7.6-dev"
 
 const grpcUA = "grpc-go/" + Version
