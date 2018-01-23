45132051b8a51ace3ed185cd0ba011797899e47f
Doug Fawley <dfawley@google.com>
2018-01-23 09:11:35 -0800

    encoding comment fix

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -87,12 +87,13 @@ var registeredCodecs = make(map[string]Codec, 0)
 // RegisterCodec registers the provided Codec for use with all gRPC clients and
 // servers.
 //
-// The Codec will be stored and looked up by result of its Name() method,
-// which should match the content-subtype of the encoding handled by the Codec.
-// This is case-insensitive, and is stored and looked up as lowercase.  If the
+// The Codec will be stored and looked up by result of its Name() method, which
+// should match the content-subtype of the encoding handled by the Codec.  This
+// is case-insensitive, and is stored and looked up as lowercase.  If the
 // result of calling Name() is an empty string, RegisterCodec will panic. See
-// Content-Type on https://grpc.io/docs/guides/wire.html#requests for more
-// details.
+// Content-Type on
+// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests for
+// more details.
 //
 // NOTE: this function must only be called during initialization time (i.e. in
 // an init() function), and is not thread-safe.  If multiple Compressors are
