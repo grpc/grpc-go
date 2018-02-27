cc0dc05c61b09c6093be2db727804005f68adc2a
Menghan Li <menghanl@google.com>
2018-02-27 13:13:27 -0800

    make timeout 10s for streaming RPCs

--- examples/route_guide/mock_routeguide/rg_mock_test.go
+++ examples/route_guide/mock_routeguide/rg_mock_test.go
@@ -60,7 +60,7 @@ func TestRouteChat(t *testing.T) {
 }
 
 func testRouteChat(client rgpb.RouteGuideClient) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	stream, err := client.RouteChat(ctx)
 	if err != nil {
