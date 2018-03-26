e793dcee05321ba3b0cb5fc07371b08933ac163a
Menghan Li <menghanl@google.com>
2018-03-26 14:35:32 -0700

    tests

--- resolver_conn_wrapper_test.go
+++ resolver_conn_wrapper_test.go
@@ -26,18 +26,10 @@ import (
 
 func TestParseTarget(t *testing.T) {
 	for _, test := range []resolver.Target{
-		{"", "", ""},
-		{"a", "", ""},
-		{"", "a", ""},
-		{"", "", "a"},
-		{"a", "b", ""},
-		{"a", "", "b"},
-		{"", "a", "b"},
-		{"a", "b", "c"},
 		{"dns", "", "google.com"},
 		{"dns", "a.server.com", "google.com"},
 		{"dns", "a.server.com", "google.com/?a=b"},
-		{"", "", "/unix/socket/address"},
+		{"passthrough", "", "/unix/socket/address"},
 	} {
 		str := test.Scheme + "://" + test.Authority + "/" + test.Endpoint
 		got := parseTarget(str)
@@ -82,6 +74,41 @@ func TestSplitTargetString(t *testing.T) {
 		got := splitTarget(test.targetStr)
 		if got != test.want {
 			t.Errorf("splitTarget(%q) = %+v, want %+v", test.targetStr, got, test.want)
+		}
+	}
+}
+
+func TestParseTargetUnknownScheme(t *testing.T) {
+	for _, test := range []struct {
+		targetStr string
+		want      resolver.Target
+	}{
+		{"", resolver.Target{"", "", ""}},
+		{":///", resolver.Target{"", "", ":///"}},
+		{"a:///", resolver.Target{"", "", "a:///"}},
+		{"://a/", resolver.Target{"", "", "://a/"}},
+		{":///a", resolver.Target{"", "", ":///a"}},
+		{"a://b/", resolver.Target{"", "", "a://b/"}},
+		{"a:///b", resolver.Target{"", "", "a:///b"}},
+		{"://a/b", resolver.Target{"", "", "://a/b"}},
+		{"a://b/c", resolver.Target{"", "", "a://b/c"}},
+
+		{"/", resolver.Target{"", "", "/"}},
+		{"google.com", resolver.Target{"", "", "google.com"}},
+		{"google.com/?a=b", resolver.Target{"", "", "google.com/?a=b"}},
+		{"/unix/socket/address", resolver.Target{"", "", "/unix/socket/address"}},
+
+		// Special test for "unix:///".
+		{"unix:///unix/socket/address", resolver.Target{"", "", "unix:///unix/socket/address"}},
+
+		// For known scheme.
+		{"dns:///google.com", resolver.Target{"dns", "", "google.com"}},
+		{"dns://a.server.com/google.com", resolver.Target{"dns", "a.server.com", "google.com"}},
+		{"dns://a.server.com/google.com/?a=b", resolver.Target{"dns", "a.server.com", "google.com/?a=b"}},
+	} {
+		got := parseTarget(test.targetStr)
+		if got != test.want {
+			t.Errorf("parseTarget(%q) = %+v, want %+v", test.targetStr, got, test.want)
 		}
 	}
 }
