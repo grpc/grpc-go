30d1a223a5d3b8107d23d09b044f59c186869822
Menghan Li <menghanl@google.com>
2018-04-10 16:09:11 -0700

    fix review comments

--- clientconn.go
+++ clientconn.go
@@ -1518,10 +1518,11 @@ func (ac *addrConn) getState() connectivity.State {
 	return ac.state
 }
 
-func (ac *addrConn) getCurAddr() resolver.Address {
+func (ac *addrConn) getCurAddr() (ret resolver.Address) {
 	ac.mu.Lock()
-	defer ac.mu.Unlock()
-	return ac.curAddr
+	ret = ac.curAddr
+	ac.mu.Unlock()
+	return
 }
 
 func (ac *addrConn) ChannelzMetric() *channelz.ChannelInternalMetric {
