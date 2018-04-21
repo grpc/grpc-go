9f6270848b8c0f0eef3358368dc9916044f2e883
Carl Mastrangelo <notcarl@google.com>
2018-04-20 17:40:32 -0700

    benchmarks: remove unused proto import

--- benchmarks/src/generated/main/java/io/grpc/benchmarks/proto/Services.java
+++ benchmarks/src/generated/main/java/io/grpc/benchmarks/proto/Services.java
@@ -24,29 +24,28 @@ public final class Services {
   static {
     java.lang.String[] descriptorData = {
       "\n\016services.proto\022\014grpc.testing\032\016messages" +
-      ".proto\032\rcontrol.proto\032\013stats.proto2\246\003\n\020B" +
-      "enchmarkService\022F\n\tUnaryCall\022\033.grpc.test" +
-      "ing.SimpleRequest\032\034.grpc.testing.SimpleR" +
-      "esponse\022N\n\rStreamingCall\022\033.grpc.testing." +
-      "SimpleRequest\032\034.grpc.testing.SimpleRespo" +
-      "nse(\0010\001\022R\n\023StreamingFromClient\022\033.grpc.te" +
-      "sting.SimpleRequest\032\034.grpc.testing.Simpl" +
-      "eResponse(\001\022R\n\023StreamingFromServer\022\033.grp" +
-      "c.testing.SimpleRequest\032\034.grpc.testing.S" +
-      "impleResponse0\001\022R\n\021StreamingBothWays\022\033.g" +
-      "rpc.testing.SimpleRequest\032\034.grpc.testing" +
-      ".SimpleResponse(\0010\0012\227\002\n\rWorkerService\022E\n" +
-      "\tRunServer\022\030.grpc.testing.ServerArgs\032\032.g" +
-      "rpc.testing.ServerStatus(\0010\001\022E\n\tRunClien" +
-      "t\022\030.grpc.testing.ClientArgs\032\032.grpc.testi" +
-      "ng.ClientStatus(\0010\001\022B\n\tCoreCount\022\031.grpc." +
-      "testing.CoreRequest\032\032.grpc.testing.CoreR" +
-      "esponse\0224\n\nQuitWorker\022\022.grpc.testing.Voi" +
-      "d\032\022.grpc.testing.Void2^\n\030ReportQpsScenar" +
-      "ioService\022B\n\016ReportScenario\022\034.grpc.testi" +
-      "ng.ScenarioResult\032\022.grpc.testing.VoidB$\n" +
-      "\030io.grpc.benchmarks.protoB\010Servicesb\006pro" +
-      "to3"
+      ".proto\032\rcontrol.proto2\246\003\n\020BenchmarkServi" +
+      "ce\022F\n\tUnaryCall\022\033.grpc.testing.SimpleReq" +
+      "uest\032\034.grpc.testing.SimpleResponse\022N\n\rSt" +
+      "reamingCall\022\033.grpc.testing.SimpleRequest" +
+      "\032\034.grpc.testing.SimpleResponse(\0010\001\022R\n\023St" +
+      "reamingFromClient\022\033.grpc.testing.SimpleR" +
+      "equest\032\034.grpc.testing.SimpleResponse(\001\022R" +
+      "\n\023StreamingFromServer\022\033.grpc.testing.Sim" +
+      "pleRequest\032\034.grpc.testing.SimpleResponse" +
+      "0\001\022R\n\021StreamingBothWays\022\033.grpc.testing.S" +
+      "impleRequest\032\034.grpc.testing.SimpleRespon" +
+      "se(\0010\0012\227\002\n\rWorkerService\022E\n\tRunServer\022\030." +
+      "grpc.testing.ServerArgs\032\032.grpc.testing.S" +
+      "erverStatus(\0010\001\022E\n\tRunClient\022\030.grpc.test" +
+      "ing.ClientArgs\032\032.grpc.testing.ClientStat" +
+      "us(\0010\001\022B\n\tCoreCount\022\031.grpc.testing.CoreR" +
+      "equest\032\032.grpc.testing.CoreResponse\0224\n\nQu" +
+      "itWorker\022\022.grpc.testing.Void\032\022.grpc.test" +
+      "ing.Void2^\n\030ReportQpsScenarioService\022B\n\016" +
+      "ReportScenario\022\034.grpc.testing.ScenarioRe" +
+      "sult\032\022.grpc.testing.VoidB$\n\030io.grpc.benc" +
+      "hmarks.protoB\010Servicesb\006proto3"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
         new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
@@ -61,11 +60,9 @@ public final class Services {
         new com.google.protobuf.Descriptors.FileDescriptor[] {
           io.grpc.benchmarks.proto.Messages.getDescriptor(),
           io.grpc.benchmarks.proto.Control.getDescriptor(),
-          io.grpc.benchmarks.proto.Stats.getDescriptor(),
         }, assigner);
     io.grpc.benchmarks.proto.Messages.getDescriptor();
     io.grpc.benchmarks.proto.Control.getDescriptor();
-    io.grpc.benchmarks.proto.Stats.getDescriptor();
   }
 
   // @@protoc_insertion_point(outer_class_scope)
