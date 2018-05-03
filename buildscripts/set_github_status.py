60a0b0c471d720b0546ee3a5b4fa4283635dfbcf
Carl Mastrangelo <notcarl@google.com>
2018-05-03 14:55:21 -0700

    all: normalize copyright header

--- buildscripts/set_github_status.py
+++ buildscripts/set_github_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 #
-# Copyright 2018 gRPC authors.
+# Copyright 2018 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- cronet/src/main/java/io/grpc/cronet/CronetWritableBuffer.java
+++ cronet/src/main/java/io/grpc/cronet/CronetWritableBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, gRPC Authors All rights reserved.
+ * Copyright 2016 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
