b83312fb7b0a679dc6ff8131be93f4b5d90c2c32
Eric Gribkoff <ericgribkoff@google.com>
2018-05-03 17:22:15 -0700

    buildscripts: generate android artifact

--- buildscripts/kokoro/linux_artifacts.sh
+++ buildscripts/kokoro/linux_artifacts.sh
@@ -18,3 +18,18 @@ docker build -t protoc-artifacts /tmp/protobuf/protobuf-92898e9e9cb2f1c006fcc509
 docker build -t grpc-java-releasing "$GRPC_JAVA_DIR"/buildscripts/grpc-java-releasing
 
 "$GRPC_JAVA_DIR"/buildscripts/run_in_docker.sh /grpc-java/buildscripts/build_artifacts_in_docker.sh
+
+# grpc-android requires the Android SDK, so build outside of Docker and
+# use --include-build for its grpc-core dependency
+LOCAL_MVN_TEMP=$(mktemp -d)
+pushd "$GRPC_JAVA_DIR/android"
+../gradlew uploadArchives \
+  --include-build "$GRPC_JAVA_DIR" \
+  -Dorg.gradle.parallel=false \
+  -PskipCodegen=true \
+  -PrepositoryDir="$LOCAL_MVN_TEMP"
+popd
+
+readonly MVN_ARTIFACT_DIR="${MVN_ARTIFACT_DIR:-$GRPC_JAVA_DIR/mvn-artifacts}"
+mkdir -p "$MVN_ARTIFACT_DIR"
+cp -r "$LOCAL_MVN_TEMP"/* "$MVN_ARTIFACT_DIR"/
