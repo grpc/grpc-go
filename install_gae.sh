ac1ff0db9ebade1e15e326fa2220fae19bdafa43
Yuxuan Li <yuxuanli@google.com>
2018-06-20 16:46:16 -0700

    uuuu

--- install_gae.sh
+++ install_gae.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-set -ex  # Exit on error; debugging enabled.
-set -o pipefail  # Fail a pipe if any sub-command fails.
-
-mkdir /tmp/sdk
-curl -o /tmp/sdk.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.64.zip"
-unzip -q /tmp/sdk.zip -d /tmp/sdk
-export PATH="$PATH:/tmp/sdk/go_appengine"
+TMP=$(mktemp -d /tmp/sdk.XXX) \
+&& curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.64.zip" \
+&& unzip -q $TMP.zip -d $TMP \
+&& export PATH="$PATH:$TMP/go_appengine"
