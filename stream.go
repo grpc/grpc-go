75f47d317b8de4e184816252c4e70bf7e030dd73
Doug Fawley <dfawley@google.com>
2018-06-29 12:59:48 -0700

    Never finish attempt unless we finish the stream or begin a new attempt

--- stream.go
+++ stream.go
@@ -644,12 +644,7 @@ func (cs *clientStream) SendMsg(m interface{}) (err error) {
 
 func (cs *clientStream) RecvMsg(m interface{}) error {
 	err := cs.withRetry(func(a *csAttempt) error {
-		err := a.recvMsg(m)
-		if !cs.desc.ServerStreams {
-			// Finish for non-server-streaming RPCs even on non-nil error.
-			a.finish(err)
-		}
-		return err
+		return a.recvMsg(m)
 	}, cs.commitAttemptLocked)
 	if err != nil || !cs.desc.ServerStreams {
 		// err != nil or non-server-streaming indicates end of stream.
