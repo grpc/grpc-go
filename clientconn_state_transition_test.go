1cf228f8d488870c4603794de09eaec4a875c178
Jean de Klerk <deklerk@google.com>
2018-10-11 11:35:01 -0700

    Review comments, and make tests more correct

--- clientconn_state_transition_test.go
+++ clientconn_state_transition_test.go
@@ -33,19 +33,23 @@ import (
 	"google.golang.org/grpc/resolver/manual"
 )
 
+const stateRecordingBalancerName = "state_recoding_balancer"
+
 var testBalancer = &stateRecordingBalancer{}
 
 func init() {
-	balancer.Register(&stateRecordingBalancerBuilder{name: "state_recording_balancer", b: testBalancer})
+	balancer.Register(testBalancer)
 }
 
 func TestStateTransitions_SingleAddress(t *testing.T) {
-	for i, test := range []struct {
+	for _, test := range []struct {
+		name   string
 		want   []connectivity.State
 		server func(net.Listener)
 	}{
 		// When the server returns server preface, the client enters READY.
 		{
+			name: "ServerEntersReadyOnPrefaceReceipt",
 			want: []connectivity.State{
 				connectivity.Connecting,
 				connectivity.Ready,
@@ -66,6 +70,7 @@ func TestStateTransitions_SingleAddress(t *testing.T) {
 		},
 		// When the connection is closed, the client enters TRANSIENT FAILURE.
 		{
+			name: "ServerEntersTransientFailureOnClose",
 			want: []connectivity.State{
 				connectivity.Connecting,
 				connectivity.TransientFailure,
@@ -76,39 +81,12 @@ func TestStateTransitions_SingleAddress(t *testing.T) {
 					t.Error(err)
 					return
 				}
-
-				conn.Close()
-			},
-		},
-		// When a READY connection is closed, the client enters TRANSIENT FAILURE before CONNECTING.
-		{
-			want: []connectivity.State{
-				connectivity.Connecting,
-				connectivity.Ready,
-				connectivity.TransientFailure,
-				connectivity.Connecting,
-			},
-			server: func(lis net.Listener) {
-				conn, err := lis.Accept()
-				if err != nil {
-					t.Error(err)
-					return
-				}
-
-				framer := http2.NewFramer(conn, conn)
-				if err := framer.WriteSettings(http2.Setting{}); err != nil {
-					t.Errorf("Error while writing settings frame. %v", err)
-					return
-				}
-
-				// Prevents race between onPrefaceReceipt and onClose.
-				time.Sleep(50 * time.Millisecond)
 
 				conn.Close()
 			},
 		},
 	} {
-		t.Logf("Test %d", i)
+		t.Logf("Test %s", test.name)
 		testStateTransitionSingleAddress(t, test.want, test.server)
 	}
 }
@@ -132,7 +110,7 @@ func testStateTransitionSingleAddress(t *testing.T, want []connectivity.State, s
 	// Launch the server.
 	go server(lis)
 
-	client, err := DialContext(ctx, lis.Addr().String(), WithWaitForHandshake(), WithInsecure(), WithBalancerName("state_recording_balancer"))
+	client, err := DialContext(ctx, lis.Addr().String(), WithWaitForHandshake(), WithInsecure(), WithBalancerName(stateRecordingBalancerName))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -150,55 +128,86 @@ func testStateTransitionSingleAddress(t *testing.T, want []connectivity.State, s
 			}
 		}
 	}
-	select {
-	case seen := <-stateNotifications:
-		t.Fatalf("unexpectedly saw extra state %v after flow %v", seen, want)
-	default:
-	}
 }
 
-func TestStateTransitions_TwoAddresses(t *testing.T) {
-	for i, test := range []struct {
-		want    []connectivity.State
-		server1 func(net.Listener, chan struct{})
-		server2 func(net.Listener, chan struct{})
-	}{
-		// When the first connection is closed, the client enters stays in CONNECTING until it tries the second
-		// address before transitioning to TRANSIENT FAILURE.
-		{
-			want: []connectivity.State{
-				connectivity.Connecting,
-				connectivity.TransientFailure,
-			},
-			server1: func(lis net.Listener, done chan struct{}) {
-				conn, err := lis.Accept()
-				if err != nil {
-					t.Error(err)
-					return
-				}
-
-				conn.Close()
-				close(done)
-			},
-			server2: func(lis net.Listener, done chan struct{}) {
-				conn, err := lis.Accept()
-				if err != nil {
-					t.Error(err)
-					return
-				}
-
-				conn.Close()
-				close(done)
-			},
-		},
-	} {
-		t.Logf("Test %d", i)
-		testStateTransitionTwoAddresses(t, test.want, test.server1, test.server2)
-	}
-}
-
-func testStateTransitionTwoAddresses(t *testing.T, want []connectivity.State, server1, server2 func(net.Listener, chan struct{})) {
+// When a READY connection is closed, the client enters TRANSIENT FAILURE before CONNECTING.
+func TestStateTransition_ReadyToTransientFailure(t *testing.T) {
 	defer leakcheck.Check(t)
+
+	want := []connectivity.State{
+		connectivity.Connecting,
+		connectivity.Ready,
+		connectivity.TransientFailure,
+		connectivity.Connecting,
+	}
+
+	stateNotifications := make(chan connectivity.State, len(want))
+	testBalancer.ResetNotifier(stateNotifications)
+	defer close(stateNotifications)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	lis, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("Error while listening. Err: %v", err)
+	}
+	defer lis.Close()
+
+	sawReady := make(chan struct{})
+
+	// Launch the server.
+	go func() {
+		conn, err := lis.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		framer := http2.NewFramer(conn, conn)
+		if err := framer.WriteSettings(http2.Setting{}); err != nil {
+			t.Errorf("Error while writing settings frame. %v", err)
+			return
+		}
+
+		// Prevents race between onPrefaceReceipt and onClose.
+		<-sawReady
+
+		conn.Close()
+	}()
+
+	client, err := DialContext(ctx, lis.Addr().String(), WithWaitForHandshake(), WithInsecure(), WithBalancerName(stateRecordingBalancerName))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	timeout := time.After(5 * time.Second)
+
+	for i := 0; i < len(want); i++ {
+		select {
+		case <-timeout:
+			t.Fatalf("timed out waiting for state %d (%v) in flow %v", i, want[i], want)
+		case seen := <-stateNotifications:
+			if seen == connectivity.Ready {
+				close(sawReady)
+			}
+			if seen != want[i] {
+				t.Fatalf("expected to see %v at position %d in flow %v, got %v", want[i], i, want, seen)
+			}
+		}
+	}
+}
+
+// When the first connection is closed, the client enters stays in CONNECTING until it tries the second
+// address (which succeeds, and then it enters READY).
+func TestStateTransitions_TriesAllAddrsBeforeTransientFailure(t *testing.T) {
+	defer leakcheck.Check(t)
+
+	want := []connectivity.State{
+		connectivity.Connecting,
+		connectivity.Ready,
+	}
 
 	stateNotifications := make(chan connectivity.State, len(want))
 	testBalancer.ResetNotifier(stateNotifications)
@@ -222,16 +231,39 @@ func testStateTransitionTwoAddresses(t *testing.T, want []connectivity.State, se
 	server1Done := make(chan struct{})
 	server2Done := make(chan struct{})
 
-	// Launch the server.
-	go server1(lis1, server1Done)
-	go server2(lis2, server2Done)
+	// Launch server 1.
+	go func() {
+		conn, err := lis1.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		conn.Close()
+		close(server1Done)
+	}()
+	// Launch server 2.
+	go func() {
+		conn, err := lis2.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		framer := http2.NewFramer(conn, conn)
+		if err := framer.WriteSettings(http2.Setting{}); err != nil {
+			t.Errorf("Error while writing settings frame. %v", err)
+			return
+		}
+		close(server2Done)
+	}()
 
 	rb := manual.NewBuilderWithScheme("whatever")
 	rb.InitialAddrs([]resolver.Address{
 		{Addr: lis1.Addr().String()},
 		{Addr: lis2.Addr().String()},
 	})
-	client, err := DialContext(ctx, "this-gets-overwritten", WithInsecure(), WithWaitForHandshake(), WithBalancerName("state_recording_balancer"), withResolverBuilder(rb))
+	client, err := DialContext(ctx, "this-gets-overwritten", WithInsecure(), WithWaitForHandshake(), WithBalancerName(stateRecordingBalancerName), withResolverBuilder(rb))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -259,22 +291,13 @@ func testStateTransitionTwoAddresses(t *testing.T, want []connectivity.State, se
 		t.Fatal("saw the correct state transitions, but timed out waiting for client to finish interactions with server 2")
 	case <-server2Done:
 	}
-	select {
-	case seen := <-stateNotifications:
-		t.Fatalf("unexpectedly saw extra state %v after flow %v", seen, want)
-	default:
-	}
 }
 
 type stateRecordingBalancer struct {
 	mu       sync.Mutex
 	notifier chan<- connectivity.State
 
-	underlyingBalancer balancer.Balancer
-}
-
-func (b *stateRecordingBalancer) HandleResolvedAddrs(addrs []resolver.Address, err error) {
-	b.underlyingBalancer.HandleResolvedAddrs(addrs, err)
+	balancer.Balancer
 }
 
 func (b *stateRecordingBalancer) HandleSubConnStateChange(sc balancer.SubConn, s connectivity.State) {
@@ -282,7 +305,7 @@ func (b *stateRecordingBalancer) HandleSubConnStateChange(sc balancer.SubConn, s
 	b.notifier <- s
 	b.mu.Unlock()
 
-	b.underlyingBalancer.HandleSubConnStateChange(sc, s)
+	b.Balancer.HandleSubConnStateChange(sc, s)
 }
 
 func (b *stateRecordingBalancer) ResetNotifier(r chan<- connectivity.State) {
@@ -293,23 +316,18 @@ func (b *stateRecordingBalancer) ResetNotifier(r chan<- connectivity.State) {
 
 func (b *stateRecordingBalancer) Close() {
 	b.mu.Lock()
-	u := b.underlyingBalancer
+	u := b.Balancer
 	b.mu.Unlock()
 	u.Close()
 }
 
-type stateRecordingBalancerBuilder struct {
-	name string
-	b    *stateRecordingBalancer
+func (b *stateRecordingBalancer) Name() string {
+	return stateRecordingBalancerName
 }
 
-func (bb *stateRecordingBalancerBuilder) Name() string {
-	return bb.name
-}
-
-func (bb *stateRecordingBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
-	bb.b.mu.Lock()
-	bb.b.underlyingBalancer = balancer.Get(PickFirstBalancerName).Build(cc, opts)
-	bb.b.mu.Unlock()
-	return bb.b
+func (b *stateRecordingBalancer) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
+	b.mu.Lock()
+	b.Balancer = balancer.Get(PickFirstBalancerName).Build(cc, opts)
+	b.mu.Unlock()
+	return b
 }
