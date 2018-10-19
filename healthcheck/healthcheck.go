90920ab179059f3292f1dd5cddd25def74ebf07c
Can Guler <canguler@google.com>
2018-10-18 18:18:18 -0700

    Fixes a typo in the filename.

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -16,87 +16,91 @@
  *
  */
 
-package healthcheck
+ package healthcheck
 
-import (
-	"context"
-	"errors"
-	"io"
-	"time"
+ import (
+	 "context"
+	 "errors"
+	 "io"
+	 "runtime/debug"
+	 "time"
 
-	"google.golang.org/grpc/internal/backoff"
+	 "google.golang.org/grpc/internal/backoff"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/internal"
-	"google.golang.org/grpc/status"
-)
+	 "google.golang.org/grpc"
+	 "google.golang.org/grpc/codes"
+	 healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	 "google.golang.org/grpc/internal"
+	 "google.golang.org/grpc/status"
+ )
 
-func init() {
-	internal.HealthCheckFunc = newClientHealthCheck
-}
+ func init() {
+	 internal.HealthCheckFunc = newClientHealthCheck
+ }
 
-func newClientHealthCheck(ctx context.Context, newStream func() (interface{}, error), update func(bool), service string) error {
-	retryCnt := 0
-	doBackoff := false
-	bo := backoff.Exponential{MaxDelay: 5 * time.Second}
-retryConnection:
-	for {
-		if !doBackoff {
-			retryCnt = 0
-		} else {
-			timer := time.NewTimer(bo.Backoff(retryCnt))
-			select {
-			case <-timer.C:
-			case <-ctx.Done():
-				timer.Stop()
-				return nil
-			}
-			retryCnt++
-		}
-		doBackoff = false
+ const maxDelay time.Duration = 5 * time.Second
 
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-		}
-		rawS, err := newStream()
-		if err != nil {
-			doBackoff = true
-			continue retryConnection
-		}
-		s, ok := rawS.(grpc.ClientStream)
-		if !ok {
-			// exit the health check function
-			return errors.New("type assertion to grpc.ClientStream failed")
-		}
-		if err = s.SendMsg(&healthpb.HealthCheckRequest{
-			Service: service,
-		}); err != nil && err != io.EOF {
-			//stream should have been closed, so we can safely continue to create a new stream.
-			continue retryConnection
-		}
-		s.CloseSend()
-		for {
-			resp := new(healthpb.HealthCheckResponse)
-			if err = s.RecvMsg(resp); err != nil {
-				if s, ok := status.FromError(err); ok && s.Code() == codes.Unimplemented {
-					update(true)
-					return err
-				}
-				// transition to TRANSIENT FAILURE when Watch() fails with status other than UNIMPLEMENTED
-				update(false)
-				// we can safely break here and continue to create a new stream, since a non-nil error has been received.
-				continue retryConnection
-			}
-			switch resp.Status {
-			case healthpb.HealthCheckResponse_SERVING:
-				update(true)
-			case healthpb.HealthCheckResponse_SERVICE_UNKNOWN, healthpb.HealthCheckResponse_UNKNOWN, healthpb.HealthCheckResponse_NOT_SERVING:
-				update(false)
-			}
-		}
-	}
-}
+ func newClientHealthCheck(ctx context.Context, newStream func() (interface{}, error), update func(bool), service string) error {
+	 debug.PrintStack()
+	 retryCnt := 0
+	 doBackoff := false
+	 bo := backoff.Exponential{MaxDelay: maxDelay}
+ retryConnection:
+	 for {
+		 if !doBackoff {
+			 retryCnt = 0
+		 } else {
+			 timer := time.NewTimer(bo.Backoff(retryCnt))
+			 select {
+			 case <-timer.C:
+			 case <-ctx.Done():
+				 timer.Stop()
+				 return nil
+			 }
+			 retryCnt++
+		 }
+		 doBackoff = false
+
+		 select {
+		 case <-ctx.Done():
+			 return nil
+		 default:
+		 }
+		 rawS, err := newStream()
+		 if err != nil {
+			 doBackoff = true
+			 continue retryConnection
+		 }
+		 s, ok := rawS.(grpc.ClientStream)
+		 if !ok {
+			 // exit the health check function
+			 return errors.New("type assertion to grpc.ClientStream failed")
+		 }
+		 if err = s.SendMsg(&healthpb.HealthCheckRequest{
+			 Service: service,
+		 }); err != nil && err != io.EOF {
+			 //stream should have been closed, so we can safely continue to create a new stream.
+			 continue retryConnection
+		 }
+		 s.CloseSend()
+		 for {
+			 resp := new(healthpb.HealthCheckResponse)
+			 if err = s.RecvMsg(resp); err != nil {
+				 if s, ok := status.FromError(err); ok && s.Code() == codes.Unimplemented {
+					 update(true)
+					 return err
+				 }
+				 // transition to TRANSIENT FAILURE when Watch() fails with status other than UNIMPLEMENTED
+				 update(false)
+				 // we can safely break here and continue to create a new stream, since a non-nil error has been received.
+				 continue retryConnection
+			 }
+			 switch resp.Status {
+			 case healthpb.HealthCheckResponse_SERVING:
+				 update(true)
+			 case healthpb.HealthCheckResponse_SERVICE_UNKNOWN, healthpb.HealthCheckResponse_UNKNOWN, healthpb.HealthCheckResponse_NOT_SERVING:
+				 update(false)
+			 }
+		 }
+	 }
+ }
