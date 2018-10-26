dabe7199137d1f4b862019868146f218e250ef8a
Carl Mastrangelo <notcarl@google.com>
2018-10-26 16:27:03 -0700

    core: add option to fail tests that use Status.equals

--- core/src/main/java/io/grpc/Status.java
+++ core/src/main/java/io/grpc/Status.java
@@ -226,6 +226,10 @@ public final class Status {
     }
   }
 
+  private static final String TEST_EQUALS_FAILURE_PROPERTY = "io.grpc.Status.failOnEqualsForTest";
+  private static final boolean failOnEqualsForTest =
+      Boolean.parseBoolean(System.getProperty(TEST_EQUALS_FAILURE_PROPERTY, "false"));
+  
   // Create the canonical list of Status instances indexed by their code values.
   private static final List<Status> STATUS_LIST = buildStatusList();
 
@@ -653,6 +657,8 @@ public final class Status {
    */
   @Override
   public boolean equals(Object obj) {
+    assert !failOnEqualsForTest
+        : "Status.equals called; disable this by setting " + TEST_EQUALS_FAILURE_PROPERTY;
     return super.equals(obj);
   }
 
