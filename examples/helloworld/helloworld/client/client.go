6603227855c8e20ae07b725c5a1a50053b80426d
Menghan Li <menghanl@google.com>
2018-12-03 16:49:25 -0800

    copy
    copy helloworld
    
    copy routeguide

---
+++ examples/helloworld/helloworld/client/client.go
@@ -0,0 +1,39 @@
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package client
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+// CallSayHello calls SayHello on c with the given name, and prints the
+// response.
+func CallSayHello(c pb.GreeterClient, name string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
