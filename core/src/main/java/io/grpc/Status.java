bcb11b1155233051f838686e89b83fbfd43b1fcc
Michael Plump <plumpy@gmail.com>
2019-02-07 17:00:10 -0500

    core: add @CheckReturnValue to Status.

--- core/src/main/java/io/grpc/Status.java
+++ core/src/main/java/io/grpc/Status.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 
@@ -50,6 +51,7 @@ import javax.annotation.concurrent.Immutable;
  * back out.
  */
 @Immutable
+@CheckReturnValue
 public final class Status {
 
   /**
