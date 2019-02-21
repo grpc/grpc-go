d44d015c44ed965c7fad4779d1ec97a69bd4e758
David Hoover <karma@deadmoose.com>
2019-02-20 16:40:57 -0800

    doc: Fix typo

--- netty/src/main/java/io/grpc/netty/ProtocolNegotiators.java
+++ netty/src/main/java/io/grpc/netty/ProtocolNegotiators.java
@@ -296,7 +296,7 @@ final class ProtocolNegotiators {
       } else {
         /*
          * Implementation note: We pick -1 as the port here rather than deriving it from the
-         * original socket address.  The SSL engine doens't use this port number when contacting the
+         * original socket address.  The SSL engine doesn't use this port number when contacting the
          * remote server, but rather it is used for other things like SSL Session caching.  When an
          * invalid authority is provided (like "bad_cert"), picking the original port and passing it
          * in would mean that the port might used under the assumption that it was correct.   By
