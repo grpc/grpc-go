e7a635d882324a32ce6f9ca26808e962cb91ea12
Eric Anderson <ejona@google.com>
2019-03-13 13:43:21 -0600

    buildscripts: Update protoc-artifacts to 3.7.0
    This was missed from d7e53e871
    
    Also added missing --tmpdir, to avoid extracting protobuf
    into the current directory.

--- buildscripts/build_docker.sh
+++ buildscripts/build_docker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
-readonly proto_dir="$(mktemp -d protobuf.XXXXXX)"
-# Download an unreleased SHA to include TLS 1.2 support:
-# https://github.com/google/protobuf/pull/4879
-wget -O - https://github.com/google/protobuf/archive/61476b8e74357ea875f71bb321874ca4530b7d50.tar.gz | tar xz -C "$proto_dir"
+readonly proto_dir="$(mktemp -d --tmpdir protobuf.XXXXXX)"
+wget -O - https://github.com/google/protobuf/archive/v3.7.0.tar.gz | tar xz -C "$proto_dir"
 
-docker build -t protoc-artifacts "$proto_dir"/protobuf-61476b8e74357ea875f71bb321874ca4530b7d50/protoc-artifacts
+docker build -t protoc-artifacts "$proto_dir"/protobuf-3.7.0/protoc-artifacts
 rm -r "$proto_dir"
