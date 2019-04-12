00e65e2b8b6e0eabd835e2be3ef852f269ecf81d
Easwar Swaminathan <easwars@google.com>
2019-04-11 17:01:14 -0700

    Handle review comments.
    Move MinConnectionTimeout() out of backoff.Strategy interface. Store
    it directly in dialOptions instead, and have ClientConn use it from
    there.

--- backoff.go
+++ backoff.go
@@ -27,33 +27,34 @@ import (
 
 // DefaultBackoffConfig uses values specified for backoff in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
-// Deprecated: use ConnectRetryParams instead.
+// Deprecated: use ConnectParams instead.
 var DefaultBackoffConfig = BackoffConfig{
 	MaxDelay: 120 * time.Second,
 }
 
 // BackoffConfig defines the parameters for the default gRPC backoff strategy.
-// Deprecated: use ConnectRetryParams instead.
+// Deprecated: use ConnectParams instead.
 type BackoffConfig struct {
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
 
-// ConnectRetryParams defines the parameters for the backoff during connection
-// retries. Users are encouraged to use this instead of BackoffConfig.
+// ConnectParams defines the parameters for connecting and retrying. Users
+// are encouraged to use this instead of BackoffConfig.
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // This API is EXPERIMENTAL.
-type ConnectRetryParams struct {
-	// BaseDelay is the amount of time to backoff after the first connection
-	// failure.
-	BaseDelay time.Duration
-	// Multiplier is the factor with which to multiply backoffs after a failed
-	// retry.
-	Multiplier float64
-	// Jitter is the factor with which backoffs are randomized.
-	Jitter float64
-	// MaxDelay is the upper bound of backoff delay.
-	MaxDelay time.Duration
+type ConnectParams struct {
+	// BackoffBaseDelay is the amount of time to backoff after the first
+	// connection failure.
+	BackoffBaseDelay time.Duration
+	// BackoffMultiplier is the factor with which to multiply backoffs after a
+	// failed retry. Should ideally be greater than 1.
+	BackoffMultiplier float64
+	// BackoffJitter is the factor with which backoffs are randomized.
+	BackoffJitter float64
+	// BackoffMaxDelay is the upper bound of backoff delay.
+	BackoffMaxDelay time.Duration
 	// MinConnectTimeout is the minimum amount of time we are willing to give a
 	// connection to complete.
 	MinConnectTimeout time.Duration
