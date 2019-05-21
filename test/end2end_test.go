4637fdfb26f5b405d2ce488d446d8037f13b4244
Can Guler <canguler@google.com>
2019-05-21 14:17:45 -0700

    Remove unintended change.

--- test/end2end_test.go
+++ test/end2end_test.go
@@ -2098,7 +2098,7 @@ func testPreloaderClientSend(t *testing.T, e env) {
 
 	// Test for streaming RPC recv.
 	// Set context for send with proper RPC Information
-	stream, err := tc.FullDuplexCall(te.ctx)
+	stream, err := tc.FullDuplexCall(te.ctx, grpc.UseCompressor("gzip"))
 	if err != nil {
 		t.Fatalf("%v.FullDuplexCall(_) = _, %v, want <nil>", tc, err)
 	}
