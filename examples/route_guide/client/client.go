f7e18a395df0da67e8189ddd22d4134d2ce5fc7a
jbpratt <29494941+jbpratt78@users.noreply.github.com>
2019-06-19 10:24:51 -0700

    examples: fixed spelling mistake in route_guide client example (#2873)

--- examples/route_guide/client/client.go
+++ examples/route_guide/client/client.go
@@ -38,7 +38,7 @@ import (
 
 var (
 	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	caFile             = flag.String("ca_file", "", "The file containning the CA root cert file")
+	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
 	serverAddr         = flag.String("server_addr", "127.0.0.1:10000", "The server address in the format of host:port")
 	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name use to verify the hostname returned by TLS handshake")
 )
