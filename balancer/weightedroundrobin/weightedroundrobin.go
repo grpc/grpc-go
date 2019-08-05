5dd27113a7ac54846e480cd93000c3eec914ee56
Andrew Lazarev <alazarev@dropbox.com>
2019-08-05 15:15:02 -0700

    Made gofmt happy

--- balancer/weightedroundrobin/weightedroundrobin.go
+++ balancer/weightedroundrobin/weightedroundrobin.go
@@ -16,12 +16,14 @@
  *
  */
 
+// Package roundrobin defines a weighted roundrobin balancer.
 package weightedroundrobin
 
 // Name is the name of weighted_round_robin balancer.
 const Name = "weighted_round_robin"
 
-// Information that should be stored inside Address metadata in order to use wrr.
-type AddrInfo struct{
+// AddrInfo will stored inside Address metadata in order to use weighted roundrobin
+// balancer.
+type AddrInfo struct {
 	Weight uint32
 }
