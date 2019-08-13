639b0e66d0daea207a2273ed1fd21a0c5a143568
Doug Fawley <dfawley@google.com>
2019-08-13 10:52:26 -0700

    transport: block reading frames when too many transport control frames are queued (#2970)
    Addresses CVE-2019-9512 (Ping Flood), CVE-2019-9514 (Reset Flood), and CVE-2019-9515 (Settings Flood).

--- internal/transport/http2_client.go
+++ internal/transport/http2_client.go
@@ -1234,6 +1234,7 @@ func (t *http2Client) reader() {
 
 	// loop to keep reading incoming messages on this transport.
 	for {
+		t.controlBuf.throttle()
 		frame, err := t.framer.fr.ReadFrame()
 		if t.keepaliveEnabled {
 			atomic.CompareAndSwapUint32(&t.activity, 0, 1)
