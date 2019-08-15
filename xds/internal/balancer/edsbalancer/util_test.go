d7c2ca0123624bc6b514315286b87838bc6c1328
Andrew Lazarev <alazarev@dropbox.com>
2019-08-15 10:49:51 -0700

    Fix compilation error

--- xds/internal/balancer/edsbalancer/util_test.go
+++ xds/internal/balancer/edsbalancer/util_test.go
@@ -65,6 +65,14 @@ func (twrr *testWRR) Next() interface{} {
 	return iww.item
 }
 
+func (twrr *testWRR) GetItems() map[interface{}]struct{} {
+	res := make(map[interface{}]struct{})
+	for _, item := range twrr.itemsWithWeight {
+		res[item.item] = struct{}{}
+	}
+	return res
+}
+
 func init() {
 	newRandomWRR = newTestWRR
 }
