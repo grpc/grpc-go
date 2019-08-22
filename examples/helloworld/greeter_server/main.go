0574097d679347aa1d3a3dc1590e0bc1decbc0c0
Michael Le <lem12@mcmaster.ca>
2019-08-22 16:56:23 -0400

    example: use proto message Get methods in helloworld (#2990)

--- examples/helloworld/greeter_server/main.go
+++ examples/helloworld/greeter_server/main.go
@@ -39,8 +39,8 @@ type server struct{}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.Name)
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
