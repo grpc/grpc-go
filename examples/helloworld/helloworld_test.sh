cfdb2075bf2af3751a491dd9af735e12945b9251
le-michael <lem12@mcmaster.ca>
2019-10-16 22:46:59 -0400

    Edit script to not modify file tree

--- examples/helloworld/helloworld_test.sh
+++ examples/helloworld/helloworld_test.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+ 
+#  Copyright 2019 gRPC authors.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# 
 
 set +e
 
 clean () {
-    if [ ! -z $PID1 ]; then
-        kill $PID1
-        wait $PID1 2>/dev/null
-    fi
-
-    rm -f greeter_server/server
-    rm -f greeter_client/client
-    
-    rm out.log
+    fuser -k 50051/tcp
+    wait
 }
-
 
 fail () {
     echo "$(tput setaf 1) $1 $(tput sgr 0)"
@@ -22,13 +29,11 @@ fail () {
 }
 
 pass () {
-    echo "$(tput setaf 2) $1 $(tput sgr 0)"
+    echo $"$(tput setaf 2) $1 $(tput sgr 0)"
 }
 
-
 # Build greeter server
-cd greeter_server && go build -o server &>/dev/null
-if [ $? -ne 0 ]; then
+if ! go build -o /dev/null ./greeter_server/*.go; then
     fail "failed to build greeter server"
 else
     pass "successfully built greeter server"
@@ -36,8 +41,7 @@ else
 fi
 
 # Build greeter client
-cd ../greeter_client && go build -o client &>/dev/null
-if [ $? -ne 0 ]; then
+if ! go build -o /dev/null ./greeter_client/*.go; then
     fail "failed to build greeter client"
 else
     pass "successfully built greeter client"
@@ -51,21 +55,17 @@ else
     pass "client failed to communicate when there is no active server"
 fi
 
-# Return to helloworld directory
-cd ..
-
 # Server should be able to start
-./greeter_server/server &> out.log &
-PID1=$!
-if [ $? -ne 0 ]; then
-    fail "failed to start greeter server"
-else
-    pass "successfully started greeter server"
-fi
+SERVER_LOG="$(mktemp)"
+go run greeter_server/*.go &> $SERVER_LOG & 
+
+# Sleep to make sure server starts
+sleep 0.5
 
 # Client should be able to communicate to the active server
-./greeter_client/client &>> out.log
-if [ $? -ne 0 ]; then
+CLIENT_LOG="$(mktemp)"
+go run greeter_client/*.go &> $CLIENT_LOG
+if [ $? -ne 0 ] ; then
     fail "client failed to communicate with server"
 else
     pass "client successfully communicated with server"
@@ -73,20 +73,24 @@ fi
 
 # Out log should contain the string 'Received: world'
 # from the server.
-server_output=`grep "Received: world" out.log`
-if [ -z "$server_output" ]; then
-    fail "out log missing server output: 'Received: world'"
+if ! grep "Received: world" $SERVER_LOG; then
+    fail "Server log missing server output: 'Received: world'
+    Got: 
+    $(cat $SERVER_LOG)
+    "
 else
-    pass "out log contains server output: 'Received: world'"
+    pass "Server log contains server output: 'Received: world'"
 fi
 
 # Out log should contain the string 'Greeting: Hello world'
 # from the client.
-client_output=`grep "Greeting: Hello world" out.log`
-if [ -z "$client_output" ]; then
-    fail "out log missing client output: 'Greeting: Hello world'"
+if ! grep "Greeting: Hello world" $CLIENT_LOG ; then
+    fail "Client log missing client output: 'Greeting: Hello world'
+    Got: 
+    $(cat $CLIENT_LOG)
+    "
 else
-    pass "out log contains client output: 'Greeting: Hello world'"
+    pass "Client log contains client output: 'Greeting: Hello world'"
 fi
 
 clean
