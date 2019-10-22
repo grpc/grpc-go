a7c1650ef711dc94b8fd7ad4ed53d90445be2c62
Ning Xie <andy.xning@gmail.com>
2019-10-23 01:46:34 +0800

    fix typos (#3111)

--- internal/internal.go
+++ internal/internal.go
@@ -28,9 +28,9 @@ import (
 )
 
 var (
-	// WithResolverBuilder is exported by dialoptions.go
+	// WithResolverBuilder is set by dialoptions.go
 	WithResolverBuilder interface{} // func (resolver.Builder) grpc.DialOption
-	// WithHealthCheckFunc is not exported by dialoptions.go
+	// WithHealthCheckFunc is set by dialoptions.go
 	WithHealthCheckFunc interface{} // func (HealthChecker) DialOption
 	// HealthCheckFunc is used to provide client-side LB channel health checking
 	HealthCheckFunc HealthChecker
