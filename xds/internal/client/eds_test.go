339fee6bcdfd9afbb58ee90ebd00abc98e3214fc
Menghan Li <menghanl@google.com>
2019-11-13 14:31:00 -0800

    [xds_eds_parsing_move] travis???

--- xds/internal/client/eds_test.go
+++ xds/internal/client/eds_test.go
@@ -23,7 +23,7 @@ import (
 
 // Only error cases are tested, normal cases are covered because EDS balancer
 // tests build an EDS responses and parses them.
-// TODO: add more tests, including normal cases.
+// TODO: add more tests, with error cases and normal cases.
 
 // Test that parsing fails if EDS response doesn't have all priorities.
 // Priorities should range from 0 (highest) to N (lowest) without skipping
