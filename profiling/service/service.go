4b4e28f170c00d94d4a736f5bf01d82d54d3b908
Adhityaa Chandrasekar <adtac@google.com>
2019-11-13 18:29:02 -0800

    PATCH V2

--- profiling/service/service.go
+++ profiling/service/service.go
@@ -16,9 +16,14 @@
  *
  */
 
+// The service package defines methods to register a gRPC client/service for a
+// profiling service that is exposed in the same server. This service can be
+// queried by a client to remotely manage the gRPC profiling behaviour of an
+// application.
 package service
 
 import (
+	"errors"
 	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
@@ -28,51 +33,69 @@ import (
 	pspb "google.golang.org/grpc/profiling/proto/service"
 )
 
+// Profiling configuration options.
 type ProfilingConfig struct {
-	Enabled         bool
+	// Setting this to true will enable profiling.
+	Enabled bool
+
+	// Profiling uses a circular buffer (ring buffer) to store statistics for
+	// only the last few RPCs so that profiling stats do not grow unbounded. This
+	// parameter defines the upper limit on the number of RPCs for which
+	// statistics should be stored at any given time. An average RPC requires
+	// approximately 2-3 KiB of memory for profiling-related statistics, so
+	// choose an appropriate number based on the amount of memory you can afford.
 	StreamStatsSize uint32
-	Server          *grpc.Server
+
+	// To expose the profiling service and its methods, a *grpc.Server must be
+	// provided.
+	Server *grpc.Server
 }
 
-func registerService(s *grpc.Server) {
-	grpclog.Infof("registering profiling service")
-	pspb.RegisterProfilingServer(s, &profilingServer{})
-}
+var errorNilServer = errors.New("No grpc.Server provided.")
 
-func Init(pc *ProfilingConfig) (err error) {
-	if err = profiling.InitStats(pc.StreamStatsSize); err != nil {
-		return
+// Init takes a *ProfilingConfig to initialise profiling (turned on/off
+// depending on the value set in pc.Enabled) and register the profiling service
+// in the server provided in pc.Server.
+func Init(pc *ProfilingConfig) error {
+	if pc.Server == nil {
+		return errorNilServer
 	}
 
-	registerService(pc.Server)
+	if err := profiling.InitStats(pc.StreamStatsSize); err != nil {
+		return err
+	}
+
+	pspb.RegisterProfilingServer(pc.Server, &profilingServer{})
 
 	// Do this last after everything has been initialised and allocated.
 	profiling.SetEnabled(pc.Enabled)
 
-	return
+	return nil
 }
 
 type profilingServer struct{}
 
-func (s *profilingServer) SetEnabled(ctx context.Context, req *pspb.SetEnabledRequest) (ser *pspb.SetEnabledResponse, err error) {
-	grpclog.Infof("processing SetEnabled (%v)", req.Enabled)
+func (s *profilingServer) Enable(ctx context.Context, req *pspb.EnableRequest) (*pspb.EnableResponse, error) {
+	if req.Enabled {
+		grpclog.Infof("Enabling profiling")
+	} else {
+		grpclog.Infof("Disabling profiling")
+	}
 	profiling.SetEnabled(req.Enabled)
 
-	ser = &pspb.SetEnabledResponse{Success: true}
-	err = nil
-	return
+	return &pspb.SetEnabledResponse{}, nil
 }
 
-func (s *profilingServer) GetStreamStats(req *pspb.GetStreamStatsRequest, stream pspb.Profiling_GetStreamStatsServer) (err error) {
-	grpclog.Infof("processing stream request for stream stats")
+func (s *profilingServer) GetStreamStats(req *pspb.GetStreamStatsRequest, stream pspb.Profiling_GetStreamStatsServer) error {
+	grpclog.Infof("Processing stream request for stream stats")
 	results := profiling.StreamStats.Drain()
-	grpclog.Infof("stream stats size: %v records", len(results))
+	grpclog.Infof("Stream stats size: %v records", len(results))
 
 	for i := 0; i < len(results); i++ {
-		if err = stream.Send(ppb.StatToStatProto(results[i].(*profiling.Stat))); err != nil {
-			return
+		if err := stream.Send(ppb.StatToStatProto(results[i].(*profiling.Stat))); err != nil {
+			return err
 		}
 	}
 
-	return
+	return nil
 }
