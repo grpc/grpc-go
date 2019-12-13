544a917870ba268c62d5264668c7ce1f0f5bd5c3
Easwar Swaminathan <easwars@google.com>
2019-12-13 17:31:56 +0000

    profiling: Fix a data race in the circular buffer.
    Also fix linter errors.

--- internal/profiling/buffer/buffer_appengine.go
+++ internal/profiling/buffer/buffer_appengine.go
@@ -18,21 +18,26 @@
  *
  */
 
-// Appengine does not support stats because of lack of support for unsafe
+package buffer
+
+// CircularBuffer is a no-op implementation for appengine builds.
+//
+// Appengine does not support stats because of lack of the support for unsafe
 // pointers, which are necessary to efficiently store and retrieve things into
 // and from a circular buffer. As a result, Push does not do anything and Drain
 // returns an empty slice.
-package buffer
-
 type CircularBuffer struct{}
 
+// NewCircularBuffer returns a no-op for appengine builds.
 func NewCircularBuffer(size uint32) (*CircularBuffer, error) {
 	return nil, nil
 }
 
+// Push returns a no-op for appengine builds.
 func (cb *CircularBuffer) Push(x interface{}) {
 }
 
+// Drain returns a no-op for appengine builds.
 func (cb *CircularBuffer) Drain() []interface{} {
 	return nil
 }
