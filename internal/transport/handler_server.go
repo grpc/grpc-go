651a72e3e52f93b71e0abd9c4f812302f933a8d3
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2020-01-23 21:03:51 +0100

    travis force rebuild

--- internal/transport/handler_server.go
+++ internal/transport/handler_server.go
@@ -356,6 +356,7 @@ func (ht *serverHandlerTransport) HandleStreams(startStream func(*Stream), trace
 	if ht.stats != nil {
 		s.ctx = ht.stats.TagRPC(s.ctx, &stats.RPCTagInfo{FullMethodName: s.method})
 		inHeader := &stats.InHeader{
+			
 			FullMethod:  s.method,
 			RemoteAddr:  ht.RemoteAddr(),
 			Compression: s.recvCompress,
