9234785b05bc57b924c56025c045edaade57a532
Doug Fawley <dfawley@google.com>
2020-01-29 09:29:53 -0800

    update generated code

--- examples/route_guide/routeguide/route_guide.pb.go
+++ examples/route_guide/routeguide/route_guide.pb.go
@@ -342,11 +342,11 @@ var fileDescriptor_b7d679f20da65b7b = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // RouteGuideClient is the client API for RouteGuide service.
 //
@@ -379,10 +379,10 @@ type RouteGuideClient interface {
 }
 
 type routeGuideClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewRouteGuideClient(cc *grpc.ClientConn) RouteGuideClient {
+func NewRouteGuideClient(cc grpc.ClientConnInterface) RouteGuideClient {
 	return &routeGuideClient{cc}
 }
 
--- reflection/grpc_testing/test.pb.go
+++ reflection/grpc_testing/test.pb.go
@@ -186,11 +186,11 @@ var fileDescriptor_c161fcfdc0c3ff1e = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // SearchServiceClient is the client API for SearchService service.
 //
@@ -201,10 +201,10 @@ type SearchServiceClient interface {
 }
 
 type searchServiceClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewSearchServiceClient(cc *grpc.ClientConn) SearchServiceClient {
+func NewSearchServiceClient(cc grpc.ClientConnInterface) SearchServiceClient {
 	return &searchServiceClient{cc}
 }
 
