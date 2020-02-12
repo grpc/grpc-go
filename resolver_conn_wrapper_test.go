e38032e927812bb354297adcab933bedeff6c177
Easwar Swaminathan <easwars@google.com>
2020-02-11 17:51:29 -0800

    internal: Move parseTarget function into internal package and export it. (#3368)
    This will be used by RLS LB policy to validate targets specified in the
    service config.

--- resolver_conn_wrapper_test.go
+++ resolver_conn_wrapper_test.go
@@ -35,60 +35,6 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func (s) TestParseTarget(t *testing.T) {
-	for _, test := range []resolver.Target{
-		{Scheme: "dns", Authority: "", Endpoint: "google.com"},
-		{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com"},
-		{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com/?a=b"},
-		{Scheme: "passthrough", Authority: "", Endpoint: "/unix/socket/address"},
-	} {
-		str := test.Scheme + "://" + test.Authority + "/" + test.Endpoint
-		got := parseTarget(str)
-		if got != test {
-			t.Errorf("parseTarget(%q) = %+v, want %+v", str, got, test)
-		}
-	}
-}
-
-func (s) TestParseTargetString(t *testing.T) {
-	for _, test := range []struct {
-		targetStr string
-		want      resolver.Target
-	}{
-		{targetStr: "", want: resolver.Target{Scheme: "", Authority: "", Endpoint: ""}},
-		{targetStr: ":///", want: resolver.Target{Scheme: "", Authority: "", Endpoint: ""}},
-		{targetStr: "a:///", want: resolver.Target{Scheme: "a", Authority: "", Endpoint: ""}},
-		{targetStr: "://a/", want: resolver.Target{Scheme: "", Authority: "a", Endpoint: ""}},
-		{targetStr: ":///a", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a"}},
-		{targetStr: "a://b/", want: resolver.Target{Scheme: "a", Authority: "b", Endpoint: ""}},
-		{targetStr: "a:///b", want: resolver.Target{Scheme: "a", Authority: "", Endpoint: "b"}},
-		{targetStr: "://a/b", want: resolver.Target{Scheme: "", Authority: "a", Endpoint: "b"}},
-		{targetStr: "a://b/c", want: resolver.Target{Scheme: "a", Authority: "b", Endpoint: "c"}},
-		{targetStr: "dns:///google.com", want: resolver.Target{Scheme: "dns", Authority: "", Endpoint: "google.com"}},
-		{targetStr: "dns://a.server.com/google.com", want: resolver.Target{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com"}},
-		{targetStr: "dns://a.server.com/google.com/?a=b", want: resolver.Target{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com/?a=b"}},
-
-		{targetStr: "/", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "/"}},
-		{targetStr: "google.com", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com"}},
-		{targetStr: "google.com/?a=b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com/?a=b"}},
-		{targetStr: "/unix/socket/address", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "/unix/socket/address"}},
-
-		// If we can only parse part of the target.
-		{targetStr: "://", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "://"}},
-		{targetStr: "unix://domain", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "unix://domain"}},
-		{targetStr: "a:b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a:b"}},
-		{targetStr: "a/b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a/b"}},
-		{targetStr: "a:/b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a:/b"}},
-		{targetStr: "a//b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a//b"}},
-		{targetStr: "a://b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a://b"}},
-	} {
-		got := parseTarget(test.targetStr)
-		if got != test.want {
-			t.Errorf("parseTarget(%q) = %+v, want %+v", test.targetStr, got, test.want)
-		}
-	}
-}
-
 // The target string with unknown scheme should be kept unchanged and passed to
 // the dialer.
 func (s) TestDialParseTargetUnknownScheme(t *testing.T) {
