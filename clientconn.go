92dd426e0c668e23e24d2cc1f78ce8e896c4dc88
Doug Fawley <dfawley@google.com>
2020-02-12 15:41:45 -0800

    balancer/base: keep bad SubConns in TransientFailure until Ready (#3366)

--- clientconn.go
+++ clientconn.go
@@ -721,6 +721,7 @@ func (cc *ClientConn) handleSubConnStateChange(sc balancer.SubConn, s connectivi
 // Caller needs to make sure len(addrs) > 0.
 func (cc *ClientConn) newAddrConn(addrs []resolver.Address, opts balancer.NewSubConnOptions) (*addrConn, error) {
 	ac := &addrConn{
+		state:        connectivity.Idle,
 		cc:           cc,
 		addrs:        addrs,
 		scopts:       opts,
