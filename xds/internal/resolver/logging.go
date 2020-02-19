02b6aab388ab3fd474e32b8e1501aacb0aa38dc6
Menghan Li <menghanl@google.com>
2020-02-19 15:31:21 -0800

    [xds_logging_new] xds resolver logs

---
+++ xds/internal/resolver/logging.go
@@ -0,0 +1,47 @@
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package resolver
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc/grpclog"
+)
+
+const (
+	prefix = "[xds-resolver %p] "
+)
+
+func debugf(p *xdsResolver, format string, args ...interface{}) {
+	if grpclog.V(2) {
+		grpclog.Infof(fmt.Sprintf(prefix, p)+format, args...)
+	}
+}
+
+func infof(p *xdsResolver, format string, args ...interface{}) {
+	grpclog.Infof(fmt.Sprintf(prefix, p)+format, args...)
+}
+
+func warningf(p *xdsResolver, format string, args ...interface{}) {
+	grpclog.Warningf(fmt.Sprintf(prefix, p)+format, args...)
+}
+
+func errorf(p *xdsResolver, format string, args ...interface{}) {
+	grpclog.Errorf(fmt.Sprintf(prefix, p)+format, args...)
+}
