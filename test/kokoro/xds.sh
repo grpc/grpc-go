bf9e14004a0f3313c6ec0d1bc67244d51d8aa3bb
Eric Gribkoff <ericgribkoff@google.com>
2020-03-12 13:37:26 -0700

    kokoro: add xds job (#3448)

---
+++ test/kokoro/xds.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -exu -o pipefail
+[[ -f /VERSION ]] && cat /VERSION
+
+cd github
+
+export GOPATH="${HOME}/gopath"
+pushd grpc-go/interop/xds/client
+go build
+popd
+
+git clone https://github.com/grpc/grpc.git
+
+grpc/tools/run_tests/helper_scripts/prep_xds.sh
+GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
+  python3 grpc/tools/run_tests/run_xds_tests.py \
+    --test_case=all \
+    --project_id=grpc-testing \
+    --gcp_suffix=$(date '+%s') \
+    --verbose \
+    --client_cmd="grpc-go/interop/xds/client/client \
+      --server=xds-experimental:///{server_uri} \
+      --stats_port={stats_port} \
+      --qps={qps}"
