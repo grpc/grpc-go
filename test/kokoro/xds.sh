904cff184aad3a75af648c276705aa50e87cdda8
Eric Gribkoff <ericgribkoff@google.com>
2020-03-12 23:22:20 -0700

    test: checkout matching grpc/grpc branch for xds test

--- test/kokoro/xds.sh
+++ test/kokoro/xds.sh
@@ -7,10 +7,11 @@ cd github
 
 export GOPATH="${HOME}/gopath"
 pushd grpc-go/interop/xds/client
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 go build
 popd
 
-git clone https://github.com/grpc/grpc.git
+git clone -b "${BRANCH}" https://github.com/grpc/grpc.git
 
 grpc/tools/run_tests/helper_scripts/prep_xds.sh
 GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
