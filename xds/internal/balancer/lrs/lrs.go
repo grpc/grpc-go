fe1d8e71817f6ab365b9ca133f1b668cbe09a7d3
Menghan Li <menghanl@google.com>
2020-04-02 16:55:06 -0700

    xds: add temporary logging to LRS (#3490)

--- xds/internal/balancer/lrs/lrs.go
+++ xds/internal/balancer/lrs/lrs.go
@@ -313,9 +313,10 @@ func (ls *lrsStore) ReportTo(ctx context.Context, cc *grpc.ClientConn, clusterNa
 			grpclog.Warningf("lrs: failed to create stream: %v", err)
 			continue
 		}
-		if err := stream.Send(&lrspb.LoadStatsRequest{
-			Node: node,
-		}); err != nil {
+		grpclog.Infof("lrs: created LRS stream")
+		req := &lrspb.LoadStatsRequest{Node: node}
+		grpclog.Infof("lrs: sending init LoadStatsRequest: %v", req)
+		if err := stream.Send(req); err != nil {
 			grpclog.Warningf("lrs: failed to send first request: %v", err)
 			continue
 		}
@@ -324,6 +325,7 @@ func (ls *lrsStore) ReportTo(ctx context.Context, cc *grpc.ClientConn, clusterNa
 			grpclog.Warningf("lrs: failed to receive first response: %v", err)
 			continue
 		}
+		grpclog.Infof("lrs: received first LoadStatsResponse: %+v", first)
 		interval, err := ptypes.Duration(first.LoadReportingInterval)
 		if err != nil {
 			grpclog.Warningf("lrs: failed to convert report interval: %v", err)
@@ -366,9 +368,9 @@ func (ls *lrsStore) sendLoads(ctx context.Context, stream lrsgrpc.LoadReportingS
 		case <-ctx.Done():
 			return
 		}
-		if err := stream.Send(&lrspb.LoadStatsRequest{
-			ClusterStats: ls.buildStats(clusterName),
-		}); err != nil {
+		req := &lrspb.LoadStatsRequest{ClusterStats: ls.buildStats(clusterName)}
+		grpclog.Infof("lrs: sending LRS loads: %+v", req)
+		if err := stream.Send(req); err != nil {
 			grpclog.Warningf("lrs: failed to send report: %v", err)
 			return
 		}
