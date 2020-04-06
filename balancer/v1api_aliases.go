074f1d1bb45286bf444227563aad48efd17a696a
Sam Salisbury <samsalisbury@gmail.com>
2020-04-06 15:17:22 +0100

    add type aliases; honour go compatibility promise

---
+++ balancer/v1api_aliases.go
@@ -0,0 +1,7 @@
+package balancer
+
+// These type aliases should be removed when this project reaches
+// major version 2. They exist so we can stabilize the API on the
+// correct names, whilst preserving the Go compatibility promise.
+
+type PickOptions = PickInfo
---
+++ resolver/v1api_aliases.go
@@ -0,0 +1,8 @@
+package resolver
+
+// These type aliases should be removed when this project reaches
+// major version 2. They exist so we can stabilize the API on the
+// correct names, whilst preserving the Go compatibility promise.
+
+type BuildOption = BuildOptions
+type ResolveNowOption = ResolveNowOptions
