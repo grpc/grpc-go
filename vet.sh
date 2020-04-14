6801928517dfe66374114dd91ec3e7b99f5e40ce
Menghan Li <menghanl@google.com>
2020-04-13 18:25:59 -0700

    [lint_we_want_to_trust_you] [lint_we_trust_you] lint

--- vet.sh
+++ vet.sh
@@ -100,6 +100,8 @@ gofmt -s -d -l . 2>&1 | fail_on_output
 goimports -l . 2>&1 | not grep -vE "(_mock|\.pb)\.go"
 golint ./... 2>&1 | not grep -vE "(_mock|\.pb)\.go:"
 go vet -all ./...
+# - special golint on package comments.
+go list ./... | xargs -n 1 ./test/go_vet/lint_package_format.sh
 
 misspell -error .
 
