ca65f696f58fedc659937e948ad8bb416a59629d
Eric Gribkoff <ericgribkoff@google.com>
2020-04-24 10:10:24 -0700

    interop: add --fail_on_failed_rpc xds client flag

--- test/kokoro/xds.sh
+++ test/kokoro/xds.sh
@@ -30,4 +30,5 @@ GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
     --client_cmd="grpc-go/interop/xds/client/client \
       --server=xds-experimental:///{server_uri} \
       --stats_port={stats_port} \
-      --qps={qps}"
+      --qps={qps} \
+      {fail_on_failed_rpc}"
