6c9e30c09db2765b8ac80ad109077d95e07ccef1
Damien Neil <neild@users.noreply.github.com>
2020-05-27 14:15:25 -0700

    cmd/protoc-gen-go-grpc: add code generator (#3453)

---
+++ cmd/protoc-gen-go-grpc/main.go
@@ -0,0 +1,47 @@
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// protoc-gen-go-grpc is a plugin for the Google protocol buffer compiler to
+// generate Go code. Install it by building this program and making it
+// accessible within your PATH with the name:
+//	protoc-gen-go-grpc
+//
+// The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
+// such that it can be invoked as:
+//	protoc --go-grpc_out=. path/to/file.proto
+//
+// This generates Go service definitions for the protocol buffer defined by
+// file.proto.  With that input, the output will be written to:
+//	path/to/file_grpc.pb.go
+package main
+
+import (
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func main() {
+	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			generateFile(gen, f)
+		}
+		return nil
+	})
+}
