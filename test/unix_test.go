e57680869dfe801246c05f8e26f27bf2a28ebfec
gargut@google.com <gargut@google.com>
2020-07-08 11:30:46 -0700

    Fixed license

--- test/unix_test.go
+++ test/unix_test.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2020 Google LLC
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
