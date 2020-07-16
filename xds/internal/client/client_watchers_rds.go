6dc7938fe87560fdb44d6e8ba500758111ef5f37
Menghan Li <menghanl@google.com>
2020-07-16 16:38:30 -0700

    xdsrouting: balancer config parsing (#3734)

--- xds/internal/client/client_watchers_rds.go
+++ xds/internal/client/client_watchers_rds.go
@@ -22,6 +22,24 @@ import (
 	"time"
 )
 
+// Int64Range is a range for header range match.
+type Int64Range struct {
+	Start int64 `json:"start"`
+	End   int64 `json:"end"`
+}
+
+// HeaderMatcher represents header matchers.
+type HeaderMatcher struct {
+	Name         string      `json:"name"`
+	InvertMatch  *bool       `json:"invertMatch,omitempty"`
+	ExactMatch   *string     `json:"exactMatch,omitempty"`
+	RegexMatch   *string     `json:"regexMatch,omitempty"`
+	PrefixMatch  *string     `json:"prefixMatch,omitempty"`
+	SuffixMatch  *string     `json:"suffixMatch,omitempty"`
+	RangeMatch   *Int64Range `json:"rangeMatch,omitempty"`
+	PresentMatch *bool       `json:"presentMatch,omitempty"`
+}
+
 type rdsUpdate struct {
 	weightedCluster map[string]uint32
 }
