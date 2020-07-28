a372f4969508d142fe66562b869c60af468bf038
Menghan Li <menghanl@google.com>
2020-07-28 10:33:47 -0700

    xds testing: add path_matching and header_matching (#3764)

--- test/kokoro/xds.sh
+++ test/kokoro/xds.sh
@@ -19,11 +19,17 @@ popd
 git clone -b "${branch}" --single-branch --depth=1 https://github.com/grpc/grpc.git
 
 grpc/tools/run_tests/helper_scripts/prep_xds.sh
+
+# Test cases "path_matching" and "header_matching" are not included in "all",
+# because not all interop clients in all languages support these new tests.
+#
+# TODO: remove "path_matching" and "header_matching" from --test_case after
+# they are added into "all".
 GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
   python3 grpc/tools/run_tests/run_xds_tests.py \
-    --test_case=all \
+    --test_case="all,path_matching,header_matching" \
     --project_id=grpc-testing \
-    --source_image=projects/grpc-testing/global/images/xds-test-server \
+    --source_image=projects/grpc-testing/global/images/xds-test-server-2 \
     --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
     --gcp_suffix=$(date '+%s') \
     --verbose \
@@ -31,4 +37,7 @@ GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
       --server=xds:///{server_uri} \
       --stats_port={stats_port} \
       --qps={qps} \
-      {fail_on_failed_rpc}"
+      {fail_on_failed_rpc} \
+      {rpcs_to_send} \
+      {metadata_to_send}"
+
