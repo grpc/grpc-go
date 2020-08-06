17baa7cffc512b48f61aea9bd8d0c158ca410716
mlboy <mlboy@126.com>
2020-08-06 19:26:15 +0800

    add unit test for attributes

--- attributes/attributes_test.go
+++ attributes/attributes_test.go
@@ -23,7 +23,25 @@ import (
 
 	"google.golang.org/grpc/attributes"
 )
+// New ...
+func TestNew(t *testing.T) {
+	k1 := 1
+	v1 := "first"
+	attr := New(k1, v1)
 
+	ret1, ok1 := attr.Value(k1).(string)
+	if !ok1 || v1 != ret1 {
+		t.Fatalf("attributes.Value error: want:%v ret:%v", v1, ret1)
+	}
+
+	k2 := "2"
+	v2 := 2
+	attr = attr.WithValues(k2, v2)
+	ret2, ok2 := attr.Value(k2).(int)
+	if !ok2 || v2 != ret2 {
+		t.Fatalf("attributes.WithValues error: want:%v ret:%v", v2, ret2)
+	}
+}
 func ExampleAttributes() {
 	type keyOne struct{}
 	type keyTwo struct{}
