ff185609e7c21ab958133813c0f2ef4f715ca629
Danny Reidenbach <dreidenbach@google.com>
2020-08-10 22:32:34 +0000

    Added String to CheckedExpr

--- security/authorization/compiler/api/compiler.go
+++ security/authorization/compiler/api/compiler.go
@@ -65,24 +65,6 @@ func createUserPolicyCelEnv() *cel.Env {
 	return env
 }
 
-func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
-	ast, iss := env.Parse(expr)
-	// Report syntactic errors, if present.
-	if iss.Err() != nil {
-		return nil, iss.Err()
-	}
-	// Type-check the expression for correctness.
-	checked, iss := env.Check(ast)
-	if iss.Err() != nil {
-		return nil, iss.Err()
-	}
-	// Check the result type is a Boolean.
-	if !proto.Equal(checked.ResultType(), decls.Bool) {
-		return nil, iss.Err()
-	}
-	return checked, nil
-}
-
 // CompileYamltoRbac compiles yaml to rbac.
 func CompileYamltoRbac(filename string) (*pb.RBAC, error) {
 	yamlFile, err := ReadYaml(filename)
@@ -142,4 +124,49 @@ func Compile(inputFilename string, outputFilename string) error {
 	serialized, err := proto.Marshal(rbac)
 	ioutil.WriteFile(outputFilename, serialized, 0644)
 	return err
+}
+
+func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
+	ast, iss := env.Parse(expr)
+	// Report syntactic errors, if present.
+	if iss.Err() != nil {
+		return nil, iss.Err()
+	}
+	// Type-check the expression for correctness.
+	checked, iss := env.Check(ast)
+	if iss.Err() != nil {
+		return nil, iss.Err()
+	}
+	// Check the result type is a Boolean.
+	if !proto.Equal(checked.ResultType(), decls.Bool) {
+		return nil, iss.Err()
+	}
+	return checked, nil
+}
+
+func convertStringToCheckedExpr(expr string) (*expr.CheckedExpr, error) {
+	env, _ := cel.NewEnv(
+		cel.Declarations(
+			decls.NewIdent("request.url_path", decls.String, nil),
+			decls.NewIdent("request.host", decls.String, nil),
+			decls.NewIdent("request.method", decls.String, nil),
+			decls.NewIdent("request.headers", decls.NewMapType(decls.String, decls.String), nil),
+			decls.NewIdent("source.address", decls.String, nil),
+			decls.NewIdent("source.port", decls.Int, nil),
+			decls.NewIdent("source.principal", decls.String, nil),
+			decls.NewIdent("destination.address", decls.String, nil),
+			decls.NewIdent("destination.port", decls.Int, nil),
+			decls.NewIdent("connection.uri_san_peer_certificate", decls.String, nil)))
+
+	checked, err := compileCel(env, expr)
+	if err != nil {
+		return nil, err
+	}
+	checkedExpr, err := cel.AstToCheckedExpr(checked)
+	if err != nil {
+		return nil, err
+	}
+
+	return checkedExpr, nil
+
 }
