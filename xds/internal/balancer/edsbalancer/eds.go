d387948a6193b47ce06a40a582ddcc857bf9532e
Easwar Swaminathan <easwars@google.com>
2020-10-16 14:27:11 -0700

    Minor eds balancer changes.

--- xds/internal/balancer/edsbalancer/eds.go
+++ xds/internal/balancer/edsbalancer/eds.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 
 	"github.com/google/go-cmp/cmp"
+
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/roundrobin"
 	"google.golang.org/grpc/connectivity"
@@ -153,6 +154,7 @@ func (x *edsBalancer) run() {
 // In both cases, the sub-balancers will be closed, and the future picks will
 // fail.
 func (x *edsBalancer) handleErrorFromUpdate(err error, fromParent bool) {
+	x.logger.Warningf("Received error: %v", err)
 	if xdsclient.ErrType(err) == xdsclient.ErrorTypeResourceNotFound {
 		if fromParent {
 			// This is an error from the parent ClientConn (can be the parent
@@ -198,11 +200,10 @@ func (x *edsBalancer) handleGRPCUpdate(update interface{}) {
 
 		x.config = cfg
 	case error:
-		x.logger.Warningf("Received error from resolver: %v", u)
 		x.handleErrorFromUpdate(u, true)
 	default:
 		// unreachable path
-		panic("wrong update type")
+		x.logger.Errorf("wrong update type: %T", update)
 	}
 }
 
