90d23d53d5be6785267882cbe283461c27df071e
pxm286 <pradeep.mamillapalli@equifax.com>
2021-01-06 00:18:14 -0500

    fix: Updated v3 type for http connection manager

--- xds/internal/version/version.go
+++ xds/internal/version/version.go
@@ -45,7 +45,7 @@ const (
 	V3RouteConfigURL          = "type.googleapis.com/envoy.config.route.v3.RouteConfiguration"
 	V3ClusterURL              = "type.googleapis.com/envoy.config.cluster.v3.Cluster"
 	V3EndpointsURL            = "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment"
-	V3HTTPConnManagerURL      = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v3.HttpConnectionManager"
+	V3HTTPConnManagerURL      = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
 	V3UpstreamTLSContextURL   = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext"
 	V3DownstreamTLSContextURL = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext"
 )
