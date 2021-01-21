7f2581f910fc21497091c4109b56d310276fc943
ZHANG Dapeng <zdapeng@google.com>
2021-01-20 17:24:22 -0800

    xds interop: add xds v3 kokoro tests (#4165)

--- test/kokoro/xds.sh
+++ test/kokoro/xds.sh
@@ -29,10 +29,12 @@ GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
   python3 grpc/tools/run_tests/run_xds_tests.py \
     --test_case="all,path_matching,header_matching,circuit_breaking" \
     --project_id=grpc-testing \
+    --project_num=830293263384 \
     --source_image=projects/grpc-testing/global/images/xds-test-server-3 \
     --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
     --gcp_suffix=$(date '+%s') \
     --verbose \
+    ${XDS_V3_OPT-} \
     --client_cmd="grpc-go/interop/xds/client/client \
       --server=xds:///{server_uri} \
       --stats_port={stats_port} \
