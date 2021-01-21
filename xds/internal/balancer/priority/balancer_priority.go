c03a3ff4eaa09456d5ad15a93c0eb0e3db1692f2
Menghan Li <menghanl@google.com>
2021-01-21 15:14:51 -0800

    [priority_policy_balancer] copyright

--- xds/internal/balancer/priority/balancer_priority.go
+++ xds/internal/balancer/priority/balancer_priority.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package priority
