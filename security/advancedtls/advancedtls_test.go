509a81494c72a9f2314a1c4ef9efd5294bcb57fb
ZhenLian <zhenlian@google.com>
2021-03-27 18:04:57 -0700

    reset the code

--- security/advancedtls/advancedtls_test.go
+++ security/advancedtls/advancedtls_test.go
@@ -181,8 +181,8 @@ func (s) TestClientOptionsConfigSuccessCases(t *testing.T) {
 			}
 		})
 	}
-	ctx, _ := context.WithCancel(context.Background())
-	fmt.Println(ctx)
+	//ctx, _ := context.WithCancel(context.Background())
+	//fmt.Println(ctx)
 }
 
 func (s) TestServerOptionsConfigErrorCases(t *testing.T) {
