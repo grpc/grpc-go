9962409bb22e8deed7d299c5c48617eef32bc704
Zach Reyes <zasweq@google.com>
2021-05-17 16:54:09 -0400

    More general cleanup

--- internal/xds/rbac/matchers.go
+++ internal/xds/rbac/matchers.go
@@ -1,8 +1,23 @@
+/*
+ * Copyright 2021 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package rbac
 
 import (
 	"fmt"
-	matcher2 "google.golang.org/grpc/internal/xds/matcher"
 	"net"
 	"regexp"
 
@@ -10,6 +25,7 @@ import (
 	v3rbacpb "github.com/envoyproxy/go-control-plane/envoy/config/rbac/v3"
 	v3route_componentspb "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	v3matcherpb "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
+	matcher2 "google.golang.org/grpc/internal/xds/matcher"
 )
 
 // TODO: Do we need errors at every node? Should we have checks on each node (i.e. destinationPort should be within a certain range)
