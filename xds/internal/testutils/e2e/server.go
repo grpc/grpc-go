069bfd0f4dc8f6bcf97dfaad99d0d600201ab52b
Easwar Swaminathan <easwars@google.com>
2021-05-17 14:56:27 -0700

    use depth logging from the e2e package

--- xds/internal/testutils/e2e/server.go
+++ xds/internal/testutils/e2e/server.go
@@ -45,10 +45,22 @@ var logger = grpclog.Component("xds-e2e")
 // envoyproxy/go-control-plane/pkg/log. This is passed to the Snapshot cache.
 type serverLogger struct{}
 
-func (l serverLogger) Debugf(format string, args ...interface{}) { logger.Infof(format, args...) }
-func (l serverLogger) Infof(format string, args ...interface{})  { logger.Infof(format, args...) }
-func (l serverLogger) Warnf(format string, args ...interface{})  { logger.Warningf(format, args...) }
-func (l serverLogger) Errorf(format string, args ...interface{}) { logger.Errorf(format, args...) }
+func (l serverLogger) Debugf(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	logger.InfoDepth(1, msg)
+}
+func (l serverLogger) Infof(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	logger.InfoDepth(1, msg)
+}
+func (l serverLogger) Warnf(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	logger.WarningDepth(1, msg)
+}
+func (l serverLogger) Errorf(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	logger.ErrorDepth(1, msg)
+}
 
 // ManagementServer is a thin wrapper around the xDS control plane
 // implementation provided by envoyproxy/go-control-plane.
