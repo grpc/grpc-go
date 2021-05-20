78aeb8349cee442c2e7f3ad89c97dd80572c56b4
dkkb <dabkb@aol.com>
2021-05-20 17:41:23 +0800

    example: improve hello world server with starting msg

--- examples/helloworld/greeter_server/main.go
+++ examples/helloworld/greeter_server/main.go
@@ -50,6 +50,7 @@ func main() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
