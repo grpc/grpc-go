6201082d038984e7fe8ff033756cee486ff6679f
Zach Reyes <zasweq@google.com>
2021-06-30 11:47:56 -0400

    Finished helper function

--- internal/xds/rbac/rbac_engine.go
+++ internal/xds/rbac/rbac_engine.go
@@ -19,7 +19,12 @@
 package rbac
 
 import (
+	"context"
+	"crypto/x509"
+	"errors"
+	"google.golang.org/grpc/credentials"
 	"net"
+	"strconv"
 
 	v3rbacpb "github.com/envoyproxy/go-control-plane/envoy/config/rbac/v3"
 	"google.golang.org/grpc/metadata"
@@ -46,6 +51,92 @@ func NewEngine(policy *v3rbacpb.RBAC) (*Engine, error) {
 		policies[name] = matcher
 	}
 	return &Engine{policies: policies}, nil
+}
+
+// Helper functions here which put connection in and out of the context.
+// This is used for authorization interceptors, so do we put it here or in authorization package?
+type connectionKey struct{}
+
+func getConnection(ctx context.Context) net.Conn {
+	conn, _ := ctx.Value(connectionKey{}).(net.Conn)
+	return conn
+}
+
+// SetConnection adds the connection to the context to be able to get
+// information about the destination ip and port for an incoming RPC.
+func SetConnection(ctx context.Context, conn net.Conn) context.Context {
+	return context.WithValue(ctx, connectionKey{}, conn)
+}
+
+// NewRPCData takes a incoming context (should be a context representing state
+// needed for server RPC Call with headers and connection piped into it) and the
+// method name of the Service being called server side and populates an RPCData
+// struct ready to be passed to the RBAC Engine to find a matching policy.
+func NewRPCData(ctx context.Context, fullMethod string) (*RPCData, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("error retrieving metadata from incoming ctx")
+	}
+
+	pi, ok := peer.FromContext(ctx)
+	if !ok {
+		return nil, errors.New("error retrieving peer info from incoming ctx")
+	}
+
+	// You need the connection in order to find the destination address and port
+	// of the incoming RPC Call.
+	conn := getConnection(ctx)
+	_, dPort, err := net.SplitHostPort(conn.LocalAddr().String())
+	if err != nil {
+		return nil, err
+	}
+	dp, err := strconv.ParseUint(dPort, 10, 32)
+
+	tlsState := pi.AuthInfo.(credentials.TLSInfo).State // TODO: Handle errors on type conversion?
+	pName, err := findPrincipalNameFromCerts(tlsState.PeerCertificates)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RPCData{
+		MD: md,
+		PeerInfo: pi,
+		FullMethod: fullMethod,
+		DestinationPort: uint32(dp),
+		DestinationAddr: conn.LocalAddr(),
+		PrincipalName: pName,
+	}, nil
+}
+
+// findPrincipalNameFromCerts extracts the principal name from the TLS
+// Certificates according to the RBAC configuration logic - "The URI SAN or DNS
+// SAN in that order is used from the certificate, otherwise the subject field
+// is used."
+func findPrincipalNameFromCerts(certs []*x509.Certificate) (string, error) {
+	if len(certs) == 0 {
+		return "", errors.New("there are no certificates present")
+	}
+	// Loop through and find URI SAN if present.
+	for _, cert := range certs {
+		for _, uriSAN := range cert.URIs {
+			return uriSAN.String(), nil
+		}
+	}
+
+	// Loop through and find DNS SAN if present.
+	for _, cert := range certs {
+		for _, dnsSAN := range cert.DNSNames {
+			return dnsSAN, nil
+		}
+	}
+
+	// If neither URI SAN or DNS SAN were present in the certificates, we can
+	// just return the subject field.
+	for _, cert := range certs {
+		return cert.Subject.String(), nil
+	}
+
+	return "", errors.New("the URI SAN, DNS SAN, or subject field was not found in the certificates")
 }
 
 // RPCData wraps data pulled from an incoming RPC that the RBAC engine needs to
