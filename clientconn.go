217e45b7b8881d86b0e83caecae2cf5e3ae01fa8
Easwar Swaminathan <easwars@google.com>
2021-09-10 08:47:21 -0700

    grpc: close underlying transport when subConn is closed when in connecting state

--- clientconn.go
+++ clientconn.go
@@ -1322,7 +1322,7 @@ func (ac *addrConn) createTransport(addr resolver.Address, copts transport.Conne
 	}
 
 	select {
-	case <-time.After(time.Until(connectDeadline)):
+	case <-connectCtx.Done():
 		// We didn't get the preface in time.
 		err := fmt.Errorf("failed to receive server preface within timeout")
 		newTr.Close(err)
@@ -1331,19 +1331,32 @@ func (ac *addrConn) createTransport(addr resolver.Address, copts transport.Conne
 	case <-prefaceReceived.Done():
 		// We got the preface - huzzah! things are good.
 		ac.mu.Lock()
-		defer ac.mu.Unlock()
-		defer prefaceReceived.Fire()
 		if connClosed.HasFired() {
 			// onClose called first; go idle but do nothing else.
 			if ac.state != connectivity.Shutdown {
 				ac.updateConnectivityState(connectivity.Idle, nil)
 			}
+			ac.mu.Unlock()
 			return nil
+		}
+		if ac.state == connectivity.Shutdown {
+			// This can happen if the subConn was removed while in `Connecting`
+			// state. tearDown() would have set the state to `Shutdown`, but
+			// would not have closed the transport since ac.transport would not
+			// been set at that point.
+			err := fmt.Errorf("subConn closed before receiving preface")
+			// We have to unlock here because newTr.Close() calls onClose()
+			// inline, which requires locking ac.mu.
+			ac.mu.Unlock()
+			newTr.Close(err)
+			channelz.Warningf(logger, ac.channelzID, "grpc: addrConn.createTransport failed to connect to %v: %v", addr, err)
+			return err
 		}
 		ac.curAddr = addr
 		ac.transport = newTr
 		hcStarted = true
 		ac.startHealthCheck(hctx) // Will set state to READY if appropriate.
+		ac.mu.Unlock()
 		return nil
 	case <-connClosed.Done():
 		// The transport has already closed.  If we received the preface, too,
