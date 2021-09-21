96cccc57237f230f949e4a9c2f89acf166e8c6fa
Doug Fawley <dfawley@google.com>
2021-09-21 10:24:40 -0700

    Revert "transport/server: add :method POST to incoming metadata (#4770)"
    This reverts commit c84a5de06496bf8416cebf9d0058f481e37c165e.

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -380,7 +380,6 @@ func (t *http2Server) operateHeaders(frame *http2.MetaHeadersFrame, handle func(
 			s.recvCompress = hf.Value
 		case ":method":
 			httpMethod = hf.Value
-			mdata[":method"] = append(mdata[":method"], hf.Value)
 		case ":path":
 			s.method = hf.Value
 		case "grpc-timeout":
