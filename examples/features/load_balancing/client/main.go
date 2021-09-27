cee1b4442c7464b613a74b4e138196a64f1ed0f6
Easwar Swaminathan <easwars@google.com>
2021-09-27 08:46:10 -0700

    fix resolver in load_balancing example

--- examples/features/load_balancing/client/main.go
+++ examples/features/load_balancing/client/main.go
@@ -23,6 +23,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"google.golang.org/grpc"
@@ -93,6 +94,7 @@ func main() {
 type exampleResolverBuilder struct{}
 
 func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	target.Endpoint = strings.TrimPrefix(target.Endpoint, "/")
 	r := &exampleResolver{
 		target: target,
 		cc:     cc,
