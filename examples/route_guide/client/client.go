c562f5980d10aa6e598468b0310a3fbe6b3eedf3
Yuan Tang <terrytangyuan@gmail.com>
2021-11-12 15:51:11 -0500

    Update client.go

--- examples/route_guide/client/client.go
+++ examples/route_guide/client/client.go
@@ -39,7 +39,7 @@ import (
 var (
 	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
-	serverAddr         = flag.String("addr", "localhost:10000", "The server address in the format of host:port")
+	serverAddr         = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
 	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
 )
 
