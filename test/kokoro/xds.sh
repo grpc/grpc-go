228443d5ad6300408a1ee7315f771e94e80710f9
Menghan Li <menghanl@google.com>
2021-11-30 15:04:06 -0800

    [xds_sh] drop v for tar

--- test/kokoro/xds.sh
+++ test/kokoro/xds.sh
@@ -16,7 +16,7 @@ shopt -u extglob
 # Install a version of Go supported by gRPC for the new features, e.g.
 # errors.Is()
 curl --retry 3 -O -L https://go.dev/dl/go1.17.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvf go1.17.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xf go1.17.3.linux-amd64.tar.gz
 sudo ln -s /usr/local/go/bin/go /usr/bin/go
 # Retry go build on errors (e.g. go get connection errors), for at most 3 times
 for i in 1 2 3; do go build && break || sleep 5; done
