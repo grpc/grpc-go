eb1b2e8e94e9dc5774b68ee965cfab77172e06a8
mohanli-ml <mohanli@google.com>
2022-01-12 19:37:05 +0000

    xds: update RPC timeout in blackhole case

--- interop/grpclb_fallback/client_linux.go
+++ interop/grpclb_fallback/client_linux.go
@@ -132,7 +132,7 @@ func waitForFallbackAndDoRPCs(client testgrpc.TestServiceClient, fallbackDeadlin
 	fallbackRetryCount := 0
 	fellBack := false
 	for time.Now().Before(fallbackDeadline) {
-		g := doRPCAndGetPath(client, 1*time.Second)
+		g := doRPCAndGetPath(client, 20*time.Second)
 		if g == testpb.GrpclbRouteType_GRPCLB_ROUTE_TYPE_FALLBACK {
 			infoLog.Println("Made one successul RPC to a fallback. Now expect the same for the rest.")
 			fellBack = true
@@ -166,7 +166,7 @@ func doFastFallbackBeforeStartup() {
 
 func doSlowFallbackBeforeStartup() {
 	runCmd(*blackholeLBAndBackendAddrsCmd)
-	fallbackDeadline := time.Now().Add(20 * time.Second)
+	fallbackDeadline := time.Now().Add(60 * time.Second)
 	conn := createTestConn()
 	defer conn.Close()
 	client := testgrpc.NewTestServiceClient(conn)
