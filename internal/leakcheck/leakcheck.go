cb3c46e2b05d564ddfe7313b8d62d7d2fa2ed859
Menghan Li <menghanl@google.com>
2022-02-01 10:21:04 -0800

    [leak_ignore] ignore once

--- internal/leakcheck/leakcheck.go
+++ internal/leakcheck/leakcheck.go
@@ -43,7 +43,7 @@ var goroutinesToIgnore = []string{
 	"(*loggingT).flushDaemon",
 	"goroutine in C code",
 	// Ignore the http read/write goroutines. gce metadata.OnGCE() was leaking
-	// these, root cause unknown, ignore.
+	// these, root cause unknown.
 	//
 	// https://github.com/grpc/grpc-go/issues/5171
 	// https://github.com/grpc/grpc-go/issues/5173
