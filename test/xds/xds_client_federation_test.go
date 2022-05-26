e41f868588c99493b35169e258a11b2ce128e139
Easwar Swaminathan <easwars@google.com>
2022-05-25 17:03:52 -0700

    test/xds: move tests to a package with _test suffix (#5382)

--- test/xds/xds_client_federation_test.go
+++ test/xds/xds_client_federation_test.go
@@ -13,9 +13,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package xds
+package xds_test
 
 import (
 	"context"
