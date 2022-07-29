642189b66953a7e2c64a933b35aaa0725a597bf1
Josh Dolitsky <josh@dolit.ski>
2022-07-29 14:47:04 -0500

    Switch from glog to klog
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/cncf/udpa/go v0.0.0-20210930031921-04548b0d99d4
 	github.com/cncf/xds/go v0.0.0-20211011173535-cb28da3451f1
 	github.com/envoyproxy/go-control-plane v0.10.2-0.20220325020618-49ff273808a1
-	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.5.2
 	github.com/google/go-cmp v0.5.6
 	github.com/google/uuid v1.1.2
@@ -16,12 +15,14 @@ require (
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/protobuf v1.27.1
+	k8s.io/klog/v2 v2.70.1
 )
 
 require (
 	cloud.google.com/go v0.34.0 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.2.1 // indirect
 	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
 	golang.org/x/text v0.3.3 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/appengine v1.4.0 // indirect
--- grpclog/glogger/glogger.go
+++ grpclog/glogger/glogger.go
@@ -23,8 +23,8 @@ package glogger
 import (
 	"fmt"
 
-	"github.com/golang/glog"
 	"google.golang.org/grpc/grpclog"
+	"k8s.io/klog/v2"
 )
 
 const d = 2
@@ -36,69 +36,69 @@ func init() {
 type glogger struct{}
 
 func (g *glogger) Info(args ...interface{}) {
-	glog.InfoDepth(d, args...)
+	klog.InfoDepth(d, args...)
 }
 
 func (g *glogger) Infoln(args ...interface{}) {
-	glog.InfoDepth(d, fmt.Sprintln(args...))
+	klog.InfoDepth(d, fmt.Sprintln(args...))
 }
 
 func (g *glogger) Infof(format string, args ...interface{}) {
-	glog.InfoDepth(d, fmt.Sprintf(format, args...))
+	klog.InfoDepth(d, fmt.Sprintf(format, args...))
 }
 
 func (g *glogger) InfoDepth(depth int, args ...interface{}) {
-	glog.InfoDepth(depth+d, args...)
+	klog.InfoDepth(depth+d, args...)
 }
 
 func (g *glogger) Warning(args ...interface{}) {
-	glog.WarningDepth(d, args...)
+	klog.WarningDepth(d, args...)
 }
 
 func (g *glogger) Warningln(args ...interface{}) {
-	glog.WarningDepth(d, fmt.Sprintln(args...))
+	klog.WarningDepth(d, fmt.Sprintln(args...))
 }
 
 func (g *glogger) Warningf(format string, args ...interface{}) {
-	glog.WarningDepth(d, fmt.Sprintf(format, args...))
+	klog.WarningDepth(d, fmt.Sprintf(format, args...))
 }
 
 func (g *glogger) WarningDepth(depth int, args ...interface{}) {
-	glog.WarningDepth(depth+d, args...)
+	klog.WarningDepth(depth+d, args...)
 }
 
 func (g *glogger) Error(args ...interface{}) {
-	glog.ErrorDepth(d, args...)
+	klog.ErrorDepth(d, args...)
 }
 
 func (g *glogger) Errorln(args ...interface{}) {
-	glog.ErrorDepth(d, fmt.Sprintln(args...))
+	klog.ErrorDepth(d, fmt.Sprintln(args...))
 }
 
 func (g *glogger) Errorf(format string, args ...interface{}) {
-	glog.ErrorDepth(d, fmt.Sprintf(format, args...))
+	klog.ErrorDepth(d, fmt.Sprintf(format, args...))
 }
 
 func (g *glogger) ErrorDepth(depth int, args ...interface{}) {
-	glog.ErrorDepth(depth+d, args...)
+	klog.ErrorDepth(depth+d, args...)
 }
 
 func (g *glogger) Fatal(args ...interface{}) {
-	glog.FatalDepth(d, args...)
+	klog.FatalDepth(d, args...)
 }
 
 func (g *glogger) Fatalln(args ...interface{}) {
-	glog.FatalDepth(d, fmt.Sprintln(args...))
+	klog.FatalDepth(d, fmt.Sprintln(args...))
 }
 
 func (g *glogger) Fatalf(format string, args ...interface{}) {
-	glog.FatalDepth(d, fmt.Sprintf(format, args...))
+	klog.FatalDepth(d, fmt.Sprintf(format, args...))
 }
 
 func (g *glogger) FatalDepth(depth int, args ...interface{}) {
-	glog.FatalDepth(depth+d, args...)
+	klog.FatalDepth(depth+d, args...)
 }
 
 func (g *glogger) V(l int) bool {
-	return bool(glog.V(glog.Level(l)))
+	return bool(klog.V(klog.Level(l)).Enabled())
 }
