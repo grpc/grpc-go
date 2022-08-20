22290f3678edb763a6bb0eec66d8e4c83d66459d
Ronak Jain <ronakj@uber.com>
2022-08-20 17:05:02 +0530

    Fix http2 spec document link

--- internal/transport/http_util.go
+++ internal/transport/http_util.go
@@ -45,7 +45,7 @@ import (
 const (
 	// http2MaxFrameLen specifies the max length of a HTTP2 frame.
 	http2MaxFrameLen = 16384 // 16KB frame
-	// http://http2.github.io/http2-spec/#SettingValues
+	// https://httpwg.org/specs/rfc7540.html#SettingValues
 	http2InitHeaderTableSize = 4096
 )
 
