689d061d46c76f52f6a965ef621f988f15655200
littlejian <17816869670@163.com>
2022-11-10 15:06:01 +0800

    Cleanup usages of resolver.Target's Scheme and Authority (#5761)

--- internal/resolver/dns/dns_resolver_test.go
+++ internal/resolver/dns/dns_resolver_test.go
@@ -23,6 +23,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/url"
 	"os"
 	"reflect"
 	"strings"
@@ -1439,7 +1440,12 @@ func TestCustomAuthority(t *testing.T) {
 
 		b := NewBuilder()
 		cc := &testClientConn{target: "foo.bar.com", errChan: make(chan error, 1)}
-		r, err := b.Build(resolver.Target{Endpoint: "foo.bar.com", Authority: a.authority}, cc, resolver.BuildOptions{})
+		target := resolver.Target{
+			Endpoint:  "foo.bar.com",
+			Authority: a.authority,
+			URL:       url.URL{Host: a.authority},
+		}
+		r, err := b.Build(target, cc, resolver.BuildOptions{})
 
 		if err == nil {
 			r.Close()
