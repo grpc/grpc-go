40b6b43ddd597f13798b06c9a3d33fff3830e225
Theo Salvo <buzzsurfr@users.noreply.github.com>
2022-12-31 15:01:19 -0500

    Replace ioutil.Discard with io.Discard

--- rpc_util.go
+++ rpc_util.go
@@ -77,7 +77,7 @@ func NewGZIPCompressorWithLevel(level int) (Compressor, error) {
 	return &gzipCompressor{
 		pool: sync.Pool{
 			New: func() interface{} {
-				w, err := gzip.NewWriterLevel(ioutil.Discard, level)
+				w, err := gzip.NewWriterLevel(io.Discard, level)
 				if err != nil {
 					panic(err)
 				}
