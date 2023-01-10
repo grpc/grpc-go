5df4d0fb1fc192cb1e3a899e299c6ed12a2db218
Arvind Bright <arvind.bright100@gmail.com>
2023-01-09 16:39:58 -0800

    address comments

--- test/end2end_test.go
+++ test/end2end_test.go
@@ -34,7 +34,6 @@ import (
 	"os"
 	"reflect"
 	"runtime"
-	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -6885,7 +6884,7 @@ func (s *httpServer) start(t *testing.T, lis net.Listener) {
 // to the test as part of the Peer call option.
 type authInfoWithConn struct {
 	credentials.CommonAuthInfo
-	connection net.Conn
+	conn net.Conn
 }
 
 func (ai *authInfoWithConn) AuthType() string {
@@ -6895,17 +6894,22 @@ func (ai *authInfoWithConn) AuthType() string {
 // connWrapperWithCloseCh wraps a net.Conn and pushes on a channel when closed.
 type connWrapperWithCloseCh struct {
 	net.Conn
-	closeCh *testutils.Channel
+	closeCh chan interface{}
 }
 
 // Close closes the connection and sends a value on the close channel.
 func (cw *connWrapperWithCloseCh) Close() error {
 	err := cw.Conn.Close()
-	cw.closeCh.Replace(nil)
-	return err
+	for {
+		select {
+		case cw.closeCh <- nil:
+			return err
+		case <-cw.closeCh:
+		}
+	}
 }
 
-// This custom creds are used for storing the connections made by the client.
+// These custom creds are used for storing the connections made by the client.
 // The closeCh in conn can be used to detect when conn is closed.
 type transportRestartCheckCreds struct {
 	mu          sync.Mutex
@@ -6918,9 +6922,9 @@ func (c *transportRestartCheckCreds) ServerHandshake(rawConn net.Conn) (net.Conn
 func (c *transportRestartCheckCreds) ClientHandshake(ctx context.Context, authority string, rawConn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	conn := &connWrapperWithCloseCh{Conn: rawConn, closeCh: testutils.NewChannel()}
+	conn := &connWrapperWithCloseCh{Conn: rawConn, closeCh: make(chan interface{}, 1)}
 	c.connections = append(c.connections, *conn)
-	authInfo := &authInfoWithConn{CommonAuthInfo: credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}, connection: conn}
+	authInfo := &authInfoWithConn{CommonAuthInfo: credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}, conn: conn}
 	return conn, authInfo, nil
 }
 func (c *transportRestartCheckCreds) Info() credentials.ProtocolInfo {
@@ -6930,23 +6934,6 @@ func (c *transportRestartCheckCreds) Clone() credentials.TransportCredentials {
 	return &transportRestartCheckCreds{}
 }
 func (c *transportRestartCheckCreds) OverrideServerName(s string) error {
-	return nil
-}
-
-type streamWithPeer struct {
-	stream testpb.TestService_FullDuplexCallClient
-	peer   *peer.Peer
-	id     string
-	conn   net.Conn
-}
-
-func (s *streamWithPeer) sendAndReceive() error {
-	if err := s.stream.Send(&testpb.StreamingOutputCallRequest{}); err != nil {
-		return fmt.Errorf("sending on %s: %v", s.id, err)
-	}
-	if _, err := s.stream.Recv(); err != nil {
-		return fmt.Errorf("receiving on %s: %v", s.id, err)
-	}
 	return nil
 }
 
@@ -6963,18 +6950,18 @@ func (s) TestClientTransportRestartsAfterStreamIDExhausted(t *testing.T) {
 
 	ss := &stubserver.StubServer{
 		FullDuplexCallF: func(stream testpb.TestService_FullDuplexCallServer) error {
-			for {
-				_, err := stream.Recv()
-				if err == io.EOF {
-					return nil
-				}
-				if err != nil {
-					return err
+			for i := 0; i < 2; i++ {
+				if _, err := stream.Recv(); err != nil {
+					return status.Errorf(codes.Internal, "unexpected error receiving: %v", err)
 				}
 				if err := stream.Send(&testpb.StreamingOutputCallResponse{}); err != nil {
-					return err
+					return status.Errorf(codes.Internal, "unexpected error sending: %v", err)
 				}
 			}
+			if recv, err := stream.Recv(); err != io.EOF {
+				return status.Errorf(codes.Internal, "Recv = %v, %v; want _, io.EOF", recv, err)
+			}
+			return nil
 		},
 	}
 
@@ -6987,64 +6974,70 @@ func (s) TestClientTransportRestartsAfterStreamIDExhausted(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
 
-	var streams []streamWithPeer
+	var streams []testpb.TestService_FullDuplexCallClient
 
-	// Setting up 3 streams and calling Send() and Recv() once on each.
+	// expectedNumConns when each stream is created.
+	expectedNumConns := []int{1, 1, 2}
+
+	// Set up 3 streams and call sendAndReceive() once on each.
 	for i := 0; i < 3; i++ {
-		p := new(peer.Peer)
-		s, err := ss.Client.FullDuplexCall(ctx, grpc.Peer(p))
+		s, err := ss.Client.FullDuplexCall(ctx)
 		if err != nil {
 			t.Fatalf("Creating FullDuplex stream: %v", err)
 		}
-		sp := streamWithPeer{stream: s, peer: p, id: "stream " + strconv.Itoa(i)}
-		streams = append(streams, sp)
 
-		if err := sp.sendAndReceive(); err != nil {
-			t.Fatal(err)
+		streams = append(streams, s)
+		if err := s.Send(&testpb.StreamingOutputCallRequest{}); err != nil {
+			t.Fatalf("Sending on stream %d: %v", i, err)
+		}
+		if _, err := s.Recv(); err != nil {
+			t.Fatalf("Receiving on stream %d: %v", i, err)
+		}
+
+		// Verify expected num of conns.
+		if len(creds.connections) != expectedNumConns[i] {
+			t.Fatalf("Number of connections created: %v, want: %v", len(creds.connections), expectedNumConns[i])
 		}
 	}
-	// Verifying only 2 connections are created so far.
-	if len(creds.connections) != 2 {
-		t.Fatalf("Number of connections created: %v, want: 2", len(creds.connections))
+
+	// Verify all streams still work.
+	for i, stream := range streams {
+		if err := stream.Send(&testpb.StreamingOutputCallRequest{}); err != nil {
+			t.Fatalf("Sending on stream %d: %v", i, err)
+		}
+		if _, err := stream.Recv(); err != nil {
+			t.Fatalf("Receiving on stream %d: %v", i, err)
+		}
 	}
 
-	// Verifying that streams on the first conn still works.
-	if err := streams[0].sendAndReceive(); err != nil {
-		t.Fatal(err)
-	}
-	if err := streams[1].sendAndReceive(); err != nil {
-		t.Fatal(err)
-	}
+	var connPerStream []net.Conn
 
-	// The peer passed via the call option is set up only after the RPC is complete,
-	// which is why we wait here until Recv() returns EOF.
-	for i := 0; i < 3; i++ {
-		if err := streams[i].stream.CloseSend(); err != nil {
-			t.Fatalf("CloseSend() on %s: %v", streams[i].id, err)
+	// The peer passed via the call option is set up only after the RPC is complete.
+	// Conn used by the stream is available in authInfo.
+	for i, stream := range streams {
+		if err := stream.CloseSend(); err != nil {
+			t.Fatalf("CloseSend() on stream %d: %v", i, err)
 		}
-		for {
-			_, err := streams[i].stream.Recv()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				t.Fatalf("Receiving on %s got: %v, want: EOF", streams[i].id, err)
-			}
+		p, ok := peer.FromContext(stream.Context())
+		if !ok {
+			t.Fatalf("Getting peer from stream context for stream %d", i)
 		}
-		streams[i].conn = streams[i].peer.AuthInfo.(*authInfoWithConn).connection
+		connPerStream = append(connPerStream, p.AuthInfo.(*authInfoWithConn).conn)
 	}
 
 	// Verifying the first and second RPCs were made on the same connection.
-	if streams[0].conn != streams[1].conn {
+	if connPerStream[0] != connPerStream[1] {
 		t.Fatal("Got streams using different connections; want same.")
 	}
 	// Verifying the third and first/second RPCs were made on different connections.
-	if streams[2].conn == streams[0].conn {
+	if connPerStream[2] == connPerStream[0] {
 		t.Fatal("Got streams using same connections; want different.")
 	}
 
 	// Verifying first connection was closed.
-	if _, err := creds.connections[0].closeCh.Receive(ctx); err != nil {
+	select {
+	case <-creds.connections[0].closeCh:
+	case <-ctx.Done():
 		t.Fatal("Timeout expired when waiting for first client transport to close")
 	}
 }
