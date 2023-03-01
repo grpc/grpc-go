d203b0dce74944ab9852fd1c1ea20f0769149f26
Arvind Bright <arvind.bright100@gmail.com>
2023-03-01 09:23:08 -0800

    vet vet

--- credentials/alts/internal/proto/grpc_gcp/altscontext.pb.go
+++ credentials/alts/internal/proto/grpc_gcp/altscontext.pb.go
@@ -24,11 +24,10 @@
 package grpc_gcp
 
 import (
-	reflect "reflect"
-	sync "sync"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
