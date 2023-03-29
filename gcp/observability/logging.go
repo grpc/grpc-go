335eeb47bf1d1e21af2dc31d877e1a746f3b9902
Zach Reyes <zasweq@google.com>
2023-03-28 20:43:12 -0400

    Add isSampled bool to log entries

--- gcp/observability/logging.go
+++ gcp/observability/logging.go
@@ -329,12 +329,14 @@ func (bml *binaryMethodLogger) buildGCPLoggingEntry(ctx context.Context, c iblog
 			sc := span.SpanContext()
 			gcploggingEntry.Trace = "projects/" + bml.projectID + "/traces/" + sc.TraceID.String()
 			gcploggingEntry.SpanID = sc.SpanID.String()
+			gcploggingEntry.TraceSampled = sc.IsSampled()
 		}
 	} else {
 		// server side span, populated through stats/opencensus package.
-		if tID, sID, ok := opencensus.GetTraceAndSpanID(ctx); ok {
-			gcploggingEntry.Trace = "projects/" + bml.projectID + "/traces/" + tID.String()
-			gcploggingEntry.SpanID = sID.String()
+		if sc, ok := opencensus.SpanContextFromContext(ctx); ok {
+			gcploggingEntry.Trace = "projects/" + bml.projectID + "/traces/" + sc.TraceID.String()
+			gcploggingEntry.SpanID = sc.SpanID.String()
+			gcploggingEntry.TraceSampled = sc.IsSampled()
 		}
 	}
 	return gcploggingEntry
