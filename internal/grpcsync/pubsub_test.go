caf61f18166bb4ef0e7c7936c22cf454070f6006
my4-dev <miyoshi.exjge@gmail.com>
2023-04-15 16:31:07 +0900

    fix test style

--- internal/grpcsync/pubsub_test.go
+++ internal/grpcsync/pubsub_test.go
@@ -32,6 +32,10 @@ type testSubscriber struct {
 	onMsgCh chan struct{}
 }
 
+func newTestSubscriber() *testSubscriber {
+	return &testSubscriber{onMsgCh: make(chan struct{})}
+}
+
 func (ts *testSubscriber) OnMessage(msg interface{}) {
 	ts.mu.Lock()
 	defer ts.mu.Unlock()
@@ -39,15 +43,18 @@ func (ts *testSubscriber) OnMessage(msg interface{}) {
 	ts.onMsgCh <- struct{}{}
 }
 
+func (ts *testSubscriber) receivedMsgs() []int {
+	ts.mu.Lock()
+	defer ts.mu.Unlock()
+	return ts.msgs
+}
+
 func (s) TestPubSub_PublishNoMsg(t *testing.T) {
 	// Create a new pubsub.
 	pubsub := NewPubSub()
 	defer pubsub.Stop()
 
-	ts := &testSubscriber{
-		msgs:    []int{},
-		onMsgCh: make(chan struct{}),
-	}
+	ts := newTestSubscriber()
 	pubsub.Subscribe(ts)
 
 	select {
@@ -57,80 +64,122 @@ func (s) TestPubSub_PublishNoMsg(t *testing.T) {
 	}
 }
 
-func (s) TestPubSub_PublishOneMsg(t *testing.T) {
-	// Create a new pubsub.
-	pubsub := NewPubSub()
-	defer pubsub.Stop()
-
-	ts := &testSubscriber{
-		msgs:    []int{},
-		onMsgCh: make(chan struct{}),
-	}
-	pubsub.Subscribe(ts)
-
-	pubsub.Publish(1)
-
-	expectedMsg := []int{1}
-	select {
-	case <-ts.onMsgCh:
-		if diff := cmp.Diff(ts.msgs, expectedMsg); diff != "" {
-			t.Errorf("Difference between ts.msgs and expectedMsg. diff(-want, +got):\n%s", diff)
-		}
-	case <-time.After(defaultTestShortTimeout):
-		t.Fatalf("The callback was invoked within defaultTestShortTimeout")
-	}
-}
-
-func (s) TestPubSub_PublishMultiMsgs_And_Stop(t *testing.T) {
+func (s) TestPutSub_PublishMsgs_RegisterSubs_And_Stop(t *testing.T) {
+	// This flag becomes true when waiting callback is timed out.
+	isTimeout := false
 	// Create a new pubsub.
 	pubsub := NewPubSub()
 
-	ts := &testSubscriber{
-		msgs:    []int{},
-		onMsgCh: make(chan struct{}),
-	}
-	pubsub.Subscribe(ts)
+	ts := newTestSubscriber()
 
-	expectedMsg := []int{}
+	pubsub.Subscribe(ts)
+	wantMsgs := []int{}
 
 	const numPublished = 10
-	for i := 0; i < numPublished; i++ {
-		pubsub.Publish(i)
-		expectedMsg = append(expectedMsg, i)
-		select {
-		case <-ts.onMsgCh:
-			if diff := cmp.Diff(ts.msgs, expectedMsg); diff != "" {
-				t.Errorf("Difference between ts.msgs and expectedMsg. diff(-want, +got):\n%s", diff)
-			}
-		case <-time.After(defaultTestShortTimeout):
-			t.Fatalf("The callback was invoked within defaultTestShortTimeout")
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		for i := 0; i < numPublished; i++ {
+			pubsub.Publish(i)
 		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < numPublished; i++ {
+			select {
+			case <-ts.onMsgCh:
+				wantMsgs = append(wantMsgs, i)
+			case <-time.After(defaultTestShortTimeout):
+				isTimeout = true
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	if isTimeout {
+		t.Fatalf("Timeout when expecting the onMessage() callback to be invoked")
+	}
+	if gotMsgs := ts.receivedMsgs(); !cmp.Equal(gotMsgs, wantMsgs) {
+		t.Fatalf("Received messages is %v, want %v", gotMsgs, wantMsgs)
+	}
+
+	ts2 := newTestSubscriber()
+	pubsub.Subscribe(ts2)
+	wantMsgs2 := []int{}
+
+	select {
+	case <-ts2.onMsgCh:
+		wantMsgs2 = append(wantMsgs2, numPublished-1)
+	case <-time.After(defaultTestShortTimeout):
+		t.Fatalf("Timeout when expecting the onMessage() callback to be invoked")
+	}
+	if gotMsgs2 := ts2.receivedMsgs(); !cmp.Equal(gotMsgs2, wantMsgs2) {
+		t.Fatalf("Received messages is %v, want %v", gotMsgs2, wantMsgs2)
+	}
+
+	var wg2 sync.WaitGroup
+	wg2.Add(3)
+	go func() {
+		for i := 0; i < numPublished; i++ {
+			pubsub.Publish(i)
+		}
+		wg2.Done()
+	}()
+	go func() {
+		for i := 0; i < numPublished; i++ {
+			select {
+			case <-ts.onMsgCh:
+				wantMsgs = append(wantMsgs, i)
+			case <-time.After(defaultTestShortTimeout):
+				isTimeout = true
+			}
+		}
+		wg2.Done()
+	}()
+	go func() {
+		for i := 0; i < numPublished; i++ {
+			select {
+			case <-ts2.onMsgCh:
+				wantMsgs2 = append(wantMsgs2, i)
+			case <-time.After(defaultTestShortTimeout):
+				isTimeout = true
+			}
+		}
+		wg2.Done()
+	}()
+	wg2.Wait()
+	if isTimeout {
+		t.Fatalf("Timeout when expecting the onMessage() callback to be invoked")
+	}
+	if gotMsgs := ts.receivedMsgs(); !cmp.Equal(gotMsgs, wantMsgs) {
+		t.Fatalf("Received messages is %v, want %v", gotMsgs, wantMsgs)
+	}
+	if gotMsgs2 := ts2.receivedMsgs(); !cmp.Equal(gotMsgs2, wantMsgs2) {
+		t.Fatalf("Received messages is %v, want %v", gotMsgs2, wantMsgs2)
 	}
 
 	pubsub.Stop()
-	time.Sleep(defaultTestShortTimeout)
 
-	pubsub.Publish(99)
+	go func() {
+		pubsub.Publish(99)
+	}()
 	// Ensure that the subscriber callback is not invoked as instantiated
 	// pubsub has already closed.
 	select {
 	case <-ts.onMsgCh:
-		if diff := cmp.Diff(ts.msgs, expectedMsg); diff != "" {
-			t.Errorf("Difference between ts.msgs and expectedMsg. diff(-want, +got):\n%s", diff)
-		}
+		t.Fatalf("The callback was invoked after pubsub being stopped")
+	case <-ts2.onMsgCh:
+		t.Fatalf("The callback was invoked after pubsub being stopped")
 	case <-time.After(defaultTestShortTimeout):
 	}
 }
 
-func (s) TestPubSub_PublishMultiMsgs_BeforeRegisterSubscriber(t *testing.T) {
+func (s) TestPubSub_PublishMsgs_BeforeRegisterSub(t *testing.T) {
 	// Create a new pubsub.
 	pubsub := NewPubSub()
 	defer pubsub.Stop()
 
-	ts := &testSubscriber{
-		msgs:    []int{},
-		onMsgCh: make(chan struct{}),
-	}
+	ts := newTestSubscriber()
 
 	pubsub.Publish(1)
 	pubsub.Publish(2)
@@ -138,85 +187,15 @@ func (s) TestPubSub_PublishMultiMsgs_BeforeRegisterSubscriber(t *testing.T) {
 
 	pubsub.Subscribe(ts)
 
-	expectedMsg := []int{3}
+	wantMsgs := []int{3}
 	// Ensure that the subscriber callback is invoked with a previously
 	// published message.
 	select {
 	case <-ts.onMsgCh:
-		if diff := cmp.Diff(ts.msgs, expectedMsg); diff != "" {
-			t.Errorf("Difference between ts.msgs and expectedMsg. diff(-want, +got):\n%s", diff)
+		if gotMsgs := ts.receivedMsgs(); !cmp.Equal(gotMsgs, wantMsgs) {
+			t.Fatalf("Received messages is %v, want %v", gotMsgs, wantMsgs)
 		}
 	case <-time.After(defaultTestShortTimeout):
-		t.Fatalf("The callback was invoked within defaultTestShortTimeout")
-	}
-}
-
-func (s) TestPutSub_PubslishMultiMsgs_RegisterMultiSubs(t *testing.T) {
-	// Create a new pubsub.
-	pubsub := NewPubSub()
-	defer pubsub.Stop()
-
-	onMsgCh := make(chan struct{})
-
-	ts := &testSubscriber{
-		msgs:    []int{},
-		onMsgCh: onMsgCh,
-	}
-	pubsub.Subscribe(ts)
-	expectedMsg := []int{}
-
-	const numPublished = 10
-	for i := 0; i < numPublished; i++ {
-		pubsub.Publish(i)
-		expectedMsg = append(expectedMsg, i)
-		select {
-		case <-ts.onMsgCh:
-			if diff := cmp.Diff(ts.msgs, expectedMsg); diff != "" {
-				t.Errorf("Difference between ts.msgs and expectedMsg. diff(-want, +got):\n%s", diff)
-			}
-		case <-time.After(defaultTestShortTimeout):
-			t.Fatalf("The callback was invoked within defaultTestShortTimeout")
-		}
-	}
-
-	ts2 := &testSubscriber{
-		msgs:    []int{},
-		onMsgCh: onMsgCh,
-	}
-	pubsub.Subscribe(ts2)
-	expectedMsg2 := []int{}
-
-	expectedMsg2 = append(expectedMsg2, numPublished-1)
-	select {
-	case <-ts.onMsgCh:
-		if diff := cmp.Diff(ts.msgs, expectedMsg); diff != "" {
-			t.Errorf("Difference between ts.msgs and expectedMsg. diff(-want, +got):\n%s", diff)
-		}
-	case <-time.After(defaultTestShortTimeout):
-		t.Fatalf("The callback was invoked within defaultTestShortTimeout")
-	}
-
-	for i := 0; i < numPublished; i++ {
-		pubsub.Publish(i)
-
-		expectedMsg = append(expectedMsg, i)
-		select {
-		case <-ts.onMsgCh:
-			if diff := cmp.Diff(ts.msgs, expectedMsg); diff != "" {
-				t.Errorf("Difference between ts.msgs and expectedMsg. diff(-want, +got):\n%s", diff)
-			}
-		case <-time.After(defaultTestShortTimeout):
-			t.Fatalf("The callback was invoked within defaultTestShortTimeout")
-		}
-
-		expectedMsg2 = append(expectedMsg2, i)
-		select {
-		case <-ts.onMsgCh:
-			if diff := cmp.Diff(ts2.msgs, expectedMsg2); diff != "" {
-				t.Errorf("Difference between ts2.msgs and expectedMsg2. diff(-want, +got):\n%s", diff)
-			}
-		case <-time.After(defaultTestShortTimeout):
-			t.Fatalf("The callback was invoked within defaultTestShortTimeout")
-		}
+		t.Fatalf("Timeout when expecting the onMessage() callback to be invoked")
 	}
 }
