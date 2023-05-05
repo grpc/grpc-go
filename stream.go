1f3fe1c8bc2cdc97521468580df01904376a6ea7
Mikhail Mazurskiy <126021+ash2k@users.noreply.github.com>
2023-05-06 01:38:20 +1000

    Update ClientStream.SendMsg doc (#6247)

--- stream.go
+++ stream.go
@@ -123,6 +123,9 @@ type ClientStream interface {
 	// calling RecvMsg on the same stream at the same time, but it is not safe
 	// to call SendMsg on the same stream in different goroutines. It is also
 	// not safe to call CloseSend concurrently with SendMsg.
+	//
+	// It is not safe to modify the message after calling SendMsg. Tracing
+	// libraries and stats handlers may use the message lazily.
 	SendMsg(m interface{}) error
 	// RecvMsg blocks until it receives a message into m or the stream is
 	// done. It returns io.EOF when the stream completes successfully. On
