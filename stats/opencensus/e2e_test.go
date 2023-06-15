7ae1bed4a452e8cf667d2576e09b0f24c25cc343
Zach Reyes <zasweq@google.com>
2023-06-14 20:23:46 -0400

    Fix flaky metrics test

--- stats/opencensus/e2e_test.go
+++ stats/opencensus/e2e_test.go
@@ -237,6 +237,23 @@ func distributionDataLatencyCount(vi *viewInformation, countWant int64, wantTags
 	return nil
 }
 
+// waitForServerCompletedRPCs waits until both Unary and Streaming metric rows
+// appear for server completed RPC's view (by checking for length of rows to be
+// 2). Returns an error if both the Unary and Streaming metric not found within
+// the passed context's timeout.
+func waitForServerCompletedRPCs(ctx context.Context, fe *fakeExporter) error {
+	for ; ctx.Err() == nil; <-time.After(time.Millisecond) {
+		rows, err := view.RetrieveData("grpc.io/server/completed_rpcs")
+		if err != nil {
+			continue
+		}
+		if len(rows) == 2 {
+			return nil
+		}
+	}
+	return fmt.Errorf("timeout when waiting for Unary and Streaming rows to be present for \"grpc.io/server/completed_rpcs\"")
+}
+
 // TestAllMetricsOneFunction tests emitted metrics from gRPC. It registers all
 // the metrics provided by this package. It then configures a system with a gRPC
 // Client and gRPC server with the OpenCensus Dial and Server Option configured,
@@ -987,10 +1004,13 @@ func (s) TestAllMetricsOneFunction(t *testing.T) {
 			},
 		},
 	}
-	// Unregister all the views. Unregistering a view causes a synchronous
-	// upload of any collected data for the view to any registered exporters.
-	// Thus, after this unregister call, the exporter has the data to make
-	// assertions on immediately.
+	// Server Side stats.End call happens asynchronously for both Unary and
+	// Streaming calls with respect to the RPC returning client side. Thus, add
+	// a sync point at the global view package level for these two rows to be
+	// recorded, which will be synchronously uploaded to exporters right after.
+	if err := waitForServerCompletedRPCs(ctx, fe); err != nil {
+		t.Fatal(err)
+	}
 	view.Unregister(allViews...)
 	// Assert the expected emissions for each metric match the expected
 	// emissions.
