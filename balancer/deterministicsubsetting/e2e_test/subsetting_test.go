ac526ee99f272a72785942230dee503727d62ec4
Sergey Matyukevich <s.matyukevich@gmail.com>
2023-07-31 08:38:57 -0600

    more verbose message in test

--- balancer/deterministicsubsetting/e2e_test/subsetting_test.go
+++ balancer/deterministicsubsetting/e2e_test/subsetting_test.go
@@ -1,3 +1,21 @@
+/*
+ *
+ * Copyright 2019 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 // Package e2e_test contains e2e test cases for the Subsetting LB Policy.
 package e2e_test
 
@@ -139,7 +157,7 @@ func checkRoundRobinRPCs(t *testing.T, ctx context.Context, clients []testgrpc.T
 	}
 
 	if maxClientsPerBackend > minClientsPerBackend+maxDiff {
-		t.Fatalf("the difference between min and max clients per backend should be <= %d, min: %d, max: %d", maxDiff, minClientsPerBackend, maxClientsPerBackend)
+		t.Fatalf("the difference between min and max clients per backend should be <= %d, clientsPerBackend: %v", maxDiff, clientsPerBackend)
 	}
 }
 
