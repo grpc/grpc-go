476a9c766940d35564d3649863fdaa4ca95b10e9
Zach Reyes <zasweq@google.com>
2023-08-08 19:28:39 -0400

    Fixed ref counts and also added e2e test

---
+++ balancer/leastrequest/balancer_test.go
@@ -0,0 +1,340 @@
+/*
+ *
+ * Copyright 2023 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package leastrequest
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/internal"
+	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/internal/stubserver"
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"
+	testpb "google.golang.org/grpc/interop/grpc_testing"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver/manual"
+	"google.golang.org/grpc/serviceconfig"
+)
+
+const (
+	defaultTestTimeout = 5 * time.Second
+)
+
+type s struct {
+	grpctest.Tester
+}
+
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
+}
+
+func (s) TestParseConfig(t *testing.T) {
+	parser := bb{}
+	tests := []struct {
+		name    string
+		input   string
+		wantCfg serviceconfig.LoadBalancingConfig
+		wantErr string
+	}{
+		{
+			name:  "happy-case-default",
+			input: `{}`,
+			wantCfg: &LBConfig{
+				ChoiceCount: 2,
+			},
+		},
+		{
+			name:  "happy-case-choice-count-set",
+			input: `{"choiceCount": 3}`,
+			wantCfg: &LBConfig{
+				ChoiceCount: 3,
+			},
+		},
+		{
+			name:  "happy-case-choice-count-greater-than-ten",
+			input: `{"choiceCount": 11}`,
+			wantCfg: &LBConfig{
+				ChoiceCount: 10,
+			},
+		},
+		{
+			name:    "choice-count-less-than-2",
+			input:   `{"choiceCount": 1}`,
+			wantErr: "must be >= 2",
+		},
+		{
+			name:    "invalid-json",
+			input:   "{{invalidjson{{",
+			wantErr: "invalid character",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotCfg, gotErr := parser.ParseConfig(json.RawMessage(test.input))
+			// Substring match makes this very tightly coupled to the
+			// internalserviceconfig.BalancerConfig error strings. However, it
+			// is important to distinguish the different types of error messages
+			// possible as the parser has a few defined buckets of ways it can
+			// error out.
+			if (gotErr != nil) != (test.wantErr != "") {
+				t.Fatalf("ParseConfig(%v) = %v, wantErr %v", test.input, gotErr, test.wantErr)
+			}
+			if gotErr != nil && !strings.Contains(gotErr.Error(), test.wantErr) {
+				t.Fatalf("ParseConfig(%v) = %v, wantErr %v", test.input, gotErr, test.wantErr)
+			}
+			if test.wantErr != "" {
+				return
+			}
+			if diff := cmp.Diff(gotCfg, test.wantCfg); diff != "" {
+				t.Fatalf("ParseConfig(%v) got unexpected output, diff (-got +want): %v", test.input, diff)
+			}
+		})
+	}
+}
+
+// setupBackends spins up four test backends, each listening on a port on
+// localhost. The four backends always reply with an empty response with no
+// error, and for streaming receive until hitting an EOF error.
+func setupBackends(t *testing.T) ([]string, func()) {
+	t.Helper()
+
+	backends := make([]*stubserver.StubServer, 4)
+	addresses := make([]string, 4)
+	// Construct and start 4 working backends.
+	for i := 0; i < 4; i++ {
+		backend := &stubserver.StubServer{
+			EmptyCallF: func(ctx context.Context, in *testpb.Empty) (*testpb.Empty, error) {
+				return &testpb.Empty{}, nil
+			},
+			FullDuplexCallF: func(stream testgrpc.TestService_FullDuplexCallServer) error {
+				for {
+					_, err := stream.Recv()
+					if err == io.EOF {
+						return nil
+					}
+				}
+			},
+		}
+		if err := backend.StartServer(); err != nil {
+			t.Fatalf("Failed to start backend: %v", err)
+		}
+		t.Logf("Started good TestService backend at: %q", backend.Address)
+		backends[i] = backend
+		addresses[i] = backend.Address
+	}
+	cancel := func() {
+		for _, backend := range backends {
+			backend.Stop()
+		}
+	}
+	return addresses, cancel
+}
+
+// checkRoundRobinRPCs verifies that EmptyCall RPCs on the given ClientConn,
+// connected to a server exposing the test.grpc_testing.TestService, are
+// roundrobined across the given backend addresses.
+//
+// Returns a non-nil error if context deadline expires before RPCs start to get
+// roundrobined across the given backends.
+func checkRoundRobinRPCs(ctx context.Context, client testgrpc.TestServiceClient, addrs []resolver.Address) error {
+	wantAddrCount := make(map[string]int)
+	for _, addr := range addrs {
+		wantAddrCount[addr.Addr]++
+	}
+	gotAddrCount := make(map[string]int)
+	for ; ctx.Err() == nil; <-time.After(time.Millisecond) {
+		gotAddrCount = make(map[string]int)
+		// Perform 3 iterations.
+		var iterations [][]string
+		for i := 0; i < 3; i++ {
+			iteration := make([]string, len(addrs))
+			for c := 0; c < len(addrs); c++ {
+				var peer peer.Peer
+				client.EmptyCall(ctx, &testpb.Empty{}, grpc.Peer(&peer))
+				if peer.Addr != nil {
+					iteration[c] = peer.Addr.String()
+				}
+			}
+			iterations = append(iterations, iteration)
+		}
+		// Ensure the the first iteration contains all addresses in addrs.
+		for _, addr := range iterations[0] {
+			gotAddrCount[addr]++
+		}
+		if diff := cmp.Diff(gotAddrCount, wantAddrCount); diff != "" {
+			continue
+		}
+		// Ensure all three iterations contain the same addresses.
+		if !cmp.Equal(iterations[0], iterations[1]) || !cmp.Equal(iterations[0], iterations[2]) {
+			continue
+		}
+		return nil
+	}
+	return fmt.Errorf("timeout when waiting for roundrobin distribution of RPCs across addresses: %v; got: %v", addrs, gotAddrCount)
+}
+
+// TestLeastRequestE2E tests the Least Request LB policy in an e2e style. The
+// Least Request balancer is configured as the top level balancer of the
+// channel, and is passed three addresses. Eventually, the test creates three
+// streams, which should be on certain backends according to the least request
+// algorithm. The randomness in the picker is injected in the test to be
+// deterministic, allowing the test to make assertions on the distribution.
+func (s) TestLeastRequestE2E(t *testing.T) {
+	defer func(u func() uint32) {
+		grpcranduint32 = u
+	}(grpcranduint32)
+	var index int
+	indexes := []uint32{
+		0, 0, 1, 1, 2, 2, // Triggers a round robin distribution.
+	}
+	grpcranduint32 = func() uint32 {
+		ret := indexes[index%len(indexes)]
+		index++
+		return ret
+	}
+	addresses, cancel := setupBackends(t)
+	defer cancel()
+
+	mr := manual.NewBuilderWithScheme("lr-e2e")
+	defer mr.Close()
+
+	// Configure least request as top level balancer of channel.
+	lrscJSON := `
+{
+  "loadBalancingConfig": [
+    {
+      "least_request_experimental": {
+        "choiceCount": 2
+      }
+    }
+  ]
+}`
+	sc := internal.ParseServiceConfig.(func(string) *serviceconfig.ParseResult)(lrscJSON)
+	firstThreeAddresses := []resolver.Address{
+		{Addr: addresses[0]},
+		{Addr: addresses[1]},
+		{Addr: addresses[2]},
+	}
+	mr.InitialState(resolver.State{
+		Addresses:     firstThreeAddresses,
+		ServiceConfig: sc,
+	})
+
+	cc, err := grpc.Dial(mr.Scheme()+":///", grpc.WithResolvers(mr), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("grpc.Dial() failed: %v", err)
+	}
+	defer cc.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+	testServiceClient := testgrpc.NewTestServiceClient(cc)
+
+	// Wait for all 3 backends to round robin across. The happens because a
+	// SubConn transitioning into READY causes a new picker update. Once the
+	// picker update with all 3 backends is present, this test can start to make
+	// assertions based on those backends.
+	if err := checkRoundRobinRPCs(ctx, testServiceClient, firstThreeAddresses); err != nil {
+		t.Fatalf("error in expected round robin: %v", err)
+	}
+
+	// Map ordering of READY SubConns is non deterministic. Thus, perform 3 RPCs
+	// mocked from the random to each index to learn the addresses of SubConns
+	// at each index.
+	index = 0
+	peerAtIndex := make([]string, 3)
+	var peer0 peer.Peer
+	if _, err := testServiceClient.EmptyCall(ctx, &testpb.Empty{}, grpc.Peer(&peer0)); err != nil {
+		t.Fatalf("testServiceClient.EmptyCall failed: %v", err)
+	}
+	peerAtIndex[0] = peer0.Addr.String()
+	if _, err := testServiceClient.EmptyCall(ctx, &testpb.Empty{}, grpc.Peer(&peer0)); err != nil {
+		t.Fatalf("testServiceClient.EmptyCall failed: %v", err)
+	}
+	peerAtIndex[1] = peer0.Addr.String()
+	if _, err := testServiceClient.EmptyCall(ctx, &testpb.Empty{}, grpc.Peer(&peer0)); err != nil {
+		t.Fatalf("testServiceClient.EmptyCall failed: %v", err)
+	}
+	peerAtIndex[2] = peer0.Addr.String()
+
+	// Start streaming RPCs, but do not finish them. Each subsequent stream
+	// should be started according to the least request algorithm, and chosen
+	// between the indexes provided.
+	index = 0
+	indexes = []uint32{
+		0, 0, // Causes first stream to be on first address.
+		0, 1, // Compares first address (which already has a RPC) to second, so choose second.
+		1, 2, // Compares second address (which already has a RPC) to third, so choose third.
+	}
+	// Start a streaming call on first, but don't finish the stream.
+	var peer1 peer.Peer
+	stream1, err := testServiceClient.FullDuplexCall(ctx, grpc.Peer(&peer1))
+	if err != nil {
+		t.Fatalf("testServiceClient.FullDuplexCall failed: %v", err)
+	}
+
+	// Start a second streaming call. From the indexes injected into random
+	// number generator, this should compare Address 1, which already has a RPC
+	// to Address 2, so thus should start the stream on Address 2.
+	var peer2 peer.Peer
+	stream2, err := testServiceClient.FullDuplexCall(ctx, grpc.Peer(&peer2))
+	if err != nil {
+		t.Fatalf("testServiceClient.FullDuplexCall failed: %v", err)
+	}
+
+	// Start a third streaming call. From the indexes injected into random
+	// number generator, this should compare Address 2, which already has a RPC
+	// to Address 3, so thus should start the stream on Address 3.
+	var peer3 peer.Peer
+	stream3, err := testServiceClient.FullDuplexCall(ctx, grpc.Peer(&peer3))
+	if err != nil {
+		t.Fatalf("testServiceClient.FullDuplexCall failed: %v", err)
+	}
+
+	// Finish the streams to collect peer information.
+	stream1.CloseSend()
+	if _, err = stream1.Recv(); err != io.EOF {
+		t.Fatalf("unexpected error: %v, expected an EOF error", err)
+	}
+	stream2.CloseSend()
+	if _, err = stream2.Recv(); err != io.EOF {
+		t.Fatalf("unexpected error: %v, expected an EOF error", err)
+	}
+	stream3.CloseSend()
+	if _, err = stream3.Recv(); err != io.EOF {
+		t.Fatalf("unexpected error: %v, expected an EOF error", err)
+	}
+	if peer1.Addr.String() != peerAtIndex[0] {
+		t.Fatalf("got: %v, want: %v", peer1.Addr.String(), peerAtIndex[0])
+	}
+	if peer2.Addr.String() != peerAtIndex[1] {
+		t.Fatalf("got: %v, want: %v", peer2.Addr.String(), peerAtIndex[1])
+	}
+	if peer3.Addr.String() != peerAtIndex[2] {
+		t.Fatalf("got: %v, want: %v", peer3.Addr.String(), peerAtIndex[2])
+	}
+}
--- balancer/leastrequest/leastrequest.go
+++ balancer/leastrequest/leastrequest.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2022 gRPC authors.
+ * Copyright 2023 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,30 +30,46 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
+// Global to stub out in tests.
+var grpcranduint32 = grpcrand.Uint32
+
 // Name is the name of the least request balancer.
 const Name = "least_request_experimental"
 
 var logger = grpclog.Component("least request")
 
+func init() {
+	balancer.Register(bb{})
+}
+
+// LBConfig is the balancer config for least_request_experimental balancer.
+type LBConfig struct {
+	serviceconfig.LoadBalancingConfig `json:"-"`
+
+	// ChoiceCount is the number of random SubConns to sample to try and find
+	// the one with the Least Request. If unset, defaults to 2. If set to < 2,
+	// will become 2, and if set to > 10, will become 10.
+	ChoiceCount uint32 `json:"choiceCount,omitempty"`
+}
+
 type bb struct{}
 
 func (bb) ParseConfig(s json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {
-	lbConfig := &leastRequestConfig{
+	lbConfig := &LBConfig{
 		ChoiceCount: 2,
 	}
 	if err := json.Unmarshal(s, lbConfig); err != nil {
 		return nil, fmt.Errorf("least-request: unable to unmarshal LBConfig: %v", err)
 	}
+	// "If `choice_count < 2`, the config will be rejected." - A48
+	if lbConfig.ChoiceCount < 2 { // sweet
+		return nil, fmt.Errorf("least-request: lbConfig.choiceCount: %v, must be >= 2", lbConfig.ChoiceCount)
+	}
 	// "If a LeastRequestLoadBalancingConfig with a choice_count > 10 is
 	// received, the least_request_experimental policy will set choice_count =
-	// 10."
+	// 10." - A48
 	if lbConfig.ChoiceCount > 10 {
 		lbConfig.ChoiceCount = 10
-	}
-	// I asked about this in chat but what happens if choiceCount < 2 (0 or 1)?
-	// Doing this for now.
-	if lbConfig.ChoiceCount < 2 {
-		lbConfig.ChoiceCount = 2
 	}
 	return lbConfig, nil
 }
@@ -63,7 +79,7 @@ func (bb) Name() string {
 }
 
 func (bb) Build(cc balancer.ClientConn, bOpts balancer.BuildOptions) balancer.Balancer {
-	b := &leastRequestBalancer{}
+	b := &leastRequestBalancer{scRPCCounts: make(map[balancer.SubConn]*int32)}
 	baseBuilder := base.NewBalancerBuilder(Name, b, base.Config{HealthCheck: true})
 	baseBalancer := baseBuilder.Build(cc, bOpts)
 	b.Balancer = baseBalancer
@@ -76,19 +92,11 @@ type leastRequestBalancer struct {
 	balancer.Balancer
 
 	choiceCount uint32
-}
-
-type leastRequestConfig struct {
-	serviceconfig.LoadBalancingConfig `json:"-"`
-
-	// ChoiceCount is the number of random SubConns to sample to try and find
-	// the one with the Least Request. If unset, defaults to 2. If set to < 2,
-	// will become 2, and if set to > 10, will become 10.
-	ChoiceCount uint32 `json:"choiceCount,omitempty"`
+	scRPCCounts map[balancer.SubConn]*int32 // Hold onto RPC counts to keep track for subsequent picker updates.
 }
 
 func (lrb *leastRequestBalancer) UpdateClientConnState(s balancer.ClientConnState) error {
-	lrCfg, ok := s.BalancerConfig.(*leastRequestConfig)
+	lrCfg, ok := s.BalancerConfig.(*LBConfig)
 	if !ok {
 		logger.Errorf("least-request: received config with unexpected type %T: %v", s.BalancerConfig, s.BalancerConfig)
 		return balancer.ErrBadResolverState
@@ -100,7 +108,7 @@ func (lrb *leastRequestBalancer) UpdateClientConnState(s balancer.ClientConnStat
 
 type scWithRPCCount struct {
 	sc      balancer.SubConn
-	numRPCs int32
+	numRPCs *int32
 }
 
 func (lrb *leastRequestBalancer) Build(info base.PickerBuildInfo) balancer.Picker {
@@ -108,10 +116,26 @@ func (lrb *leastRequestBalancer) Build(info base.PickerBuildInfo) balancer.Picke
 	if len(info.ReadySCs) == 0 {
 		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
 	}
+
+	for sc := range lrb.scRPCCounts {
+		if _, ok := info.ReadySCs[sc]; !ok { // If no longer ready, no more need for the ref to count active RPCs.
+			delete(lrb.scRPCCounts, sc)
+		}
+	}
+
+	// Create new refs if needed.
+	for sc := range info.ReadySCs {
+		if _, ok := lrb.scRPCCounts[sc]; !ok {
+			lrb.scRPCCounts[sc] = new(int32)
+		}
+	}
+
+	// Copy refs to counters into picker.
 	scs := make([]scWithRPCCount, 0, len(info.ReadySCs))
 	for sc := range info.ReadySCs {
 		scs = append(scs, scWithRPCCount{
-			sc: sc,
+			sc:      sc,
+			numRPCs: lrb.scRPCCounts[sc], // guaranteed to be present due to algorithm
 		})
 	}
 
@@ -132,27 +156,25 @@ type picker struct {
 func (p *picker) Pick(balancer.PickInfo) (balancer.PickResult, error) {
 	var pickedSC *scWithRPCCount
 	for i := 0; i < int(p.choiceCount); i++ {
-		index := grpcrand.Uint32() % uint32(len(p.subConns))
+		index := grpcranduint32() % uint32(len(p.subConns))
 		sc := p.subConns[index]
 		if pickedSC == nil {
 			pickedSC = &sc
 			continue
 		}
-		if sc.numRPCs < pickedSC.numRPCs {
+		if *sc.numRPCs < *pickedSC.numRPCs {
 			pickedSC = &sc
 		}
 	}
 	// "The counter for a subchannel should be atomically incremented by one
 	// after it has been successfully picked by the picker." - A48
-	atomic.AddInt32(&pickedSC.numRPCs, 1)
-
+	atomic.AddInt32(pickedSC.numRPCs, 1)
 	// "the picker should add a callback for atomically decrementing the
 	// subchannel counter once the RPC finishes (regardless of Status code)." -
 	// A48.
 	done := func(balancer.DoneInfo) {
-		atomic.AddInt32(&pickedSC.numRPCs, -1)
+		atomic.AddInt32(pickedSC.numRPCs, -1)
 	}
-
 	return balancer.PickResult{
 		SubConn: pickedSC.sc,
 		Done:    done,
