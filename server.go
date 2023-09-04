ae2db21b0f3cde549659d64fca64f91fd7fd1895
Genri Dark <genri.dark@bk.ru>
2023-09-04 15:54:28 +0300

    rpc_util: added a byte slice return to recvBufferPool

--- server.go
+++ server.go
@@ -1311,6 +1311,9 @@ func (s *Server) processUnaryRPC(ctx context.Context, t transport.ServerTranspor
 		}
 		return err
 	}
+
+	defer s.opts.recvBufferPool.Put(&d)
+
 	if channelz.IsOn() {
 		t.IncrMsgRecv()
 	}
