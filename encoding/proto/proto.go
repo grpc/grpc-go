24902d874d42b9a84d9b3970d44971886d90b3a2
Arvind Bright <arvind.bright100@gmail.com>
2024-02-05 14:20:02 -0800

    deps: fix backwards compatibility with encoding

--- encoding/proto/proto.go
+++ encoding/proto/proto.go
@@ -25,6 +25,7 @@ import (
 
 	"google.golang.org/grpc/encoding"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/protoadapt"
 )
 
 // Name is the name registered for the proto compressor.
@@ -38,18 +39,30 @@ func init() {
 type codec struct{}
 
 func (codec) Marshal(v any) ([]byte, error) {
-	vv, ok := v.(proto.Message)
-	if !ok {
+	var vv proto.Message
+	switch v := v.(type) {
+	case protoadapt.MessageV1:
+		vv = protoadapt.MessageV2Of(v)
+	case protoadapt.MessageV2:
+		vv = v
+	default:
 		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
 	}
+
 	return proto.Marshal(vv)
 }
 
 func (codec) Unmarshal(data []byte, v any) error {
-	vv, ok := v.(proto.Message)
-	if !ok {
+	var vv proto.Message
+	switch v := v.(type) {
+	case protoadapt.MessageV1:
+		vv = protoadapt.MessageV2Of(v)
+	case protoadapt.MessageV2:
+		vv = v
+	default:
 		return fmt.Errorf("failed to unmarshal, message is %T, want proto.Message", v)
 	}
+
 	return proto.Unmarshal(data, vv)
 }
 
