4d3bc7221415b4bfb1aaf1f56564ad57dd880209
Anand Inguva <anandinguva98@gmail.com>
2024-02-09 18:34:33 -0500

    Add a comment on error case

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -642,6 +642,8 @@ func (t *http2Server) HandleStreams(ctx context.Context, handle func(*Stream)) {
 		switch frame := frame.(type) {
 		case *http2.MetaHeadersFrame:
 			if err := t.operateHeaders(ctx, frame, handle); err != nil {
+				// Any error processing client headers,
+				// e.g. invalid stream ID, is considered a protocol violation.
 				t.controlBuf.put(&goAway{
 					code:      http2.ErrCodeProtocol,
 					debugData: []byte(err.Error()),
