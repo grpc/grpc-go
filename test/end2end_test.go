cfa206baa3ce147389dfc46c2fcc6dbc8c5a7b09
Anand Inguva <34158215+AnandInguva@users.noreply.github.com>
2024-02-09 17:03:43 -0500

    Update test/end2end_test.go
    Co-authored-by: Arvind Bright <arvind.bright100@gmail.com>

--- test/end2end_test.go
+++ test/end2end_test.go
@@ -3926,7 +3926,7 @@ func (s) TestClientInvalidStreamID(t *testing.T) {
 	st.greet()
 	st.writeHeadersGRPC(2, "/grpc.testing.TestService/StreamingInputCall", true)
 	goAwayFrame := st.wantGoAway(http2.ErrCodeProtocol)
-	got := string(goAwayFrame.DebugData()[:])
+	got := string(goAwayFrame.DebugData())
 	want := "received an illegal stream id: 2. headers frame: [FrameHeader HEADERS flags=END_STREAM|END_HEADERS stream=2 len=60]"
 	if got != want {
 		t.Fatalf("Error received: %v, want: %v", got, want)
