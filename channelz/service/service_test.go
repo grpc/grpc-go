5d342be7c9b14919403db1bc9d426b222a4eee09
Clement <clement.jean@epitech.eu>
2024-03-27 11:07:17 +0800

    rm protoimpl

--- channelz/service/service_test.go
+++ channelz/service/service_test.go
@@ -34,11 +34,12 @@ import (
 	"google.golang.org/grpc/internal/channelz"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/protobuf/encoding/prototext"
-	"google.golang.org/protobuf/protoadapt"
+	"google.golang.org/protobuf/reflect/protodesc"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/reflect/protoregistry"
-	"google.golang.org/protobuf/runtime/protoimpl"
 	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/dynamicpb"
 	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -98,26 +99,66 @@ func convertSocketRefSliceToMap(sktRefs []*channelzpb.SocketRef) map[int64]strin
 	return m
 }
 
-type OtherSecurityValue struct {
-	LocalCertificate  []byte `protobuf:"bytes,1,opt,name=local_certificate,json=localCertificate,proto3" json:"local_certificate,omitempty"`
-	RemoteCertificate []byte `protobuf:"bytes,2,opt,name=remote_certificate,json=remoteCertificate,proto3" json:"remote_certificate,omitempty"`
-}
-
-func (m *OtherSecurityValue) Reset() { *m = OtherSecurityValue{} }
-func (m *OtherSecurityValue) String() string {
-	opt := prototext.MarshalOptions{Multiline: false}
-	return opt.Format(protoadapt.MessageV2Of(m))
-}
-func (*OtherSecurityValue) ProtoMessage() {}
+var (
+	filePath = "service_test.proto"
+	syntax   = "proto3"
+	pkg      = "grpc.credentials"
+	msgName  = "OtherChannelzSecurityValue"
+	local    = "local_certificate"
+	remote   = "remote_certificate"
+	one      = int32(1)
+	two      = int32(2)
+)
 
 func init() {
 	// Ad-hoc registering the proto type here to facilitate Unmarshal of OtherSecurityValue.
-	m := (*OtherSecurityValue)(nil)
-	s := "grpc.credentials.OtherChannelzSecurityValue"
-	mt := protoimpl.X.LegacyMessageTypeOf(m, protoreflect.FullName(s))
-	if err := protoregistry.GlobalTypes.RegisterMessage(mt); err != nil {
+	pb := &descriptorpb.FileDescriptorProto{
+		Name:    &filePath,
+		Syntax:  &syntax,
+		Package: &pkg,
+		MessageType: []*descriptorpb.DescriptorProto{
+			{
+				Name: &msgName,
+				Field: []*descriptorpb.FieldDescriptorProto{
+					{
+						Label:  descriptorpb.FieldDescriptorProto_LABEL_OPTIONAL.Enum(),
+						Name:   &local,
+						Type:   descriptorpb.FieldDescriptorProto_TYPE_BYTES.Enum(),
+						Number: &one,
+					},
+					{
+						Label:  descriptorpb.FieldDescriptorProto_LABEL_OPTIONAL.Enum(),
+						Name:   &remote,
+						Type:   descriptorpb.FieldDescriptorProto_TYPE_BYTES.Enum(),
+						Number: &two,
+					},
+				},
+			},
+		},
+	}
+	fd, err := protodesc.NewFile(pb, protoregistry.GlobalFiles)
+	if err != nil {
 		panic(err)
 	}
+	if err = protoregistry.GlobalFiles.RegisterFile(fd); err != nil {
+		panic(err)
+	}
+}
+
+func newOtherSecurityValue(localCert, remoteCert []byte) *dynamicpb.Message {
+	fd, err := protoregistry.GlobalFiles.FindFileByPath(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	md := fd.Messages().ByName(protoreflect.Name(msgName))
+	msg := dynamicpb.NewMessage(md)
+	fields := md.Fields()
+	localCertField := fields.ByName(protoreflect.Name(local))
+	remoteCertField := fields.ByName(protoreflect.Name(remote))
+	msg.Set(localCertField, protoreflect.ValueOfBytes(localCert))
+	msg.Set(remoteCertField, protoreflect.ValueOfBytes(remoteCert))
+	return msg
 }
 
 func (s) TestGetTopChannels(t *testing.T) {
@@ -635,7 +676,7 @@ func (s) TestGetSocket(t *testing.T) {
 	}), newSocket(czSocket{
 		security: &credentials.OtherChannelzSecurityValue{
 			Name:  "YYYY",
-			Value: protoadapt.MessageV2Of(&OtherSecurityValue{LocalCertificate: []byte{1, 2, 3}, RemoteCertificate: []byte{4, 5, 6}}),
+			Value: newOtherSecurityValue([]byte{1, 2, 3}, []byte{4, 5, 6}),
 		},
 	}),
 	}
