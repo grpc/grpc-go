d91269b24e21e475a40c295b645ad5ce446c2f70
Arvind Bright <arvind.bright100@gmail.com>
2024-04-03 15:41:33 -0700

    this is a test for running the workflow in my fork

--- cmd/protoc-gen-go-grpc/protoc-gen-go-grpc_test.sh
+++ cmd/protoc-gen-go-grpc/protoc-gen-go-grpc_test.sh
@@ -4,11 +4,11 @@ set -ex # Debugging enabled.
 
 trap "git clean --force --quiet" EXIT
 
+# Build protoc-gen-go-grpc binary and add to $PATH.
+go build google.golang.org/grpc/cmd/protoc-gen-go-grpc
+PATH="${PWD}:${PATH}"
+
 WD="$(dirname $0)"
-
-go build .
-PATH="${WD}:${PATH}"
-
 protoc \
     --go-grpc_out=. \
     --go-grpc_opt=paths=source_relative \
