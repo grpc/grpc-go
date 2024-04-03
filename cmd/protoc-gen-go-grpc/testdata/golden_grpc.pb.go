52f1e4e0a7bde654ce79c56f573d1af1f0a64349
Arvind Bright <arvind.bright100@gmail.com>
2024-04-03 15:53:09 -0700

    check again with a change to the generator

--- cmd/protoc-gen-go-grpc/testdata/golden_grpc.pb.go
+++ cmd/protoc-gen-go-grpc/testdata/golden_grpc.pb.go
@@ -53,8 +53,8 @@ type bidirectionalStreamingServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewBidirectionalStreamingServiceClient(cc1 grpc.ClientConnInterface) BidirectionalStreamingServiceClient {
-	return &bidirectionalStreamingServiceClient{cc1}
+func NewBidirectionalStreamingServiceClient(cc grpc.ClientConnInterface) BidirectionalStreamingServiceClient {
+	return &bidirectionalStreamingServiceClient{cc}
 }
 
 func (c *bidirectionalStreamingServiceClient) UnaryMethod(ctx context.Context, in *EventRequest, opts ...grpc.CallOption) (*EventResponse, error) {
