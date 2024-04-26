15a33a79b3cf8b9e846a978ed425ba99fc6af560
Sergii Tkachenko <sergiitk@google.com>
2024-04-26 09:16:40 -0700

    test/kokoro: move test suite lists to psm_interop_kokoro_lib.sh

---
+++ test/kokoro/psm-interop-test-go.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Copyright 2024 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -eo pipefail
+
+# Constants
+readonly GRPC_LANGUAGE="go"
+readonly GITHUB_REPOSITORY_NAME="grpc-go"
+readonly TEST_DRIVER_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/${TEST_DRIVER_REPO_OWNER:-grpc}/psm-interop/${TEST_DRIVER_BRANCH:-main}/.kokoro/psm_interop_kokoro_lib.sh"
+readonly BUILD_SCRIPT_DIR="$(dirname "$0")"
+
+echo "Sourcing test driver install script from: ${TEST_DRIVER_INSTALL_SCRIPT_URL}"
+source /dev/stdin <<< "$(curl -s "${TEST_DRIVER_INSTALL_SCRIPT_URL}")"
+psm::run "${PSM_TEST_SUITE}"
