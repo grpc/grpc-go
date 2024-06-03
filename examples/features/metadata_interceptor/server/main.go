7a6355cd491b51d287f92aff7563a7fbf1dd96da
Purnesh Dixit <purneshdixit@google.com>
2024-06-03 10:39:28 +0530

    Add SetTrailer and SetHeader in metadata_interceptor example

--- examples/features/metadata_interceptor/server/main.go
+++ examples/features/metadata_interceptor/server/main.go
@@ -49,10 +49,22 @@ func unaryInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, 
 		return nil, errMissingMetadata
 	}
 
+	// create and set metadata from interceptor to server
 	md.Append("key1", "value1")
 	ctx = metadata.NewIncomingContext(ctx, md)
 
-	return handler(ctx, req)
+	// call the handler to complete the normal execution of the RPC
+	resp, err := handler(ctx, req)
+
+	// create and set header metadata from interceptor to client
+	header := metadata.Pairs("header-key", "val")
+	grpc.SetHeader(ctx, header)
+
+	// create and set trailer metadata from interceptor to client
+	trailer := metadata.Pairs("trailer-key", "val")
+	grpc.SetTrailer(ctx, trailer)
+
+	return resp, err
 }
 
 func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
@@ -89,10 +101,22 @@ func streamInterceptor(srv any, ss grpc.ServerStream, info *grpc.StreamServerInf
 		return errMissingMetadata
 	}
 
+	// create and set metadata from interceptor to server
 	md.Append("key1", "value1")
 	ctx := metadata.NewIncomingContext(ss.Context(), md)
 
-	return handler(srv, &wrappedStream{ss, ctx})
+	// call the handler to complete the normal execution of the RPC
+	err := handler(srv, &wrappedStream{ss, ctx})
+
+	// create and set header metadata from interceptor to client
+	header := metadata.Pairs("header-key", "val")
+	ss.SetHeader(header)
+
+	// create and set trailer metadata from interceptor to client
+	trailer := metadata.Pairs("trailer-key", "val")
+	ss.SetTrailer(trailer)
+
+	return err
 }
 
 func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {
