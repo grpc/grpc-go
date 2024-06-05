b09abe96817746ba93380ca456c009d0909fb124
Gregory Cooke <gregorycooke@google.com>
2024-06-05 19:23:21 +0000

    add removed line that was important

--- security/advancedtls/crl.go
+++ security/advancedtls/crl.go
@@ -411,7 +411,9 @@ func extractCRLIssuer(crlBytes []byte) ([]byte, error) {
 	}
 	der := cryptobyte.String(crlBytes)
 	var issuer cryptobyte.String
+	// This doubled der.ReadASN1 is intentional, it modifies the input buffer
 	if !der.ReadASN1(&der, cbasn1.SEQUENCE) ||
+		!der.ReadASN1(&der, cbasn1.SEQUENCE) ||
 		!der.SkipOptionalASN1(cbasn1.INTEGER) ||
 		!der.SkipASN1(cbasn1.SEQUENCE) ||
 		!der.ReadASN1Element(&issuer, cbasn1.SEQUENCE) {
