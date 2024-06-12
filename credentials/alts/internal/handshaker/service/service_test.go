8b37565c0750814e6adfec55549663bfd774ac1a
Abhishek Ranjan <aranjans@google.com>
2024-06-12 11:40:18 +0530

    fix: Add grpc tester receiver to TestDial

--- credentials/alts/internal/handshaker/service/service_test.go
+++ credentials/alts/internal/handshaker/service/service_test.go
@@ -20,30 +20,34 @@ package service
 
 import (
 	"testing"
+
+	"google.golang.org/grpc/internal/grpctest"
 )
+
+type s struct {
+	grpctest.Tester
+}
+
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
+}
 
 const (
 	testAddress1 = "some_address_1"
 	testAddress2 = "some_address_2"
 )
 
-// TestDial verifies the behavior of the Dial function in managing gRPC connections
-// and checks the below scenarios:
-//   - First call to Dial with an address creates a new connection and stores it.
-//   - Second call to Dial with the same address returns the existing connection
-//     and expects the returned connection matching with ƒirst connection.
-//   - Third call to Dial with a different address creates a separate new connection
-//     and expects a different connection from connnection1 and connection2.
-func TestDial(t *testing.T) {
+// TestDial verifies the behaviour of alts handshake when there are multiple Dials.
+// If a connection has already been established, this function returns it.
+// Otherwise, a new connection is created.
+func (s) TestDial(t *testing.T) {
 	// First call to Dial, it should create a connection to the server running
 	// at the given address.
 	conn1, err := Dial(testAddress1)
 	if err != nil {
 		t.Fatalf("first call to Dial(%v) failed: %v", testAddress1, err)
 	}
-	if conn1 == nil {
-		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)
-	}
+	defer conn1.Close()
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
 	}
@@ -53,6 +57,7 @@ func TestDial(t *testing.T) {
 	if err != nil {
 		t.Fatalf("second call to Dial(%v) failed: %v", testAddress1, err)
 	}
+	defer conn2.Close()
 	if got, want := conn2, conn1; got != want {
 		t.Fatalf("second call to Dial(%v)=(%v, _), want (%v,. _)", testAddress1, got, want)
 	}
@@ -60,15 +65,12 @@ func TestDial(t *testing.T) {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
 	}
 
-	// Third call to Dial using a different address should create a new
-	// connection.
+	// Third call to Dial using a different address should create a new connection.
 	conn3, err := Dial(testAddress2)
 	if err != nil {
 		t.Fatalf("third call to Dial(%v) failed: %v", testAddress2, err)
 	}
-	if conn3 == nil {
-		t.Fatalf("third call to Dial(%v)=(nil, _), want not nil", testAddress2)
-	}
+	defer conn3.Close()
 	if got, want := hsConnMap[testAddress2], conn3; got != want {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress2, got, want)
 	}
