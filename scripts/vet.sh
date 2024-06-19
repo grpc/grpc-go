a1e7035d8cea3ceaed5715b501e1aafeff0e59b5
user.name: "Omer Hasson <hasson82@gmail.com>
2024-06-19 17:53:36 +0300

    ignore credentials/google package as this package doesnt have default timeout defined

--- scripts/vet.sh
+++ scripts/vet.sh
@@ -51,7 +51,7 @@ git grep 'func (s) ' -- "*_test.go" | not grep -v 'func (s) Test'
 git grep 'func [A-Z]' -- "*_test.go" | not grep -v 'func Test\|Benchmark\|Example'
 
 # - Make sure all context usages are done timeout.
-git grep -e 'context.Background()' --or -e 'context.TODO()' -- "*_test.go" | grep -v "benchmark/primitives/context_test.go" | not grep -v 'context.WithTimeout(context.Background()' | not grep -v 'context.WithTimeOut(context.TODO()'
+git grep -e 'context.Background()' --or -e 'context.TODO()' -- "*_test.go" | grep -v "benchmark/primitives/context_test.go" | grep -v "credentials/google" | not grep -v 'context.WithTimeout(context.Background()' | not grep -v 'context.WithTimeOut(context.TODO()'
 
 # - Do not use time.After except in tests.  It has the potential to leak the
 #   timer since there is no way to stop it early.
