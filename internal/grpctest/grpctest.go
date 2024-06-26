23975fc60fe6d681065a42e2206e8554aab8ac5c
PapaCharlie <paul.chesnais@gmail.com>
2024-06-26 19:49:01 +0200

    Add TODO on leakcheck

--- internal/grpctest/grpctest.go
+++ internal/grpctest/grpctest.go
@@ -49,6 +49,10 @@ type Tester struct{}
 // Setup updates the tlogger.
 func (Tester) Setup(t *testing.T) {
 	TLogger.Update(t)
+	// TODO: There is one final leak around closing connections without completely
+	//  draining the recvBuffer that has yet to be resolved. All other leaks have been
+	//  completely addressed, and this can be turned back on as soon as this issue is
+	//  fixed.
 	//leakcheck.SetTrackingBufferPool(errorer{t: t})
 }
 
