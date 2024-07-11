bd7f205394d98171f469503d7a531dd083750bd8
vivek <infovivek2020@gmail.com>
2024-07-11 16:49:50 +0530

    diverge resolution in master

--- channelz/grpc_channelz_v1/channelz_grpc.pb.go
+++ channelz/grpc_channelz_v1/channelz_grpc.pb.go
@@ -36,8 +36,8 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-// Requires gRPC-Go v1.62.0 or later.
-const _ = grpc.SupportPackageIsVersion8
+// Requires gRPC-Go v1.64.0 or later.
+const _ = grpc.SupportPackageIsVersion9
 
 const (
 	Channelz_GetTopChannels_FullMethodName   = "/grpc.channelz.v1.Channelz/GetTopChannels"
