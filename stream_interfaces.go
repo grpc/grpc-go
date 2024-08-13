5eb8edaaad21591226294e84e68ed0ccf32a0b0a
vissajanardhan <janardhan1819@gmail.com>
2024-08-13 20:56:52 +0530

    Updated the stream interfaces description as per the documentation and comments

--- stream_interfaces.go
+++ stream_interfaces.go
@@ -23,10 +23,10 @@ package grpc
 // message. It is used in generated code.
 type ServerStreamingClient[Res any] interface {
 	// Recv receives the next message from the server. The client can repeatedly
-	// call the Recv method to read the response stream. If an error occurs on
-	// the stream, it will be returned as an instance of the status package.
-	// Refer to the status package documentation for more details. The stream
-	// ends with (nil, io.EOF) once all messages are received.
+	// call Recv to read messages from the server-to-client response stream. Recv
+	// returns (nil, io.EOF) once the server-to-client stream is completely read.
+	// If an error occurs on the stream, it will be returned as an instance of the
+	// status package. Refer to the status package documentation for more details.
 	Recv() (*Res, error)
 	ClientStream
 }
@@ -35,12 +35,12 @@ type ServerStreamingClient[Res any] interface {
 // request, many responses) RPC. It is generic over the type of the response
 // message. It is used in generated code.
 type ServerStreamingServer[Res any] interface {
-	// Send sends a response message to the client. This method can be called
-	// multiple times to send multiple messages as part of the server's response
-	// stream. If an error occurs on the stream, it will be returned as an instance
-	// of the status package. Refer to the status package documentation for more
-	// details. The stream ends when the handler method returns. No methods on this
-	// interface should be called afterward.
+	// Send can send a stream of messages to the client. It may be called multiple
+	// times to send multiple messages. However, once the handler method has returned,
+	// no further calls to this method should be made.
+	// If an error occurs on the stream, it will be returned as an instance of the status
+	// package. Refer to the status package documentation for more details. End-of-stream
+	// for the server-to-client stream is indicated by the return of the handler method.
 	Send(*Res) error
 	ServerStream
 }
@@ -51,18 +51,16 @@ type ServerStreamingServer[Res any] interface {
 // generated code.
 type ClientStreamingClient[Req any, Res any] interface {
 	// Send sends a request message to the server. The client can repeatedly call
-	// the Send method to send the request message stream. If an error occurs on
-	// the stream, it will be returned as an instance of the status package. Refer
-	// to the status package documentation for more details.
+	// Send to send messages as part of the client-to-server request stream. If an
+	// error occurs on the stream, it will be returned as an instance of the status
+	// package. Refer to the status package documentation for more details.
 	Send(*Req) error
 
-	// CloseAndRecv closes the sending side of the request stream and waits for
-	// the server's unary response. This method is typically called after sending
-	// all request messages to signal the end of the stream and to receive the final
-	// response from the server. If an error occurs on the stream, it will be returned
-	// as an instance of the status package. Refer to the status package documentation
-	// for more details. The CloseAndRecv method must be called once and only once,
-	// to close the request stream and receive the single response message from the server.
+	// CloseAndRecv closes the client-to-server request stream and waits for the server's
+	// unary response. This method must be called once and only once after sending all
+	// request messages to close the stream and receive the final response from the server.
+	// If an error occurs on the stream, it will be returned as an instance of the status
+	// package. Refer to the status package documentation for more details.
 	CloseAndRecv() (*Res, error)
 	ClientStream
 }
@@ -72,19 +70,18 @@ type ClientStreamingClient[Req any, Res any] interface {
 // message stream and the type of the unary response message. It is used in
 // generated code.
 type ClientStreamingServer[Req any, Res any] interface {
-	// Recv reads a request message from the client. This method is called repeatedly
-	// to receive all messages sent by the client as part of the request stream.
-	// If an error occurs on the stream, it will be returned as an instance of the
-	// status package. Refer to the status package documentation for more details. The
-	// stream ends with (nil, io.EOF) once all messages have been received.
+	// Recv reads a request message from the client. This method can be called
+	// repeatedly to receive the full stream of messages from the client. Recv returns
+	// (nil, io.EOF) once the end of the stream is reached. Errors will be returned
+	// as instances of the status package. Refer to the status package documentation
+	// for more details.
 	Recv() (*Req, error)
 
-	// SendAndClose sends a unary response message to the client and closes the stream.
-	// This method is typically called after processing all request messages from the
-	// client to send the final response and close the stream. If an error occurs on
-	// the stream, it will be returned as an instance of the status package. Refer to
-	// the status package documentation for more details. The stream is terminated upon
-	// calling this method. No further methods on this interface should be called afterward.
+	// SendAndClose sends a single response message to the client and closes the stream.
+	// This method must be called once and only once after all request messages have
+	// been processed. If an error occurs on the stream, the error returned will be
+	// implemented by the status package. Please see the documentation in that package
+	// for more information. No further methods should be called after SendAndClose.
 	SendAndClose(*Res) error
 	ServerStream
 }
@@ -94,17 +91,18 @@ type ClientStreamingServer[Req any, Res any] interface {
 // request message stream and the type of the response message stream. It is
 // used in generated code.
 type BidiStreamingClient[Req any, Res any] interface {
-	// Send sends a message to the server. This method is called repeatedly
-	// to send all messages as part of the request message stream. If an error
-	// occurs on the stream, it will be returned as an instance of the status
-	// package. Refer to the status package documentation for more details.
+	// Send sends a message to the server. This method can be called repeatedly
+	// to send messages as part of the client-to-server request stream. If an
+	// error occurs on the stream, it will be returned as an instance of the status
+	// package. Refer to the status package documentation for more details. End-of-stream
+	// for the client-to-server stream can be indicated by calling the CloseSend method.
 	Send(*Req) error
 
 	// Recv receives the next message from the server's response stream. This method
-	// can be called repeatedly to receive all messages sent by the server. If an
-	// error occurs on the stream, it will be returned as an instance of the status
-	// package. Refer to the status package documentation for more details. End-of-stream
-	// can be indicated by calling the CloseSend method on the stream.
+	// can be called repeatedly to receive all messages sent by the server. Recv returns
+	// (nil, io.EOF) once the server-to-client stream is completely read. If an error occurs
+	// on the stream, it will be returned as an instance of the status package. Refer to
+	// the status package documentation for more details.
 	Recv() (*Res, error)
 	ClientStream
 }
@@ -115,16 +113,15 @@ type BidiStreamingClient[Req any, Res any] interface {
 // used in generated code.
 type BidiStreamingServer[Req any, Res any] interface {
 	// Recv receives a request message from the client. The server-side handler can
-	// repeatedly call Recv to read the request message stream. If an error occurs on
-	// the stream, it will be returned as an instance of the status package. Refer to
-	// the status package documentation for more details. Recv returns (nil, io.EOF)
-	// once it has reached the end of the request stream from the client.
+	// repeatedly call Recv to read the request message stream. Recv returns (nil, io.EOF)
+	// once the end of the client-to-server stream is reached. Errors are returned as
+	// instances of the status package. Refer to the status package documentation for details.
 	Recv() (*Req, error)
 
 	// Send sends a response message to the client. The server-side handler can repeatedly
-	// call Send to write to the response message stream. If an error occurs, it will be
-	// returned as an instance of the status package. Refer to the status package documentation
-	// for more details. No further methods on this interface should be called afterward.
+	// call Send to write to the server-to-client message stream. The end of the response
+	// stream is indicated by the return of the bidi method handler. Errors are returned as
+	// instances of the status package. Refer to the status package documentation for details.
 	Send(*Res) error
 	ServerStream
 }
