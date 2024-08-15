9636c56603031c874edf28ff55df532607964633
Abhishek Ranjan <aranjans@google.com>
2024-08-15 18:58:38 +0530

    Set write deadline for net.Conn

--- internal/transport/http2_client.go
+++ internal/transport/http2_client.go
@@ -985,6 +985,7 @@ func (t *http2Client) closeStream(s *Stream, err error, rst bool, rstCode http2.
 // only once on a transport. Once it is called, the transport should not be
 // accessed anymore.
 func (t *http2Client) Close(err error) {
+	t.conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
 	t.mu.Lock()
 	// Make sure we only close once.
 	if t.state == closing {
