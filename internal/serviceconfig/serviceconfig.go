e15b23d5db31a8af0dc14c5a90abbda746a55986
vissajanardhan <janardhan1819@gmail.com>
2024-08-30 17:30:11 +0530

    re-trigger

--- internal/serviceconfig/serviceconfig.go
+++ internal/serviceconfig/serviceconfig.go
@@ -88,7 +88,6 @@ func (bc *BalancerConfig) UnmarshalJSON(b []byte) error {
 			name    string
 			jsonCfg json.RawMessage
 		)
-
 		// Get the key:value pair from the map. We have already made sure that
 		// the map contains a single entry.
 		for name, jsonCfg = range lbcfg {
