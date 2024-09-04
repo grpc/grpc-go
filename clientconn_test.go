9d02e35557fc530f3aefa8feab850bb7374bc1e8
Vissa Janardhan Krishna Sai <vissajanardhan@google.com>
2024-09-04 06:47:08 +0000

    Resolving merge conflicts and regenerated script

--- clientconn_test.go
+++ clientconn_test.go
@@ -1267,7 +1267,6 @@ func (ccw *stateRecordingCCWrapper) NewSubConn(addrs []resolver.Address, opts ba
 func keepReading(conn net.Conn) {
 	buf := make([]byte, 1024)
 	for _, err := conn.Read(buf); err == nil; _, err = conn.Read(buf) {
-		// keep reading until something causes the connection to die
 	}
 }
 
