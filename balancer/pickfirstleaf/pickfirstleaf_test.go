2e9f8739b58de20180840346b475f25caf9c3890
Arjan Bal <arjansbal@google.com>
2024-09-26 01:50:39 +0530

    Move common tests to pickfirst_test.go

--- balancer/pickfirstleaf/pickfirstleaf_test.go
+++ balancer/pickfirstleaf/pickfirstleaf_test.go
@@ -20,7 +20,6 @@ package pickfirstleaf
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"testing"
 	"time"
@@ -243,88 +242,5 @@ func (s) TestPickFirstLeaf_TFPickerUpdate(t *testing.T) {
 	sc2.UpdateState(balancer.SubConnState{ConnectivityState: connectivity.TransientFailure, ConnectionError: newTfErr})
 	if err := cc.WaitForPickerWithErr(ctx, newTfErr); err != nil {
 		t.Fatalf("cc.WaitForPickerWithErr(%v) returned error: %v", tfErr, err)
-	}
-}
-
-// TestPickFirstLeaf_InitialResolverError sends a resolver error to the balancer
-// before a valid resolver update. It verifies that the clientconn state is
-// updated to TRANSIENT_FAILURE.
-func (s) TestPickFirstLeaf_InitialResolverError(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cancel()
-	cc := testutils.NewBalancerClientConn(t)
-	bal := pickfirstBuilder{}.Build(cc, balancer.BuildOptions{})
-	defer bal.Close()
-	bal.ResolverError(errors.New("resolution failed: test error"))
-
-	if err := cc.WaitForConnectivityState(ctx, connectivity.TransientFailure); err != nil {
-		t.Fatalf("cc.WaitForConnectivityState(%v) returned error: %v", connectivity.TransientFailure, err)
-	}
-
-	// After sending a valid update, the LB policy should report CONNECTING.
-	ccState := balancer.ClientConnState{
-		ResolverState: resolver.State{
-			Endpoints: []resolver.Endpoint{
-				{Addresses: []resolver.Address{{Addr: "1.1.1.1:1"}}},
-				{Addresses: []resolver.Address{{Addr: "2.2.2.2:2"}}},
-			},
-		},
-	}
-	if err := bal.UpdateClientConnState(ccState); err != nil {
-		t.Fatalf("UpdateClientConnState(%v) returned error: %v", ccState, err)
-	}
-
-	if err := cc.WaitForConnectivityState(ctx, connectivity.Connecting); err != nil {
-		t.Fatalf("cc.WaitForConnectivityState(%v) returned error: %v", connectivity.Connecting, err)
-	}
-}
-
-// TestPickFirstLeaf_ResolverErrorinTF sends a resolver error to the balancer
-// before when it's attempting to connect to a SubConn TRANSIENT_FAILURE. It
-// verifies that the picker is updated and the SubConn is not closed.
-func (s) TestPickFirstLeaf_ResolverErrorinTF(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cancel()
-	cc := testutils.NewBalancerClientConn(t)
-	bal := pickfirstBuilder{}.Build(cc, balancer.BuildOptions{})
-	defer bal.Close()
-
-	// After sending a valid update, the LB policy should report CONNECTING.
-	ccState := balancer.ClientConnState{
-		ResolverState: resolver.State{
-			Endpoints: []resolver.Endpoint{
-				{Addresses: []resolver.Address{{Addr: "1.1.1.1:1"}}},
-			},
-		},
-	}
-
-	if err := bal.UpdateClientConnState(ccState); err != nil {
-		t.Fatalf("UpdateClientConnState(%v) returned error: %v", ccState, err)
-	}
-
-	sc1 := <-cc.NewSubConnCh
-	if err := cc.WaitForConnectivityState(ctx, connectivity.Connecting); err != nil {
-		t.Fatalf("cc.WaitForConnectivityState(%v) returned error: %v", connectivity.Connecting, err)
-	}
-
-	scErr := fmt.Errorf("test error: connection refused")
-	sc1.UpdateState(balancer.SubConnState{
-		ConnectivityState: connectivity.TransientFailure,
-		ConnectionError:   scErr,
-	})
-
-	if err := cc.WaitForPickerWithErr(ctx, scErr); err != nil {
-		t.Fatalf("cc.WaitForPickerWithErr(%v) returned error: %v", scErr, err)
-	}
-
-	bal.ResolverError(errors.New("resolution failed: test error"))
-	if err := cc.WaitForErrPicker(ctx); err != nil {
-		t.Fatalf("cc.WaitForPickerWithErr() returned error: %v", err)
-	}
-
-	select {
-	case <-time.After(defaultTestShortTimeout):
-	case sc := <-cc.ShutdownSubConnCh:
-		t.Fatalf("Unexpected SubConn shutdown: %v", sc)
 	}
 }
