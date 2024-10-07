d97740d0dfc666e45b1d8a2f890d4520905c05ba
eshitachandwani <59800922+eshitachandwani@users.noreply.github.com>
2024-10-07 11:52:03 +0530

    Update metadata/client/main.go

--- examples/features/metadata/client/main.go
+++ examples/features/metadata/client/main.go
@@ -16,7 +16,7 @@
  *
  */
 
-// Binary client demonstrates how to send and receive metadata to and from RPC.
+// Binary client demonstrates how to send and receive metadata to and from an RPC.
 package main
 
 import (
