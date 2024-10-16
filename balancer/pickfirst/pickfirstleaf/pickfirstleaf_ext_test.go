0a3ffd330d31ae4632bb1efc13cefb1111145495
Arjan Bal <arjansbal@google.com>
2024-10-16 16:15:09 +0530

    Change to e2e style test

--- balancer/pickfirst/pickfirstleaf/pickfirstleaf_ext_test.go
+++ balancer/pickfirst/pickfirstleaf/pickfirstleaf_ext_test.go
@@ -21,14 +21,17 @@ package pickfirstleaf_test
 import (
 	"context"
 	"fmt"
+	"net"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"golang.org/x/net/http2"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/balancer"
+	pfinternal "google.golang.org/grpc/balancer/pickfirst/internal"
 	"google.golang.org/grpc/balancer/pickfirst/pickfirstleaf"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/connectivity"
@@ -851,6 +854,268 @@ func (s) TestPickFirstLeaf_EmptyAddressList(t *testing.T) {
 	}
 }
 
+// TestPickFirstLeaf_HappyEyeballs_TFAfterEndOfList verifies that pickfirst
+// correctly detects the end of the first happy eyeballs pass when the timer
+// causes pickfirst to reach the end of the address list and failures are
+// reported out of order.
+func (s) TestPickFirstLeaf_HappyEyeballs_TFAfterEndOfList(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+
+	timerCh := make(chan struct{})
+	originalTimer := pfinternal.TimeAfterFunc
+	pfinternal.TimeAfterFunc = func(_ time.Duration, f func()) *time.Timer {
+		// Set a really long expiration to prevent it from triggering
+		// automatically.
+		ret := time.AfterFunc(time.Hour, f)
+		go func() {
+			select {
+			case <-ctx.Done():
+			case <-timerCh:
+			}
+			ret.Reset(0)
+		}()
+		return ret
+	}
+
+	defer func() {
+		pfinternal.TimeAfterFunc = originalTimer
+	}()
+
+	defer func() {
+		pfinternal.TimeAfterFunc = originalTimer
+	}()
+
+	servers := newHangingServerGroup(t, 3)
+	defer servers.close()
+	rb := manual.NewBuilderWithScheme("whatever")
+	rb.InitialState(resolver.State{Addresses: servers.addrs})
+	cc, err := grpc.NewClient("whatever:///this-gets-overwritten",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"loadBalancingConfig": [{"%s":{}}]}`, pickfirstleaf.Name)),
+		grpc.WithResolvers(rb))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cc.Close()
+	cc.Connect()
+
+	testutils.AwaitState(ctx, t, cc, connectivity.Connecting)
+
+	// Verify that only the first server is contacted.
+	if err := servers.awaitContacted(ctx, 0); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[0], err)
+	}
+
+	// Ensure no other servers are contacted.
+	if got, want := servers.isContacted(1), false; got != want {
+		t.Fatalf("Servers.isContacted(%q) = %t, want %t", servers.addrs[1], got, want)
+	}
+	if got, want := servers.isContacted(2), false; got != want {
+		t.Fatalf("Servers.isContacted(%q) = %t, want %t", servers.addrs[2], got, want)
+	}
+
+	// Make the happy eyeballs timer fire twice so that pickfirst reaches the
+	// last address in the list.
+	timerCh <- struct{}{}
+
+	// Verify that the second server is contacted and 3rd isn't.
+	if err := servers.awaitContacted(ctx, 1); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[1], err)
+	}
+
+	if got, want := servers.isContacted(2), false; got != want {
+		t.Fatalf("Servers.isContacted(%q) = %t, want %t", servers.addrs[2], got, want)
+	}
+	timerCh <- struct{}{}
+	if err := servers.awaitContacted(ctx, 2); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[2], err)
+	}
+
+	// First SubConn Fails.
+	servers.closeConn(0)
+
+	// No TF should be reported until the first pass is complete.
+	shortCtx, shortCancel := context.WithTimeout(ctx, defaultTestShortTimeout)
+	defer shortCancel()
+
+	testutils.AwaitNotState(shortCtx, t, cc, connectivity.TransientFailure)
+
+	// Move off the end of the list, pickfirst should still be waiting for TFs
+	// to be reported.
+	timerCh <- struct{}{}
+
+	// Third SubConn fails.
+	servers.closeConn(2)
+
+	testutils.AwaitNotState(shortCtx, t, cc, connectivity.TransientFailure)
+
+	// Last SubConn fails, this should result in a TF update.
+	servers.closeConn(1)
+	testutils.AwaitState(ctx, t, cc, connectivity.TransientFailure)
+}
+
+// TestPickFirstLeaf_HappyEyeballs_TriggerConnectionDelay verifies that
+// pickfirst attempts to connect to the second backend once the happy eyeballs
+// timer expires.
+func (s) TestPickFirstLeaf_HappyEyeballs_TriggerConnectionDelay(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+
+	timerCh := make(chan struct{})
+	originalTimer := pfinternal.TimeAfterFunc
+	pfinternal.TimeAfterFunc = func(_ time.Duration, f func()) *time.Timer {
+		// Set a really long expiration to prevent it from triggering
+		// automatically.
+		ret := time.AfterFunc(time.Hour, f)
+		go func() {
+			select {
+			case <-ctx.Done():
+			case <-timerCh:
+			}
+			ret.Reset(0)
+		}()
+		return ret
+	}
+
+	defer func() {
+		pfinternal.TimeAfterFunc = originalTimer
+	}()
+
+	defer func() {
+		pfinternal.TimeAfterFunc = originalTimer
+	}()
+
+	servers := newHangingServerGroup(t, 2)
+	defer servers.close()
+	rb := manual.NewBuilderWithScheme("whatever")
+	rb.InitialState(resolver.State{Addresses: servers.addrs})
+	cc, err := grpc.NewClient("whatever:///this-gets-overwritten",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"loadBalancingConfig": [{"%s":{}}]}`, pickfirstleaf.Name)),
+		grpc.WithResolvers(rb))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cc.Close()
+	cc.Connect()
+
+	testutils.AwaitState(ctx, t, cc, connectivity.Connecting)
+
+	// Verify that the first server is contacted.
+	if err := servers.awaitContacted(ctx, 0); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[0], err)
+	}
+
+	if got, want := servers.isContacted(1), false; got != want {
+		t.Fatalf("Servers.isContacted(%q) = %t, want %t", servers.addrs[1], got, want)
+	}
+
+	timerCh <- struct{}{}
+
+	// Second connection attempt is successful.
+	if err := servers.awaitContacted(ctx, 1); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[1], err)
+	}
+	servers.enterReady(1)
+	testutils.AwaitState(ctx, t, cc, connectivity.Ready)
+}
+
+// TestPickFirstLeaf_HappyEyeballs_TFThenTimerFires tests the pickfirst balancer
+// by causing a SubConn to fail and then jumping to the 3rd SubConn after the
+// happy eyeballs timer expires.
+func (s) TestPickFirstLeaf_HappyEyeballs_TFThenTimerFires(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+
+	timerMu := sync.Mutex{}
+	timerCh := make(chan struct{})
+	originalTimer := pfinternal.TimeAfterFunc
+	pfinternal.TimeAfterFunc = func(_ time.Duration, f func()) *time.Timer {
+		// Set a really long expiration to prevent it from triggering
+		// automatically.
+		ret := time.AfterFunc(time.Hour, f)
+		go func() {
+			timerMu.Lock()
+			ch := timerCh
+			timerMu.Unlock()
+			select {
+			case <-ctx.Done():
+			case <-ch:
+			}
+			ret.Reset(0)
+		}()
+		return ret
+	}
+
+	defer func() {
+		pfinternal.TimeAfterFunc = originalTimer
+	}()
+
+	servers := newHangingServerGroup(t, 3)
+	defer servers.close()
+	rb := manual.NewBuilderWithScheme("whatever")
+	rb.InitialState(resolver.State{Addresses: servers.addrs})
+	cc, err := grpc.NewClient("whatever:///this-gets-overwritten",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"loadBalancingConfig": [{"%s":{}}]}`, pickfirstleaf.Name)),
+		grpc.WithResolvers(rb))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cc.Close()
+	cc.Connect()
+
+	testutils.AwaitState(ctx, t, cc, connectivity.Connecting)
+
+	// Verify that only the first server is contacted.
+	if err := servers.awaitContacted(ctx, 0); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[0], err)
+	}
+
+	// Ensure no other servers are contacted.
+	if got, want := servers.isContacted(1), false; got != want {
+		t.Fatalf("Servers.isContacted(%q) = %t, want %t", servers.addrs[1], got, want)
+	}
+	if got, want := servers.isContacted(2), false; got != want {
+		t.Fatalf("Servers.isContacted(%q) = %t, want %t", servers.addrs[2], got, want)
+	}
+
+	// First SubConn Fails.
+	// Replace the timer channel so that the old timers don't attempt to read
+	// messages pushed next.
+	timerMu.Lock()
+	timerCh = make(chan struct{})
+	timerMu.Unlock()
+	servers.closeConn(0)
+
+	// The second server is contacted.
+	// Verify that only the first server is contacted.
+	if err := servers.awaitContacted(ctx, 1); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[1], err)
+	}
+
+	// Ensure no other servers are contacted.
+	if got, want := servers.isContacted(2), false; got != want {
+		t.Fatalf("Servers.isContacted(%q) = %t, want %t", servers.addrs[2], got, want)
+	}
+
+	// The happy eyeballs timer expires, skipping server[1] and requesting the creation
+	// of a third SubConn.
+	timerCh <- struct{}{}
+
+	if err := servers.awaitContacted(ctx, 2); err != nil {
+		t.Fatalf("Server with address %q not contacted: %v", servers.addrs[2], err)
+	}
+
+	// Second SubConn connects.
+	servers.enterReady(1)
+	testutils.AwaitState(ctx, t, cc, connectivity.Connecting)
+}
+
 // stateStoringBalancer stores the state of the subconns being created.
 type stateStoringBalancer struct {
 	balancer.Balancer
@@ -954,4 +1219,101 @@ type ccStateSubscriber struct {
 
 func (c *ccStateSubscriber) OnMessage(msg any) {
 	c.transitions = append(c.transitions, msg.(connectivity.State))
+}
+
+// handingServerGroup is a group of servers that accept a TCP connection and
+// remain idle until asked to close the connection. They can be used to control
+// how long it takes for a subchannel to report a TRANSIENT_FAILURE in tests.
+type handingServerGroup struct {
+	addrs                []resolver.Address
+	listeners            []net.Listener
+	serverConnCloseFuncs []func()
+	serverContacted      []*grpcsync.Event
+	readyChans           []chan struct{}
+}
+
+func newHangingServerGroup(t *testing.T, count int) *handingServerGroup {
+	listeners := []net.Listener{}
+	closeFns := []func(){}
+	addrs := []resolver.Address{}
+	contactedEvents := []*grpcsync.Event{}
+	readyChans := []chan struct{}{}
+
+	for i := 0; i < count; i++ {
+		lis, err := net.Listen("tcp", "localhost:0")
+		if err != nil {
+			t.Fatalf("Error while listening. Err: %v", err)
+		}
+		listeners = append(listeners, lis)
+		addrs = append(addrs, resolver.Address{Addr: lis.Addr().String()})
+		closeChan := make(chan struct{})
+		closeFns = append(closeFns, sync.OnceFunc(func() { close(closeChan) }))
+		contacted := grpcsync.NewEvent()
+		contactedEvents = append(contactedEvents, contacted)
+		readyChan := make(chan struct{})
+		readyChans = append(readyChans, readyChan)
+
+		go func() {
+			conn, err := lis.Accept()
+			defer conn.Close()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			contacted.Fire()
+
+			for {
+				select {
+				case <-closeChan:
+					return
+				case <-readyChan:
+					framer := http2.NewFramer(conn, conn)
+					if err := framer.WriteSettings(http2.Setting{}); err != nil {
+						t.Fatalf("Error while writing settings frame. %v", err)
+						return
+					}
+
+				}
+			}
+		}()
+	}
+
+	return &handingServerGroup{
+		addrs:                addrs,
+		listeners:            listeners,
+		serverConnCloseFuncs: closeFns,
+		serverContacted:      contactedEvents,
+		readyChans:           readyChans,
+	}
+}
+
+func (hg *handingServerGroup) close() {
+	for _, fn := range hg.serverConnCloseFuncs {
+		fn()
+	}
+	for _, l := range hg.listeners {
+		l.Close()
+	}
+}
+
+func (hg *handingServerGroup) closeConn(serverIdx int) {
+	hg.serverConnCloseFuncs[serverIdx]()
+}
+
+func (hg *handingServerGroup) isContacted(serverIdx int) bool {
+	return hg.serverContacted[serverIdx].HasFired()
+}
+
+func (hg *handingServerGroup) enterReady(serverIdx int) {
+	hg.readyChans[serverIdx] <- struct{}{}
+}
+
+func (hg *handingServerGroup) awaitContacted(ctx context.Context, serverIdx int) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-hg.serverContacted[serverIdx].Done():
+	}
+	return nil
 }
