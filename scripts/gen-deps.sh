9eb9a16d6823585c01bc1892e93660aa99ac0d6e
Doug Fawley <dfawley@google.com>
2024-10-23 15:43:44 -0700

    github: add dependency checking workflow

---
+++ scripts/gen-deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e          # Exit on error
+set -o pipefail # Fail a pipe if any sub-command fails.
+
+if [[ "$#" -ne 1 || ! -d "$1" ]]; then
+    echo "Specify a valid output directory as the first parameter."
+    exit 1
+fi
+
+SCRIPTS_DIR="$(dirname "$0")"
+OUTPUT_DIR="$1"
+
+cd "${SCRIPTS_DIR}/.."
+
+git ls-files -- '*.go' | grep -v '\(^\|/\)\(internal\|examples\|benchmark\|interop\|test\|testdata\)\(/\|$\)' | xargs dirname | sort -u | while read d; do
+  pushd "$d" > /dev/null
+  pkg="$(echo "$d" | sed 's;\.;grpc;' | sed 's;/;_;g')"
+  go list -deps . | sort >| "${OUTPUT_DIR}/$pkg"
+  popd > /dev/null
+done
