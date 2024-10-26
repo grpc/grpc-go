309dbf6efb83ad294c7b26315e961303bd3aba98
Purnesh Dixit <purneshdixit@google.com>
2024-10-26 23:54:24 +0530

    address testing comments of merging in t-test and updating top level comments

--- stats/opentelemetry/internal/grpc_trace_bin_propagator_test.go
+++ stats/opentelemetry/internal/grpc_trace_bin_propagator_test.go
@@ -42,116 +42,157 @@ func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-// TestInject_FastPath verifies that the GRPCTraceBinPropagator correctly
-// injects OpenTelemetry trace context data using the CustomCarrier.
+// TestInject verifies that the GRPCTraceBinPropagator correctly injects
+// OpenTelemetry span context as `grpc-trace-bin` header in the provided
+// carrier's context, for both fast and slow path, if span context is valid. If
+// span context is invalid, carrier's context is not modified which is verified
+// by retrieving zero value span context from carrier's context.
 //
-// It is called the fast path because it injects the trace context directly in
-// binary format.
-func (s) TestInject_FastPath(t *testing.T) {
-	p := GRPCTraceBinPropagator{}
-	sc := oteltrace.NewSpanContext(oteltrace.SpanContextConfig{
-		TraceID:    [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-		SpanID:     [8]byte{17, 18, 19, 20, 21, 22, 23, 24},
-		TraceFlags: oteltrace.FlagsSampled,
-	})
-	tCtx, tCancel := context.WithCancel(context.Background())
-	tCtx = oteltrace.ContextWithSpanContext(tCtx, sc)
-	defer tCancel()
+// For fast path, it passes `CustomCarrier` as carrier to `Inject()` which
+// injects the span context using `CustomCarrier.SetBinary()`. It then
+// retrieves the injected span context using `stats.OutgoingTrace()` for
+// verification because `SetBinary()` does injection directly in binary format.
+//
+// For slow path, it passes `otel.MapCarrier` as carrier to `Inject()` which
+// injects the span context after base64 encoding as string using
+// `carrier.Set()`. It then retrieves the injected span context using
+// `carrier.Get()` for verification because `Set()` does injection in string
+// format.
+func (s) TestInject(t *testing.T) {
+	tests := []struct {
+		name string
+		sc   oteltrace.SpanContext
+		fast bool
+	}{
+		{
+			name: "fast path, valid context",
+			sc: oteltrace.SpanContext{}.WithTraceID(
+				oteltrace.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}).WithSpanID(
+				oteltrace.SpanID{17, 18, 19, 20, 21, 22, 23, 24}).WithTraceFlags(
+				oteltrace.TraceFlags(1)),
+			fast: true,
+		},
+		{
+			name: "fast path, invalid context",
+			sc:   oteltrace.SpanContext{},
+			fast: true,
+		},
+		{
+			name: "slow path, valid context",
+			sc: oteltrace.SpanContext{}.WithTraceID(
+				oteltrace.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}).WithSpanID(
+				oteltrace.SpanID{17, 18, 19, 20, 21, 22, 23, 24}).WithTraceFlags(
+				oteltrace.TraceFlags(1)),
+			fast: false,
+		},
+		{
+			name: "slow path, invalid context",
+			sc:   oteltrace.SpanContext{},
+			fast: false,
+		},
+	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	c := itracing.NewCustomCarrier(metadata.NewOutgoingContext(ctx, metadata.MD{}))
-	p.Inject(tCtx, c)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			p := GRPCTraceBinPropagator{}
+			tCtx, tCancel := context.WithCancel(context.Background())
+			tCtx = oteltrace.ContextWithSpanContext(tCtx, test.sc)
+			defer tCancel()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-	got := stats.OutgoingTrace(c.Context())
-	want := binary(sc)
-	if string(got) != string(want) {
-		t.Fatalf("got = %v, want %v", got, want)
+			var c otelpropagation.TextMapCarrier
+			if test.fast {
+				c = itracing.NewCustomCarrier(metadata.NewOutgoingContext(ctx, metadata.MD{}))
+			} else {
+				c = otelpropagation.MapCarrier{}
+			}
+			p.Inject(tCtx, c)
+			var got oteltrace.SpanContext
+			if test.fast {
+				got, _ = fromBinary(stats.OutgoingTrace(c.(*itracing.CustomCarrier).Context()))
+			} else {
+				b, _ := base64.StdEncoding.DecodeString(c.Get(itracing.GRPCTraceBinHeaderKey))
+				got, _ = fromBinary(b)
+			}
+			if test.sc.TraceID() != got.TraceID() && test.sc.SpanID() != got.SpanID() && test.sc.TraceFlags() != got.TraceFlags() {
+				t.Fatalf("got = %v, want %v", got, test.sc)
+			}
+		})
 	}
 }
 
-// TestInject_SlowPath verifies that the GRPCTraceBinPropagator correctly
-// injects OpenTelemetry trace context data using any other text based carrier.
+// TestExtract verifies that the GRPCTraceBinPropagator correctly extracts
+// OpenTelemetry span context data for fast and slow path, if valid span
+// context was injected. If invalid context was injected, it verifies that a
+// zero value span context was retrieved.
 //
-// It is called the slow path because it base64 encodes the binary trace
-// context before injecting it.
-func (s) TestInject_SlowPath(t *testing.T) {
-	p := GRPCTraceBinPropagator{}
-	sc := oteltrace.NewSpanContext(oteltrace.SpanContextConfig{
-		TraceID:    [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-		SpanID:     [8]byte{17, 18, 19, 20, 21, 22, 23, 24},
-		TraceFlags: oteltrace.FlagsSampled,
-	})
-	tCtx, tCancel := context.WithCancel(context.Background())
-	tCtx = oteltrace.ContextWithSpanContext(tCtx, sc)
-	defer tCancel()
-
-	c := otelpropagation.MapCarrier{}
-	p.Inject(tCtx, c)
-
-	got := c.Get(itracing.GRPCTraceBinHeaderKey)
-	want := base64.StdEncoding.EncodeToString(binary(sc))
-	if got != want {
-		t.Fatalf("got = %v, want %v", got, want)
-	}
-}
-
-// TestExtract_FastPath verifies that the GRPCTraceBinPropagator correctly
-// extracts OpenTelemetry trace context data using the CustomCarrier.
+// For fast path, it uses the CustomCarrier and sets the span context in the
+// binary format using `stats.SetIncomingTrace` in its context and then
+// verifies that same span context is extracted directly in binary foramt.
 //
-// It is called the fast path because it extracts the trace context directly
-// in the binary format.
-func (s) TestExtract_FastPath(t *testing.T) {
-	p := GRPCTraceBinPropagator{}
-	sc := oteltrace.NewSpanContext(oteltrace.SpanContextConfig{
-		TraceID:    [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-		SpanID:     [8]byte{17, 18, 19, 20, 21, 22, 23, 24},
-		TraceFlags: oteltrace.FlagsSampled,
-		Remote:     true,
-	})
-	bd := binary(sc)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	c := itracing.NewCustomCarrier(stats.SetIncomingTrace(ctx, bd))
-	tCtx := p.Extract(ctx, c)
-	got := oteltrace.SpanContextFromContext(tCtx)
-
-	if !got.Equal(sc) {
-		t.Fatalf("got = %v, want %v", got, sc)
+// For slow path, it uses a MapCarrier and sets base64 encoded span context in
+// its context and then verifies that same span context is extracted after
+// base64 decoding.
+func (s) TestExtract(t *testing.T) {
+	tests := []struct {
+		name string
+		sc   oteltrace.SpanContext
+		fast bool
+	}{
+		{
+			name: "fast path, valid context",
+			sc: oteltrace.SpanContext{}.WithTraceID(
+				oteltrace.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}).WithSpanID(
+				oteltrace.SpanID{17, 18, 19, 20, 21, 22, 23, 24}).WithTraceFlags(
+				oteltrace.TraceFlags(1)).WithRemote(true),
+			fast: true,
+		},
+		{
+			name: "fast path, invalid context",
+			sc:   oteltrace.SpanContext{},
+			fast: true,
+		},
+		{
+			name: "slow path, valid context",
+			sc: oteltrace.SpanContext{}.WithTraceID(
+				oteltrace.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}).WithSpanID(
+				oteltrace.SpanID{17, 18, 19, 20, 21, 22, 23, 24}).WithTraceFlags(
+				oteltrace.TraceFlags(1)).WithRemote(true),
+			fast: false,
+		},
+		{
+			name: "slow path, invalid context",
+			sc:   oteltrace.SpanContext{},
+			fast: false,
+		},
 	}
-}
 
-// TestExtract_SlowPath verifies that the GRPCTraceBinPropagator correctly
-// extracts OpenTelemetry trace context data using any other text based carrier.
-//
-// It is called the slow path because it base64 decodes the binary trace
-// context before extracting it.
-func (s) TestExtract_SlowPath(t *testing.T) {
-	p := GRPCTraceBinPropagator{}
-	sc := oteltrace.NewSpanContext(oteltrace.SpanContextConfig{
-		TraceID:    [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-		SpanID:     [8]byte{17, 18, 19, 20, 21, 22, 23, 24},
-		TraceFlags: oteltrace.FlagsSampled,
-		Remote:     true,
-	})
-	bd := binary(sc)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			p := GRPCTraceBinPropagator{}
+			bd := binary(test.sc)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-	c := otelpropagation.MapCarrier{
-		itracing.GRPCTraceBinHeaderKey: base64.StdEncoding.EncodeToString(bd),
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	tCtx := p.Extract(ctx, c)
-	got := oteltrace.SpanContextFromContext(tCtx)
-
-	if !got.Equal(sc) {
-		t.Fatalf("got = %v, want %v", got, sc)
+			var c otelpropagation.TextMapCarrier
+			if test.fast {
+				c = itracing.NewCustomCarrier(stats.SetIncomingTrace(ctx, bd))
+			} else {
+				c = otelpropagation.MapCarrier{itracing.GRPCTraceBinHeaderKey: base64.StdEncoding.EncodeToString(bd)}
+			}
+			tCtx := p.Extract(ctx, c)
+			got := oteltrace.SpanContextFromContext(tCtx)
+			if !got.Equal(test.sc) {
+				t.Fatalf("got = %v, want %v", got, test.sc)
+			}
+		})
 	}
 }
 
 // TestBinary verifies that the binary() function correctly serializes a valid
-// OpenTelemetry SpanContext into its binary format representation.
+// OpenTelemetry span context into its binary format representation. If span
+// context is invalid, it verifies that serialization is nil.
 func (s) TestBinary(t *testing.T) {
 	tests := []struct {
 		name string
@@ -159,34 +200,37 @@ func (s) TestBinary(t *testing.T) {
 		want []byte
 	}{
 		{
-			name: "valid",
+			name: "valid context",
 			sc: oteltrace.SpanContext{}.WithTraceID(
-				oteltrace.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-			).WithSpanID(
-				oteltrace.SpanID{17, 18, 19, 20, 21, 22, 23, 24},
-			).WithTraceFlags(
-				oteltrace.TraceFlags(1),
-			),
-			want: []byte{0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 17, 18, 19, 20, 21, 22, 23, 24, 2, 1},
+				oteltrace.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}).WithSpanID(
+				oteltrace.SpanID{17, 18, 19, 20, 21, 22, 23, 24}).WithTraceFlags(
+				oteltrace.TraceFlags(1)),
+			want: binary(oteltrace.SpanContext{}.WithTraceID(
+				oteltrace.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}).WithSpanID(
+				oteltrace.SpanID{17, 18, 19, 20, 21, 22, 23, 24}).WithTraceFlags(
+				oteltrace.TraceFlags(1))),
 		},
 		{
-			name: "zero value",
+			name: "zero value context",
 			sc:   oteltrace.SpanContext{},
 			want: nil,
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := binary(tt.sc); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("binary() = %v, want %v", got, tt.want)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got := binary(test.sc); !reflect.DeepEqual(got, test.want) {
+				t.Errorf("binary() = %v, want %v", got, test.want)
 			}
 		})
 	}
 }
 
-// TestFromBinary verifies that the fromBinary function correctly deserializes
-// a binary format representation of a valid OpenTelemetry SpanContext into its
-// corresponding SpanContext.
+// TestFromBinary verifies that the fromBinary() function correctly
+// deserializes a binary format representation of a valid OpenTelemetry span
+// context into its corresponding span context format. If span context's binary
+// representation is invalid, it verifies that deserialization is zero value
+// span context.
 func (s) TestFromBinary(t *testing.T) {
 	tests := []struct {
 		name string
@@ -237,15 +281,16 @@ func (s) TestFromBinary(t *testing.T) {
 			ok:   false,
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, ok := fromBinary(tt.b)
-			if ok != tt.ok {
-				t.Errorf("fromBinary() ok = %v, want %v", ok, tt.ok)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, ok := fromBinary(test.b)
+			if ok != test.ok {
+				t.Errorf("fromBinary() ok = %v, want %v", ok, test.ok)
 				return
 			}
-			if !got.Equal(tt.want) {
-				t.Errorf("fromBinary() got = %v, want %v", got, tt.want)
+			if !got.Equal(test.want) {
+				t.Errorf("fromBinary() got = %v, want %v", got, test.want)
 			}
 		})
 	}
--- stats/opentelemetry/internal/tracing/custom_carrier_test.go
+++ stats/opentelemetry/internal/tracing/custom_carrier_test.go
@@ -22,6 +22,7 @@ import (
 	"context"
 	"encoding/base64"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -39,6 +40,24 @@ func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
+func verifyOutgoingTraceGRPCTraceBinHeader(ctx context.Context, t *testing.T, wantB []byte) {
+	gotB := stats.OutgoingTrace(ctx)
+	if len(wantB) == 0 && gotB != nil {
+		t.Fatalf("stats.OutgoingTrace() is non-nil, want nil")
+	}
+	if gotB != nil && !reflect.DeepEqual(gotB, wantB) {
+		t.Fatalf("stats.OutgoingTrace() = %v, want %v", gotB, wantB)
+	}
+}
+
+// TestGet verifies that `CustomCarrier.Get()` returns correct value for the
+// corresponding key in the carrier's context, if key is present. If key is not
+// present, it verifies that empty string is returned.
+//
+// If key ends with `-bin`, it verifies that a correct base64 encoded
+// string value is returned for the `grpc-trace-bin` header's binary value, if
+// present. For any other `-bin` key, it verifies that an empty string is
+// returned.
 func (s) TestGet(t *testing.T) {
 	tests := []struct {
 		name string
@@ -65,26 +84,47 @@ func (s) TestGet(t *testing.T) {
 			want: "",
 		},
 		{
+			name: "grpc-trace-bin key",
+			md:   metadata.MD{},
+			key:  "grpc-trace-bin",
+			want: base64.StdEncoding.EncodeToString([]byte{0x01, 0x02, 0x03}),
+		},
+		{
 			name: "non-grpc-trace-bin key",
-			md:   metadata.Pairs("non-trace-bin", "\x01\x02\x03"),
+			md:   metadata.Pairs("non-trace-bin", base64.StdEncoding.EncodeToString([]byte{0x01, 0x02, 0x03})),
 			key:  "non-trace-bin",
-			want: "",
+			want: "", // -bin key that isn't grpc-trace-bin should always get empty string
 		},
 	}
 
 	for _, test := range tests {
-		ctx, cancel := context.WithCancel(context.Background())
 		t.Run(test.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			if strings.HasSuffix(test.key, "-bin") { // for binary headers set `grpc-trace-bin` using `stats.SetIncomingTrace()`
+				b, _ := base64.StdEncoding.DecodeString(test.want)
+				ctx = stats.SetIncomingTrace(ctx, b)
+			}
 			c := NewCustomCarrier(metadata.NewIncomingContext(ctx, test.md))
 			got := c.Get(test.key)
 			if got != test.want {
 				t.Fatalf("Get() = %s, want %s", got, test.want)
 			}
-			cancel()
 		})
 	}
 }
 
+// TestSet verifies that a key-value pair is set in carrier's context using
+// `CustomCarrier.Set()`. If key is not present, it verifies that key-value
+// pair is insterted. If key is already present, it verifies that value is
+// overwritten.
+//
+// If key ends with `-bin`, it verifies that a binary value is set for
+// `grpc-trace-bin` header, if key is `grpc-trace-bin` and value is base64
+// encoded. For any other `-bin` key, it verifies that no binary value is set.
+//
+// It also verifies that both existing and newly inserted keys are present in
+// the carrier's context using `CustomCarrier.Keys()`.
 func (s) TestSet(t *testing.T) {
 	tests := []struct {
 		name      string
@@ -119,11 +159,19 @@ func (s) TestSet(t *testing.T) {
 			wantKeys:  []string{"key2", "key1"},
 		},
 		{
-			name:      "set non-grcp-trace-bin binary key",
+			name:      "set grpc-trace-bin binary key",
 			initialMD: metadata.MD{"key1": []string{"value1"}},
-			setKey:    "non-grcp-trace-bin",
+			setKey:    "grpc-trace-bin",
 			setValue:  base64.StdEncoding.EncodeToString([]byte{0x01, 0x02, 0x03}),
-			wantValue: "", // non-grcp-trace-bin key should not be set
+			wantValue: base64.StdEncoding.EncodeToString([]byte{0x01, 0x02, 0x03}),
+			wantKeys:  []string{"key1", "grpc-trace-bin"},
+		},
+		{
+			name:      "set non-grpc-trace-bin binary key",
+			initialMD: metadata.MD{"key1": []string{"value1"}},
+			setKey:    "non-grpc-trace-bin",
+			setValue:  base64.StdEncoding.EncodeToString([]byte{0x01, 0x02, 0x03}),
+			wantValue: "", // -bin key that isn't grpc-trace-bin should never be set
 			wantKeys:  []string{"key1"},
 		},
 	}
@@ -131,76 +179,47 @@ func (s) TestSet(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 			c := NewCustomCarrier(metadata.NewOutgoingContext(ctx, test.initialMD))
-
 			c.Set(test.setKey, test.setValue)
-
 			gotKeys := c.Keys()
-			equalKeys := cmp.Equal(test.wantKeys, gotKeys, cmpopts.SortSlices(func(a, b string) bool {
-				return a < b
-			}))
+			equalKeys := cmp.Equal(test.wantKeys, gotKeys, cmpopts.SortSlices(func(a, b string) bool { return a < b }))
 			if !equalKeys {
-				t.Fatalf("got keys %v, want %v", gotKeys, test.wantKeys)
+				t.Fatalf("c.Keys() = keys %v, want %v", gotKeys, test.wantKeys)
 			}
-			gotMD, _ := metadata.FromOutgoingContext(c.Context())
-			if test.wantValue != "" && gotMD.Get(test.setKey)[0] != test.setValue {
+			// for binary headers verify `grpc-trace-bin` binary value for outgoing trace in carrier's context
+			if strings.HasSuffix(test.setKey, "-bin") {
+				wantB, _ := base64.StdEncoding.DecodeString(test.wantValue)
+				verifyOutgoingTraceGRPCTraceBinHeader(c.ctx, t, wantB)
+				return
+			}
+			// for non -bin headers verify string value in carrier's context metadata
+			if gotMD, _ := metadata.FromOutgoingContext(c.Context()); test.wantValue != "" && gotMD.Get(test.setKey)[0] != test.setValue {
 				t.Fatalf("got value %s, want %s, for key %s", gotMD.Get(test.setKey)[0], test.setValue, test.setKey)
 			}
-			cancel()
 		})
 	}
 }
 
-func (s) TestGetBinary_GRPCTraceBinHeaderKey(t *testing.T) {
+func (s) TestGetBinary(t *testing.T) {
 	want := []byte{0x01, 0x02, 0x03}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	c := NewCustomCarrier(stats.SetIncomingTrace(ctx, want))
-
 	got := c.GetBinary()
 	if got == nil {
-		t.Fatalf("GetBinary() = nil, want %v", got)
+		t.Fatalf("c.GetBinary() = nil, want %v", got)
 	}
-	if string(got) != string(want) {
-		t.Fatalf("GetBinary() = %s, want %s", got, want)
-	}
-}
-
-func TestGet_GRPCTraceBinHeaderKey(t *testing.T) {
-	tc := []byte{0x01, 0x02, 0x03}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	c := NewCustomCarrier(stats.SetIncomingTrace(ctx, tc))
-
-	got := c.Get(GRPCTraceBinHeaderKey)
-	want := base64.StdEncoding.EncodeToString(tc)
-	if got != want {
-		t.Fatalf("Get() = %s, want %s", got, want)
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("c.GetBinary() = %s, want %s", got, want)
 	}
 }
 
-func (s) TestSetBinary_GRPCTraceBinHeaderKey(t *testing.T) {
+func (s) TestSetBinary(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	want := []byte{0x01, 0x02, 0x03}
 	c := NewCustomCarrier(ctx)
-
 	c.SetBinary(want)
-	got := stats.OutgoingTrace(c.Context())
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("got %v, want %v", got, want)
-	}
-}
-
-func (s) TestSet_GRPCTraceBinHeaderKey(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	want := []byte{0x01, 0x02, 0x03}
-	c := NewCustomCarrier(ctx)
-
-	c.Set(GRPCTraceBinHeaderKey, base64.StdEncoding.EncodeToString(want))
-	got := stats.OutgoingTrace(c.Context())
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("got %v, want %v", got, want)
-	}
+	verifyOutgoingTraceGRPCTraceBinHeader(c.Context(), t, want)
 }
