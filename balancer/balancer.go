43ee17261c66304b4e7777e701224813d8ca28be
Arjan Singh Bal <46515553+arjan-bal@users.noreply.github.com>
2024-11-05 23:22:26 +0530

    balancer: Enforce embedding the SubConn interface in implementations (#7758)

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -155,6 +155,9 @@ type SubConn interface {
 	// indicate the shutdown operation.  This may be delivered before
 	// in-progress RPCs are complete and the actual connection is closed.
 	Shutdown()
+	// enforceEmbedding is an unexported method to force implementers embed
+	// this interface, allowing gRPC to add methods without breaking users.
+	enforceEmbedding()
 }
 
 // NewSubConnOptions contains options to create new SubConn.
