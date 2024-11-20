cffba1de71b85ef819adb0c88a29eb3b6fe40b62
Arjan Bal <arjansbal@google.com>
2024-11-20 23:12:06 +0530

    Remove env var support

--- internal/envconfig/xds.go
+++ internal/envconfig/xds.go
@@ -56,5 +56,7 @@ var (
 
 	// XDSDualstackEndpointsEnabled is true if gRPC should read the
 	// "additional addresses" in the xDS endpoint resource.
-	XDSDualstackEndpointsEnabled = boolFromEnv("GRPC_EXPERIMENTAL_XDS_DUALSTACK_ENDPOINTS", false)
+	// TODO: Control this using an env variable when all LB policies
+	// handle endpoints.
+	XDSDualstackEndpointsEnabled = false
 )
