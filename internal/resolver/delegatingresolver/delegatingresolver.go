bccef6b9b2ffe4ac0d81a6a6750df052a378397e
eshitachandwani <emchandwani@google.com>
2024-11-20 13:12:54 +0530

    delegating resolver

---
+++ internal/resolver/delegatingresolver/delegatingresolver.go
@@ -0,0 +1,238 @@
+/*
+ *
+ * Copyright 2024 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package delegatingresolver implements the default resolver for gRPC.
+// It handles both target URI and proxy address resolution, unless:
+//  - A custom dialer is set using WithContextDialer dialoption.
+//  - Proxy usage is explicitly disabled using WithNoProxy dialoption.
+//  - Client-side resolution is explicitly enforced using WithTargetResolutionEnabled.
+
+package delegatingresolver
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"sync"
+
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/serviceconfig"
+)
+
+var (
+	// The following variable will be overwritten in the tests.
+	httpProxyFromEnvironment = http.ProxyFromEnvironment
+
+	logger = grpclog.Component("delegatingresolver")
+)
+
+// delegatingResolver manages the resolution of both a target URI and an
+// optional proxy URI. It acts as an intermediary between the underlying
+// resolvers and the gRPC ClientConn. This type implements the
+// resolver.ClientConn interface to intercept state updates from both the target
+// and proxy resolvers, combining the results before forwarding them to the
+// client connection.
+type delegatingResolver struct {
+	target         resolver.Target     // parsed target URI to be resolved
+	cc             resolver.ClientConn // gRPC ClientConn
+	targetResolver resolver.Resolver   // resolver for the target URI, based on its scheme
+	proxyResolver  resolver.Resolver   // resolver for the proxy URI; nil if no proxy is configured
+	targetAddrs    []resolver.Address  // resolved or unresolved target addresses, depending on proxy configuration
+	proxyAddrs     []resolver.Address  // resolved proxy addresses; empty if no proxy is configured
+	proxyURL       *url.URL            // proxy URL, derived from proxy environment and target
+	mu             sync.Mutex          // protects access to the resolver state during updates
+}
+
+func mapAddress(address string) (*url.URL, error) {
+	req := &http.Request{
+		URL: &url.URL{
+			Scheme: "https",
+			Host:   address,
+		},
+	}
+	url, err := httpProxyFromEnvironment(req)
+	if err != nil {
+		return nil, err
+	}
+	return url, nil
+}
+
+func New(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions, targetResolverBuilder resolver.Builder) (resolver.Resolver, error) {
+	r := &delegatingResolver{
+		target: target,
+		cc:     cc,
+	}
+
+	var err error
+	r.proxyURL, err = mapAddress(target.Endpoint())
+	if err != nil {
+		return nil, fmt.Errorf("failed to determine proxy URL for target : %v", err)
+	}
+
+	if r.proxyURL == nil {
+		// Add an info log here.
+		logger.Info("No proxy URL detected")
+		return targetResolverBuilder.Build(target, cc, opts)
+	}
+
+	// When the scheme is 'dns', resolution should be handled by the proxy, not
+	// the client. Therefore, we bypass the target resolver and store the
+	// unresolved target address.
+	if target.URL.Scheme == "dns" {
+		r.targetAddrs = []resolver.Address{{Addr: target.Endpoint()}}
+	} else {
+		// targetResolverBuilder must not be nil. If it is nil, the channel should
+		// error out and not call this function.
+		r.targetResolver, err = targetResolverBuilder.Build(target, &wrappingClientConn{r, "target"}, opts)
+		if err != nil {
+			return nil, fmt.Errorf("delegating_resolver: unable to build the target resolver : %v", err)
+		}
+	}
+	r.proxyResolver, err = proxyURIResolver(r.proxyURL, opts, r)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func proxyURIResolver(proxyURL *url.URL, opts resolver.BuildOptions, presolver *delegatingResolver) (resolver.Resolver, error) {
+	scheme := "dns"
+	proxyBuilder := resolver.Get(scheme)
+	if proxyBuilder == nil {
+		return nil, fmt.Errorf("delegating_resolver: resolver for proxy not found")
+	}
+	host := "dns:///" + proxyURL.Host
+	u, err := url.Parse(host)
+	if err != nil {
+		return nil, err
+	}
+
+	proxyTarget := resolver.Target{URL: *u}
+	return proxyBuilder.Build(proxyTarget, &wrappingClientConn{presolver, "proxy"}, opts)
+}
+
+func (r *delegatingResolver) ResolveNow(o resolver.ResolveNowOptions) {
+	if r.targetResolver != nil {
+		r.targetResolver.ResolveNow(o)
+	}
+	if r.proxyResolver != nil {
+		r.proxyResolver.ResolveNow(o)
+	}
+}
+
+func (r *delegatingResolver) Close() {
+	if r.targetResolver != nil {
+		r.targetResolver.Close()
+	}
+	if r.proxyResolver != nil {
+		r.proxyResolver.Close()
+	}
+}
+
+type keyType string
+
+const proxyConnectAddrKey = keyType("grpc.resolver.delegatingresolver.proxyConnectAddr")
+const userKey = keyType("grpc.resolver.delegatingresolver.user")
+
+// Set returns a copy of the provided state with attributes containing address
+// to be sent in connect request to proxy.  It's data should not be mutated
+// after calling Set.
+func SetConnectAddr(resAddr resolver.Address, addr string) resolver.Address {
+	resAddr.Attributes = resAddr.Attributes.WithValue(proxyConnectAddrKey, addr)
+	return resAddr
+}
+
+// Get returns the proxy connect address in resolver.State, or nil if not present.
+// The returned data should not be mutated.
+func GetConnectAddr(addr resolver.Address) string {
+	s, _ := addr.Attributes.Value(proxyConnectAddrKey).(string)
+	return s
+}
+
+// Set returns a copy of the provided state with attributes containing user info.
+// It's data should not be mutated after calling Set.
+func SetUser(addr resolver.Address, user *url.Userinfo) resolver.Address {
+	addr.Attributes = addr.Attributes.WithValue(userKey, user)
+	return addr
+}
+
+// Get returns the user info in the resolver.State, or nil if not present.
+// The returned data should not be mutated.
+func GetUser(addr resolver.Address) *url.Userinfo {
+	s, _ := addr.Attributes.Value(userKey).(*url.Userinfo)
+	return s
+}
+
+func (r *delegatingResolver) updateState(state resolver.State) resolver.State {
+	var addresses []resolver.Address
+	for _, proxyAddr := range r.proxyAddrs {
+		for _, targetAddr := range r.targetAddrs {
+			newAddr := resolver.Address{
+				Addr: proxyAddr.Addr,
+			}
+			newAddr = SetConnectAddr(newAddr, targetAddr.Addr)
+			if r.proxyURL.User != nil {
+				newAddr = SetUser(newAddr, r.proxyURL.User)
+			}
+			addresses = append(addresses, newAddr)
+		}
+	}
+	// Set the addresses in the current state.
+	state.Addresses = addresses
+	return state
+}
+
+type wrappingClientConn struct {
+	parent       *delegatingResolver
+	resolverType string
+}
+
+// UpdateState intercepts state updates from the target and proxy resolvers.
+func (wcc *wrappingClientConn) UpdateState(state resolver.State) error {
+	wcc.parent.mu.Lock()
+	defer wcc.parent.mu.Unlock()
+	var curState resolver.State
+	if wcc.resolverType == "target" {
+		wcc.parent.targetAddrs = state.Addresses
+		curState = state
+	}
+	if wcc.resolverType == "proxy" {
+		wcc.parent.proxyAddrs = state.Addresses
+	}
+
+	if len(wcc.parent.targetAddrs) == 0 || len(wcc.parent.proxyAddrs) == 0 {
+		return nil
+	}
+	curState = wcc.parent.updateState(curState)
+	return wcc.parent.cc.UpdateState(curState)
+}
+
+// ReportError intercepts errors from the child resolvers and pass to ClientConn.
+func (wcc *wrappingClientConn) ReportError(err error) {
+	wcc.parent.cc.ReportError(err)
+}
+
+// NewAddress is a no-op as it has been deprecated.
+func (wcc *wrappingClientConn) NewAddress(addrs []resolver.Address) {}
+
+// ParseServiceConfig parses the provided service config and returns an
+// object that provides the parsed config.
+func (wcc *wrappingClientConn) ParseServiceConfig(serviceConfigJSON string) *serviceconfig.ParseResult {
+	return wcc.parent.cc.ParseServiceConfig(serviceConfigJSON)
+}
---
+++ internal/resolver/delegatingresolver/delegatingresolver_test.go
@@ -0,0 +1,250 @@
+/*
+ *
+ * Copyright 2024 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package delegatingresolver
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/internal/testutils"
+	"google.golang.org/grpc/resolver"
+	_ "google.golang.org/grpc/resolver/dns" // To register dns resolver.
+	"google.golang.org/grpc/resolver/manual"
+	"google.golang.org/grpc/serviceconfig"
+)
+
+type s struct {
+	grpctest.Tester
+}
+
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
+}
+
+const (
+	targetTestAddr          = "test.com"
+	resolvedTargetTestAddr  = "1.2.3.4:8080"
+	resolvedTargetTestAddr1 = "1.2.3.5:8080"
+	envProxyAddr            = "proxytest.com"
+	resolvedProxyTestAddr   = "2.3.4.5:7687"
+	resolvedProxyTestAddr1  = "2.3.4.6:7687"
+)
+
+// overwriteAndRestore overwrite function httpProxyFromEnvironment and
+// returns a function to restore the default values.
+func overwrite(hpfe func(req *http.Request) (*url.URL, error)) func() {
+	backHPFE := httpProxyFromEnvironment
+	httpProxyFromEnvironment = hpfe
+	return func() {
+		httpProxyFromEnvironment = backHPFE
+	}
+}
+
+func (s) TestMapAddressEnv(t *testing.T) {
+	// Overwrite the function in the test and restore them in defer.
+	hpfe := func(req *http.Request) (*url.URL, error) {
+		if req.URL.Host == targetTestAddr {
+			return &url.URL{
+				Scheme: "https",
+				Host:   envProxyAddr,
+			}, nil
+		}
+		return nil, nil
+	}
+	defer overwrite(hpfe)()
+
+	// envTestAddr should be handled by ProxyFromEnvironment.
+	got, err := mapAddress(targetTestAddr)
+	if err != nil {
+		t.Error(err)
+	}
+	if got.Host != envProxyAddr {
+		t.Errorf("want %v, got %v", envProxyAddr, got)
+	}
+}
+
+// createTestResolverClientConn initializes a test ResolverClientConn and
+// returns it along with channels for resolver state updates and errors.
+func createTestResolverClientConn(t *testing.T) (*testutils.ResolverClientConn, chan resolver.State, chan error) {
+	stateCh := make(chan resolver.State, 1)
+	errCh := make(chan error, 1)
+
+	tcc := &testutils.ResolverClientConn{
+		Logger:       t,
+		UpdateStateF: func(s resolver.State) error { stateCh <- s; return nil },
+		ReportErrorF: func(err error) { errCh <- err },
+	}
+	return tcc, stateCh, errCh
+}
+
+// TestDelegatingResolverNoProxy verifies the behavior of the delegating resolver
+// when no proxy is configured.
+func (s) TestDelegatingResolverNoProxy(t *testing.T) {
+	t.Setenv("HTTPS_PROXY", "")
+	mr := manual.NewBuilderWithScheme("test") // Set up a manual resolver to control the address resolution.
+	target := "test:///" + targetTestAddr
+
+	tcc, stateCh, _ := createTestResolverClientConn(t)
+	// Create a delegating resolver with no proxy configuration
+	dr, err := New(resolver.Target{URL: *testutils.MustParseURL(target)}, tcc, resolver.BuildOptions{}, mr)
+	if err != nil || dr == nil {
+		t.Fatalf("Failed to create delegating resolver: %v", err)
+	}
+
+	// Update the manual resolver with a test address.
+	mr.UpdateState(resolver.State{
+		Addresses:     []resolver.Address{{Addr: resolvedTargetTestAddr}},
+		ServiceConfig: &serviceconfig.ParseResult{},
+	})
+
+	// Verify that the delegating resolver outputs the same address.
+	expectedState := resolver.State{
+		Addresses:     []resolver.Address{{Addr: resolvedTargetTestAddr}},
+		ServiceConfig: &serviceconfig.ParseResult{},
+	}
+	state := <-stateCh
+	if len(state.Addresses) != 1 || !cmp.Equal(expectedState, state) {
+		t.Errorf("Unexpected state from delegating resolver: %v, want %v", state, expectedState)
+	}
+}
+
+// setupDNS unregisters the DNS resolver and registers a manual resolver for the
+// same scheme. This allows the test to mock the DNS resolution for the proxy resolver.
+func setupDNS(t *testing.T) *manual.Resolver {
+
+	mr := manual.NewBuilderWithScheme("dns")
+
+	dnsResolverBuilder := resolver.Get("dns")
+	resolver.Register(mr)
+
+	t.Cleanup(func() { resolver.Register(dnsResolverBuilder) })
+	return mr
+}
+
+// TestDelegatingResolverwithDNSAndProxy verifies that the delegating resolver
+// correctly updates state when the target URI scheme is DNS and a proxy is
+// configured.
+func (s) TestDelegatingResolverwithDNSAndProxy(t *testing.T) {
+	t.Setenv("HTTPS_PROXY", "testProxyAddr.com")
+	hpfe := func(req *http.Request) (*url.URL, error) {
+		return &url.URL{
+			Scheme: "https",
+			Host:   envProxyAddr,
+		}, nil
+	}
+	defer overwrite(hpfe)()
+	mrTarget := manual.NewBuilderWithScheme("test") // Manual resolver to control the target resolution.
+	mrProxy := setupDNS(t)                          // Set up a manual DNS resolver to control the proxy address resolution.
+	target := "dns:///" + targetTestAddr
+
+	tcc, stateCh, _ := createTestResolverClientConn(t)
+	dr, err := New(resolver.Target{URL: *testutils.MustParseURL(target)}, tcc, resolver.BuildOptions{}, mrTarget)
+	if err != nil {
+		t.Fatalf("Failed to create delegating resolver: %v", err)
+	}
+	if dr == nil {
+		t.Fatalf("Failed to create delegating resolver")
+	}
+
+	mrProxy.UpdateState(resolver.State{
+		Addresses:     []resolver.Address{{Addr: resolvedProxyTestAddr}},
+		ServiceConfig: &serviceconfig.ParseResult{},
+	})
+
+	// Verify that the delegating resolver outputs the same address.
+	expectedAddr := resolver.Address{Addr: resolvedProxyTestAddr}
+	expectedAddr = SetConnectAddr(expectedAddr, targetTestAddr)
+	expectedState := resolver.State{Addresses: []resolver.Address{expectedAddr}}
+
+	state := <-stateCh
+	if len(state.Addresses) != 1 || !cmp.Equal(expectedState, state) {
+		t.Fatalf("Unexpected state from delegating resolver: %v\n, want %v\n", state, expectedState)
+	}
+}
+
+// TestDelegatingResolverwithDNSAndProxy verifies that the delegating resolver
+// correctly updates state when the target URI scheme is not DNS and a proxy is
+// configured.
+func (s) TestDelegatingResolverwithCustomResolverAndProxy(t *testing.T) {
+	t.Setenv("HTTPS_PROXY", "envTestAddr.com")
+	hpfe := func(req *http.Request) (*url.URL, error) {
+		return &url.URL{
+			Scheme: "https",
+			Host:   envProxyAddr,
+		}, nil
+	}
+	defer overwrite(hpfe)()
+
+	mrTarget := manual.NewBuilderWithScheme("test") // Manual resolver to control the target resolution.
+	mrProxy := setupDNS(t)                          // Set up a manual DNS resolver to control the proxy address resolution.
+	target := "test:///" + targetTestAddr
+
+	tcc, stateCh, _ := createTestResolverClientConn(t)
+	dr, err := New(resolver.Target{URL: *testutils.MustParseURL(target)}, tcc, resolver.BuildOptions{}, mrTarget)
+	if err != nil {
+		t.Fatalf("Failed to create delegating resolver: %v", err)
+	}
+	if dr == nil {
+		t.Fatalf("Failed to create delegating resolver")
+	}
+
+	mrProxy.UpdateState(resolver.State{
+		Addresses: []resolver.Address{
+			{Addr: resolvedProxyTestAddr},
+			{Addr: resolvedProxyTestAddr1},
+		},
+		ServiceConfig: &serviceconfig.ParseResult{},
+	})
+	mrTarget.UpdateState(resolver.State{
+		Addresses: []resolver.Address{
+			{Addr: resolvedTargetTestAddr},
+			{Addr: resolvedTargetTestAddr1},
+		},
+		ServiceConfig: &serviceconfig.ParseResult{},
+	})
+
+	expectedAddr := resolver.Address{Addr: resolvedProxyTestAddr}
+	expectedAddr = SetConnectAddr(expectedAddr, resolvedTargetTestAddr)
+
+	expectedAddr1 := resolver.Address{Addr: resolvedProxyTestAddr}
+	expectedAddr1 = SetConnectAddr(expectedAddr1, resolvedTargetTestAddr1)
+
+	expectedAddr2 := resolver.Address{Addr: resolvedProxyTestAddr1}
+	expectedAddr2 = SetConnectAddr(expectedAddr2, resolvedTargetTestAddr)
+
+	expectedAddr3 := resolver.Address{Addr: resolvedProxyTestAddr1}
+	expectedAddr3 = SetConnectAddr(expectedAddr3, resolvedTargetTestAddr1)
+
+	expectedState := resolver.State{Addresses: []resolver.Address{
+		expectedAddr,
+		expectedAddr1,
+		expectedAddr2,
+		expectedAddr3,
+	},
+		ServiceConfig: &serviceconfig.ParseResult{},
+	}
+
+	state := <-stateCh
+	if len(state.Addresses) != 4 || !cmp.Equal(expectedState, state) {
+		t.Fatalf("Unexpected state from delegating resolver: %v\n, want %v\n", state, expectedState)
+	}
+}
