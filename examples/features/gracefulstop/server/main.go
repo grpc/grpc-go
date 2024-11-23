5ee5e3b626d4d22f2f346086bc5bfaefdecb1af6
Purnesh Dixit <purneshdixit@google.com>
2024-11-23 23:18:51 +0530

    use ServerStreaming scenario to demonstrate graceful shutdown

--- examples/features/gracefulstop/server/main.go
+++ examples/features/gracefulstop/server/main.go
@@ -19,30 +19,61 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
 
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50052, "port number")
+var (
+	port           = flag.Int("port", 50052, "port number")
+	streamMessages int32
+	mu             sync.Mutex
+)
 
-// server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedEchoServer
 }
 
-// SayHello implements helloworld.GreeterServer.
-func (s *server) SayHello(_ context.Context, _ *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello"}, nil
+// ServerStreamingEcho implements the EchoService.ServerStreamingEcho method.
+// It receives an EchoRequest and sends back a stream of EchoResponses.
+// The stream will contain up to 5 messages, each sent after a 1-second delay.
+// If the client cancels the request or if more than 5 messages are sent,
+// the stream will be closed with an error.
+func (s *server) ServerStreamingEcho(_ *pb.EchoRequest, stream pb.Echo_ServerStreamingEchoServer) error {
+	ctx := stream.Context()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			atomic.AddInt32(&streamMessages, 1)
+
+			mu.Lock()
+
+			if streamMessages > 5 {
+				return fmt.Errorf("request failed")
+			}
+
+			if err := stream.Send(&pb.EchoResponse{Message: fmt.Sprintf("Messages Sent: %d", streamMessages)}); err != nil {
+				return err
+			}
+
+			mu.Unlock()
+
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
 
 func main() {
@@ -59,28 +90,20 @@ func main() {
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterEchoServer(s, &server{})
 
 	go func() {
 		// Wait for an OS signal for graceful shutdown.
 		<-stop
-		fmt.Println("Shutting down server...")
+		timer := time.AfterFunc(10*time.Second, func() { // forceful stop after 10 seconds
+			log.Printf("Graceful shutdown did not complete within 10 seconds. Forcing shutdown...")
+			s.Stop()
+		})
+		defer timer.Stop()
 		s.GracefulStop()
-		close(stop)
 	}()
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	select {
-	case <-stop:
-		log.Printf("Server stopped gracefully")
-	case <-ctx.Done():
-		log.Printf("Graceful stop timeout reached. Forcing server stop.")
-		s.Stop() // Forceful stop
 	}
 }
