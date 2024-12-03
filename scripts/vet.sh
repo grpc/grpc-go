e17a20c7daf0ea91e9189ea41f05649ec07942e8
Arjan Bal <arjansbal@google.com>
2024-12-03 10:55:12 +0530

    Fix typo in vet comment

--- scripts/vet.sh
+++ scripts/vet.sh
@@ -89,7 +89,7 @@ git grep '"github.com/envoyproxy/go-control-plane/envoy' -- '*.go' ':(exclude)*.
 git grep -e 'context.Background()' --or -e 'context.TODO()' -- "*_test.go" | grep -v "benchmark/primitives/context_test.go" | grep -v "credential
 s/google" | grep -v "internal/transport/" | grep -v "xds/internal/" | grep -v "security/advancedtls" | grep -v 'context.WithTimeout(' | not grep -v 'context.WithCancel('
 
-# Disallow usage of net.ParseIP in favour of ipnet.ParseAddr as the former
+# Disallow usage of net.ParseIP in favour of netip.ParseAddr as the former
 # can't parse link local IPv6 addresses.
 not git grep 'net.ParseIP' -- '*.go'
 
