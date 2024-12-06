fa74a99642833e691eec69eeb5675ae1ee1ff633
Vissa Janardhan Krishna Sai <vissajanardhan@google.com>
2024-12-06 06:42:57 +0000

    removing redundant server code

--- authz/audit/audit_logging_test.go
+++ authz/audit/audit_logging_test.go
@@ -24,7 +24,6 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"io"
-	"net"
 	"os"
 	"testing"
 	"time"
@@ -273,16 +272,11 @@ func (s) TestAuditLogger(t *testing.T) {
 			defer s.Stop()
 			ss.S = s
 			stubserver.StartTestService(t, ss)
-			lis, err := net.Listen("tcp", "localhost:0")
-			if err != nil {
-				t.Fatalf("Error listening: %v", err)
-			}
-			go s.Serve(lis)
 
 			// Setup gRPC test client with certificates containing a SPIFFE Id.
-			clientConn, err := grpc.NewClient(lis.Addr().String(), grpc.WithTransportCredentials(clientCreds))
+			clientConn, err := grpc.NewClient(ss.Address, grpc.WithTransportCredentials(clientCreds))
 			if err != nil {
-				t.Fatalf("grpc.NewClient(%v) failed: %v", lis.Addr().String(), err)
+				t.Fatalf("grpc.NewClient(%v) failed: %v", ss.Address, err)
 			}
 			defer clientConn.Close()
 			client := testgrpc.NewTestServiceClient(clientConn)
