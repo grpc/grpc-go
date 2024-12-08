1fea0aa9847bde0ef3d77ebb6984ef0cb00d03a2
Purnesh Dixit <purneshdixit@google.com>
2024-12-08 18:05:08 +0530

    xds client poc with grpc transport

--- xds/clients/xdsclient/config.go
+++ xds/clients/xdsclient/config.go
@@ -21,7 +21,17 @@ package xdsclient
 import (
 	"time"
 
+	"google.golang.org/grpc/internal/backoff"
 	"google.golang.org/grpc/xds/clients"
+)
+
+const (
+	defaultWatchExpiryTimeout       = 15 * time.Second
+	defaultIdleChannelExpiryTimeout = 5 * time.Minute
+)
+
+var (
+	defaultStreamBackoffFunc = backoff.DefaultExponential.Backoff
 )
 
 // Config contains xDS fields applicable to xDS client.
@@ -31,18 +41,21 @@ type Config struct {
 	Authorities      map[string]clients.Authority // Required
 	Node             clients.Node                 // Required
 	TransportBuilder clients.TransportBuilder     // Required
-
-	Extensions any
+	ResourceTypes    map[string]ResourceType      // Required
 
 	// Below values will have default values but can be overridden for testing
 	watchExpiryTimeOut       time.Duration
 	idleChannelExpiryTimeout time.Duration
-	streamBackOffTimeout     time.Duration
+	streamBackOffTimeout     func(int) time.Duration
 }
 
 // NewConfig returns an xDS config configured with mandatory parameters.
-func NewConfig(xDSServers []clients.ServerConfig, authorities map[string]clients.Authority, node clients.Node, transport clients.TransportBuilder) (*Config, error) {
-	return &Config{XDSServers: xDSServers, Authorities: authorities, Node: node, TransportBuilder: transport}, nil
+func NewConfig(xDSServers []clients.ServerConfig, authorities map[string]clients.Authority, node clients.Node, transport clients.TransportBuilder, resourceTypes map[string]ResourceType) *Config {
+	c := &Config{XDSServers: xDSServers, Authorities: authorities, Node: node, TransportBuilder: transport, ResourceTypes: resourceTypes}
+	c.idleChannelExpiryTimeout = defaultIdleChannelExpiryTimeout
+	c.watchExpiryTimeOut = defaultWatchExpiryTimeout
+	c.streamBackOffTimeout = defaultStreamBackoffFunc
+	return c
 }
 
 func (c *Config) SetWatchExpiryTimeoutForTesting(d time.Duration) {
@@ -53,6 +66,26 @@ func (c *Config) SetIdleChannelExpiryTimeoutForTesting(d time.Duration) {
 	c.idleChannelExpiryTimeout = d
 }
 
-func (c *Config) SetStreamBackOffTimeoutForTesting(d time.Duration) {
+func (c *Config) SetStreamBackOffTimeoutForTesting(d func(int) time.Duration) {
 	c.streamBackOffTimeout = d
 }
+
+/*
+gke-test1-pods-d3a9ce1f;  gke-test1-services-d3a9ce1f;   gke-bct-psm-interop-lb-primary-pods-01f890b5;    gke-bct-psm-interop-url-map-pods-5d7c48b4;    gke-bct-psm-interop-security-pods-e4fd9946;   gke-bct-psm-interop-url-map-pods-3c3f560d;          gke-bct-psm-interop-security-pods-9dacfca9;     gke-bct-psm-interop-lb-primary-pods-ffb45a87
+10.48.0.0/14;             10.52.0.0/20;                  10.125.0.0/16;                                   10.75.0.0/16;                                 10.96.0.0/16;                                 10.76.0.0/16;                                                                     10.97.0.0/16;                                   10.80.0.0/14
+
+
+gke-bct-psm-interop-lb-secondary-pods-2331935c	10.127.0.0/16
+
+
+
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-0bx8   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.19   34.144.8.90     Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-b6xg   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.29   34.144.4.236    Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-gnq1   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.44   34.144.7.194    Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-km9q   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.17   34.144.13.14    Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-mjl1   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.87   34.144.13.37    Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-nm62   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.26   34.144.13.47    Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-r7qt   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.18   34.144.10.74    Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-trc3   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.78   34.144.6.72     Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+gke-bct-psm-interop-secu-default-pool-a0ed89f2-v8m9   Ready    <none>   8d    v1.30.5-gke.1656000   10.138.0.28   34.144.10.230   Container-Optimized OS from Google   6.1.100+         containerd://1.7.22
+*/
--- xds/clients/xdsclient/xds_client.go
+++ xds/clients/xdsclient/xds_client.go
@@ -18,9 +18,106 @@
 
 package xdsclient
 
+import (
+	"sync"
+	"sync/atomic"
+
+	"google.golang.org/grpc/internal/cache"
+	"google.golang.org/grpc/internal/grpclog"
+	"google.golang.org/grpc/internal/grpcsync"
+	"google.golang.org/grpc/xds/clients"
+	clientsinternal "google.golang.org/grpc/xds/clients/internal"
+	"google.golang.org/grpc/xds/internal/xdsclient/transport/ads"
+	"google.golang.org/grpc/xds/internal/xdsclient/xdsresource"
+)
+
 // XDSClient represents an xDS client.
 type XDSClient struct {
 	config *Config
+
+	done            *grpcsync.Event              // Fired when the client is closed.
+	serializer      *grpcsync.CallbackSerializer // Serializer for invoking resource watcher callbacks.
+	serializerClose func()                       // Function to close the serializer.
+	logger          *grpclog.PrefixLogger        // Logger for this client.
+
+	// The XDSClient owns a bunch of channels to individual xDS servers
+	// specified in the config. Authorities acquire references
+	// to these channels based on server configs within the authority config.
+	// The XDSClient maintains a list of interested authorities for each of
+	// these channels, and forwards updates from the channels to each of these
+	// authorities.
+	//
+	// Once all references to a channel are dropped, the channel is moved to the
+	// idle cache where it lives for a configured duration before being closed.
+	// If the channel is required before the idle timeout fires, it is revived
+	// from the idle cache and used.
+	channelsMu        sync.Mutex
+	xdsActiveChannels map[string]*channelState // Map from server config to in-use xdsChannels.
+	xdsIdleChannels   *cache.TimeoutCache      // Map from server config to idle xdsChannels.
+	closeCond         *sync.Cond
+}
+
+// channelState represents the state of an xDS channel.
+//
+// It receives callbacks for events on the underlying ADS stream and invokes
+// corresponding callbacks on interested authoririties.
+type channelState struct {
+	parent       *XDSClient
+	serverConfig *clients.ServerConfig
+
+	// Access to the following fields should be protected by the parent's
+	// channelsMu.
+	channel               *clientsinternal.XDSChannel
+	interestedAuthorities map[*clients.Authority]bool
+}
+
+func (cs *channelState) adsStreamFailure(err error) {
+	if cs.parent.done.HasFired() {
+		return
+	}
+
+	cs.parent.channelsMu.Lock()
+	defer cs.parent.channelsMu.Unlock()
+	for authority := range cs.interestedAuthorities {
+		authority.adsStreamFailure(cs.serverConfig, err)
+	}
+}
+
+func (cs *channelState) adsResourceUpdate(typ xdsresource.Type, updates map[string]ads.DataAndErrTuple, md xdsresource.UpdateMetadata, onDone func()) {
+	if cs.parent.done.HasFired() {
+		return
+	}
+
+	cs.parent.channelsMu.Lock()
+	defer cs.parent.channelsMu.Unlock()
+
+	if len(cs.interestedAuthorities) == 0 {
+		onDone()
+		return
+	}
+
+	authorityCnt := new(atomic.Int64)
+	authorityCnt.Add(int64(len(cs.interestedAuthorities)))
+	done := func() {
+		if authorityCnt.Add(-1) == 0 {
+			onDone()
+		}
+	}
+	for authority := range cs.interestedAuthorities {
+		authority.adsResourceUpdate(cs.serverConfig, typ, updates, md, done)
+	}
+}
+
+func (cs *channelState) adsResourceDoesNotExist(typ xdsresource.Type, resourceName string) {
+	if cs.parent.done.HasFired() {
+		return
+	}
+
+	cs.parent.channelsMu.Lock()
+	defer cs.parent.channelsMu.Unlock()
+	for authority := range cs.interestedAuthorities {
+		authority.adsResourceDoesNotExist(typ, resourceName)
+	}
 }
 
 // New returns an xDS Client configured with provided config.
