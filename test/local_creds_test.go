87a8136e3748701da48950360740eb7df0cac768
Pasupuleti Sravani <pvsravani@google.com>
2024-12-31 07:45:05 +0000

    Adding listener to stubserver

--- test/local_creds_test.go
+++ test/local_creds_test.go
@@ -161,12 +161,10 @@ func spoofDialer(addr net.Addr) func(target string, t time.Duration) (net.Conn, 
 }
 
 func testLocalCredsE2EFail(t *testing.T, dopts []grpc.DialOption) error {
-
 	lis, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		return fmt.Errorf("Failed to create listener: %v", err)
 	}
-
 	var fakeClientAddr, fakeServerAddr net.Addr
 	fakeClientAddr = &net.IPAddr{
 		IP:   netip.MustParseAddr("10.8.9.10").AsSlice(),
@@ -185,18 +183,6 @@ func testLocalCredsE2EFail(t *testing.T, dopts []grpc.DialOption) error {
 	}
 	defer ss.S.Stop()
 	stubserver.StartTestService(t, ss)
-
-	/*var fakeClientAddr, fakeServerAddr net.Addr
-	fakeClientAddr = &net.IPAddr{
-		IP:   netip.MustParseAddr("10.8.9.10").AsSlice(),
-		Zone: "",
-	}
-	fakeServerAddr = &net.IPAddr{
-		IP:   netip.MustParseAddr("10.8.9.11").AsSlice(),
-		Zone: "",
-	}*/
-
-	//go ss.S.Serve(spoofListener(lis, fakeClientAddr))
 
 	cc, err := grpc.NewClient(lis.Addr().String(), append(dopts, grpc.WithDialer(spoofDialer(fakeServerAddr)))...)
 	if err != nil {
