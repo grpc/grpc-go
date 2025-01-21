aa1434bb5e2bd52e71d87174e374c981ae883628
Vissa Janardhan Krishna Sai <vissajanardhan@google.com>
2025-01-21 11:38:54 +0000

    updating explicit client connection

--- xds/internal/balancer/clusterresolver/e2e_test/balancer_test.go
+++ xds/internal/balancer/clusterresolver/e2e_test/balancer_test.go
@@ -245,7 +245,7 @@ func (s) TestErrorFromParentLB_ResourceNotFound(t *testing.T) {
 	// resolver, and dial the test backends.
 	cc, cleanup := setupAndDial(t, bootstrapContents)
 	defer cleanup()
-
+	cc.Connect()
 	// Wait for the EDS resource to be requested.
 	select {
 	case <-ctx.Done():
