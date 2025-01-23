f1102b3e7d43b4e673921508c5b20d709336224e
Vissa Janardhan Krishna Sai <vissajanardhan@google.com>
2025-01-23 10:09:21 +0000

    Revert "reverting test update"
    This reverts commit 658b745011d0718f084db127ba9f09f9051d4ff6.

--- vendor/go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest/README.md
+++
@@ -1,3 +0,0 @@
-# SDK Metric data test
-
-[![PkgGoDev](https://pkg.go.dev/badge/go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest)](https://pkg.go.dev/go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest)
--- vendor/go.opentelemetry.io/otel/sdk/resource/host_id_darwin.go
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-package resource // import "go.opentelemetry.io/otel/sdk/resource"
-
-var platformHostIDReader hostIDReader = &hostIDReaderDarwin{
-	execCommand: execCommand,
-}
