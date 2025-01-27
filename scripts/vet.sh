869fdc524456912da9484672283e9075b4ad2a5e
Vinothkumar <vinothkumarr@google.com>
2025-01-27 06:12:23 +0000

    This simplifies the code and avoids running the find command multiple times

--- scripts/vet.sh
+++ scripts/vet.sh
@@ -88,6 +88,9 @@ not git grep 'net.ParseIP' -- '*.go'
 
 misspell -error .
 
+# Get the absolute path to revive.toml relative to the script location
+REVIVE_CONFIG_PATH="$(dirname "$(realpath "$0")")/revive.toml"
+
 # - gofmt, goimports, go vet, go mod tidy.
 # Perform these checks on each module inside gRPC.
 for MOD_FILE in $(find . -name 'go.mod'); do
@@ -101,6 +104,15 @@ for MOD_FILE in $(find . -name 'go.mod'); do
   git status --porcelain 2>&1 | fail_on_output || \
     (git status; git --no-pager diff; exit 1)
   
+  # Error for violation of enabled lint rules in config excluding generated code.
+  revive \
+    -set_exit_status=1 \
+    -exclude "testdata/grpc_testing_not_regenerated/" \
+    -exclude "**/*.pb.go" \
+    -formatter plain \
+    -config "$REVIVE_CONFIG_PATH" \
+    ./...
+
   # - Collection of static analysis checks
   SC_OUT="$(mktemp)"
   # By default, Staticcheck targets the Go version declared in go.mod via the go
@@ -190,17 +202,6 @@ GetTlsCertificateCertificateProviderInstance
 GetValidationContextCertificateProviderInstance
 XXXXX PleaseIgnoreUnused'
   popd
-
-# Error for violation of enabled lint rules in config excluding generated code.
-  echo "Revive config directory: $(dirname "$0")"
-  revive \
-    -set_exit_status=1 \
-    -exclude "testdata/grpc_testing_not_regenerated/" \
-    -exclude "**/*.pb.go" \
-    -formatter plain \
-    -config "$(dirname "$0")/revive.toml" \
-    "$MOD_DIR/..."
-
 done
 
 echo SUCCESS
