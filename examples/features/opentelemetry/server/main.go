a33cc4b885f1af542066ec03e0868b911a23a0f1
Vissa Janardhan Krishna Sai <vissajanardhan@google.com>
2025-02-06 10:38:54 +0000

    fixing vet issue

--- examples/features/opentelemetry/server/main.go
+++ examples/features/opentelemetry/server/main.go
@@ -54,7 +54,7 @@ type echoServer struct {
 	addr string
 }
 
-func (s *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+func (s *echoServer) UnaryEcho(_ context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
