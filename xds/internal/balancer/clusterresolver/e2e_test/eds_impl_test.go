57fe1bc0eff43f4714e8c20ce5a6622fc3a2f1c7
Vissa Janardhan Krishna Sai <vissajanardhan@google.com>
2025-02-20 10:54:04 +0000

    revert fatal msg

--- xds/internal/balancer/clusterresolver/e2e_test/eds_impl_test.go
+++ xds/internal/balancer/clusterresolver/e2e_test/eds_impl_test.go
@@ -820,7 +820,7 @@ func (s) TestEDS_ClusterResourceUpdates(t *testing.T) {
 		SkipValidation: true,
 	}
 	if err := managementServer.Update(ctx, resources); err != nil {
-		t.Fatalf("Failed to update management server resources: %v", err)
+		t.Fatal(err)
 	}
 
 	// Create xDS client, configure cds_experimental LB policy with a manual
@@ -850,7 +850,7 @@ func (s) TestEDS_ClusterResourceUpdates(t *testing.T) {
 	// Change the cluster resource to not contain an eds_service_name.
 	resources.Clusters = []*v3clusterpb.Cluster{e2e.DefaultCluster(clusterName, "", e2e.SecurityLevelNone)}
 	if err := managementServer.Update(ctx, resources); err != nil {
-		t.Fatalf("Failed to update cluster resources: %v", err)
+		t.Fatal(err)
 	}
 
 	// Ensure that an EDS watch for eds_service_name is canceled and new watch
@@ -893,7 +893,7 @@ func (s) TestEDS_ClusterResourceUpdates(t *testing.T) {
 		},
 	}
 	if err := managementServer.Update(ctx, resources); err != nil {
-		t.Fatalf("Failed to update cluster circuit breakers: %v", err)
+		t.Fatal(err)
 	}
 
 	// Ensure that RPCs continue to get routed to the second backend for the
