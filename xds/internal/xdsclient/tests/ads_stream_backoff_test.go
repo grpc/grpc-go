c7db760171d90d704b61f668104ec80e821ac7b8
Easwar Swaminathan <easwars@google.com>
2025-02-20 06:09:24 -0800

    xdsclient: ensure xDS node ID in included in NACK and connectivity errors (#8103)

--- xds/internal/xdsclient/tests/ads_stream_backoff_test.go
+++ xds/internal/xdsclient/tests/ads_stream_backoff_test.go
@@ -22,7 +22,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
@@ -104,13 +103,15 @@ func (s) TestADS_BackoffAfterStreamFailure(t *testing.T) {
 
 	// Override the backoff implementation to push on a channel that is read by
 	// the test goroutine.
+	backoffCtx, backoffCancel := context.WithCancel(ctx)
 	streamBackoff := func(v int) time.Duration {
 		select {
 		case backoffCh <- struct{}{}:
-		case <-ctx.Done():
+		case <-backoffCtx.Done():
 		}
 		return 0
 	}
+	defer backoffCancel()
 
 	// Create an xDS client with bootstrap pointing to the above server.
 	nodeID := uuid.New().String()
@@ -130,13 +131,8 @@ func (s) TestADS_BackoffAfterStreamFailure(t *testing.T) {
 	}
 
 	// Verify that the received stream error is reported to the watcher.
-	u, err := lw.updateCh.Receive(ctx)
-	if err != nil {
-		t.Fatal("Timeout when waiting for an error callback on the listener watcher")
-	}
-	gotErr := u.(listenerUpdateErrTuple).err
-	if !strings.Contains(gotErr.Error(), streamErr.Error()) {
-		t.Fatalf("Received stream error: %v, wantErr: %v", gotErr, streamErr)
+	if err := verifyListenerError(ctx, lw.updateCh, streamErr.Error(), nodeID); err != nil {
+		t.Fatal(err)
 	}
 
 	// Verify that the stream is closed.
@@ -157,6 +153,12 @@ func (s) TestADS_BackoffAfterStreamFailure(t *testing.T) {
 	if err := waitForResourceNames(ctx, t, ldsResourcesCh, []string{listenerName}); err != nil {
 		t.Fatal(err)
 	}
+
+	// To prevent indefinite blocking during xDS client close, which is caused
+	// by a blocking backoff channel write, cancel the backoff context early
+	// given that the test is complete.
+	backoffCancel()
+
 }
 
 // Tests the case where a stream breaks because the server goes down. Verifies
