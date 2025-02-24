ebe37f3279cb730a81e3eb78a2a32dbfee5a176e
Vissa Janardhan Krishna Sai <vissajanardhan@google.com>
2025-02-24 06:09:59 +0000

    explicit naming for traces and metrics

--- examples/features/opentelemetry/client/main.go
+++ examples/features/opentelemetry/client/main.go
@@ -46,8 +46,8 @@ import (
 
 var (
 	addr               = flag.String("addr", ":50051", "the server address to connect to")
-	prometheusEndpoint = flag.String("prometheus_endpoint", ":9465", "the Prometheus exporter endpoint")
-	otlpEndpoint       = flag.String("otlp_endpoint", "localhost:4318", "the OTLP collector endpoint")
+	prometheusEndpoint = flag.String("prometheus_endpoint", ":9465", "the Prometheus exporter endpoint for metrics")
+	otlpEndpoint       = flag.String("otlp_endpoint", ":4318", "the OTLP collector endpoint for traces")
 	serviceName        = "grpc-client"
 )
 
