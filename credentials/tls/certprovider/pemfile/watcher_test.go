6fdee4a3a22d19231bbbcfb35b1b1cd50209987d
Gregory Cooke <gregorycooke@google.com>
2025-02-27 19:23:18 +0000

    more watcher tests

--- credentials/tls/certprovider/pemfile/watcher_test.go
+++ credentials/tls/certprovider/pemfile/watcher_test.go
@@ -36,9 +36,10 @@ import (
 const (
 	// These are the names of files inside temporary directories, which the
 	// plugin is asked to watch.
-	certFile = "cert.pem"
-	keyFile  = "key.pem"
-	rootFile = "ca.pem"
+	certFile         = "cert.pem"
+	keyFile          = "key.pem"
+	rootFile         = "ca.pem"
+	spiffeBundleFile = "spiffebundle.json"
 
 	defaultTestRefreshDuration = 100 * time.Millisecond
 	defaultTestTimeout         = 5 * time.Second
@@ -121,9 +122,10 @@ func (s) TestNewProvider(t *testing.T) {
 		{
 			desc: "Everything is specified",
 			options: Options{
-				KeyFile:  testdata.Path("x509/client1_key.pem"),
-				CertFile: testdata.Path("x509/client1_cert.pem"),
-				RootFile: testdata.Path("x509/client_ca_cert.pem"),
+				KeyFile:             testdata.Path("x509/client1_key.pem"),
+				CertFile:            testdata.Path("x509/client1_cert.pem"),
+				RootFile:            testdata.Path("x509/client_ca_cert.pem"),
+				SpiffeBundleMapFile: testdata.Path("spiffe/spiffebundle.json"),
 			},
 			wantError: false,
 		},
@@ -179,7 +181,7 @@ func createTmpFile(t *testing.T, src, dst string) {
 // tempDir with the given dirSuffix. It also reads from certSrc, keySrc and
 // rootSrc files are creates appropriate files under the newly create tempDir.
 // Returns the name of the created tempDir.
-func createTmpDirWithFiles(t *testing.T, dirSuffix, certSrc, keySrc, rootSrc string) string {
+func createTmpDirWithFiles(t *testing.T, dirSuffix, certSrc, keySrc, rootSrc, spiffeBundleSrc string) string {
 	t.Helper()
 
 	// Create a temp directory. Passing an empty string for the first argument
@@ -193,12 +195,13 @@ func createTmpDirWithFiles(t *testing.T, dirSuffix, certSrc, keySrc, rootSrc str
 	createTmpFile(t, testdata.Path(certSrc), path.Join(dir, certFile))
 	createTmpFile(t, testdata.Path(keySrc), path.Join(dir, keyFile))
 	createTmpFile(t, testdata.Path(rootSrc), path.Join(dir, rootFile))
+	createTmpFile(t, testdata.Path(spiffeBundleSrc), path.Join(dir, spiffeBundleFile))
 	return dir
 }
 
 // initializeProvider performs setup steps common to all tests (except the one
 // which uses symlinks).
-func initializeProvider(t *testing.T, testName string) (string, certprovider.Provider, *testutils.Channel, func()) {
+func initializeProvider(t *testing.T, testName string, useSpiffeBundle bool) (string, certprovider.Provider, *testutils.Channel, func()) {
 	t.Helper()
 
 	// Override the newDistributor to one which pushes on a channel that we
@@ -209,12 +212,23 @@ func initializeProvider(t *testing.T, testName string) (string, certprovider.Pro
 	newDistributor = func() distributor { return d }
 
 	// Create a new provider to watch the files in tmpdir.
-	dir := createTmpDirWithFiles(t, testName+"*", "x509/client1_cert.pem", "x509/client1_key.pem", "x509/client_ca_cert.pem")
-	opts := Options{
-		CertFile:        path.Join(dir, certFile),
-		KeyFile:         path.Join(dir, keyFile),
-		RootFile:        path.Join(dir, rootFile),
-		RefreshDuration: defaultTestRefreshDuration,
+	dir := createTmpDirWithFiles(t, testName+"*", "x509/client1_cert.pem", "x509/client1_key.pem", "x509/client_ca_cert.pem", "spiffe/spiffebundle.json")
+	var opts Options
+	if useSpiffeBundle {
+		opts = Options{
+			CertFile:            path.Join(dir, certFile),
+			KeyFile:             path.Join(dir, keyFile),
+			RootFile:            path.Join(dir, rootFile),
+			SpiffeBundleMapFile: path.Join(dir, spiffeBundleFile),
+			RefreshDuration:     defaultTestRefreshDuration,
+		}
+	} else {
+		opts = Options{
+			CertFile:        path.Join(dir, certFile),
+			KeyFile:         path.Join(dir, keyFile),
+			RootFile:        path.Join(dir, rootFile),
+			RefreshDuration: defaultTestRefreshDuration,
+		}
 	}
 	prov, err := NewProvider(opts)
 	if err != nil {
@@ -243,21 +257,30 @@ func initializeProvider(t *testing.T, testName string) (string, certprovider.Pro
 // successfully, and the underlying files do not change. Verifies that the
 // plugin does not push new updates to the distributor in this case.
 func (s) TestProvider_NoUpdate(t *testing.T) {
-	_, prov, distCh, cancel := initializeProvider(t, "no_update")
-	defer cancel()
+	baseName := "no_update"
+	for _, useSpiffeBundle := range []bool{true, false} {
+		testName := baseName
+		if useSpiffeBundle {
+			testName = testName + "_" + "withSpiffeBundle"
+		}
+		t.Run(testName, func(t *testing.T) {
+			_, prov, distCh, cancel := initializeProvider(t, "no_update", useSpiffeBundle)
+			defer cancel()
 
-	// Make sure the provider is healthy and returns key material.
-	ctx, cc := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cc()
-	if _, err := prov.KeyMaterial(ctx); err != nil {
-		t.Fatalf("provider.KeyMaterial() failed: %v", err)
-	}
+			// Make sure the provider is healthy and returns key material.
+			ctx, cc := context.WithTimeout(context.Background(), defaultTestTimeout)
+			defer cc()
+			if _, err := prov.KeyMaterial(ctx); err != nil {
+				t.Fatalf("provider.KeyMaterial() failed: %v", err)
+			}
 
-	// Files haven't change. Make sure no updates are pushed by the provider.
-	sCtx, sc := context.WithTimeout(context.Background(), 2*defaultTestRefreshDuration)
-	defer sc()
-	if _, err := distCh.Receive(sCtx); err == nil {
-		t.Fatal("new key material pushed to distributor when underlying files did not change")
+			// Files haven't change. Make sure no updates are pushed by the provider.
+			sCtx, sc := context.WithTimeout(context.Background(), 2*defaultTestRefreshDuration)
+			defer sc()
+			if _, err := distCh.Receive(sCtx); err == nil {
+				t.Fatal("new key material pushed to distributor when underlying files did not change")
+			}
+		})
 	}
 }
 
@@ -265,46 +288,59 @@ func (s) TestProvider_NoUpdate(t *testing.T) {
 // created successfully and the underlying files change. Verifies that the
 // changes are picked up by the provider.
 func (s) TestProvider_UpdateSuccess(t *testing.T) {
-	dir, prov, distCh, cancel := initializeProvider(t, "update_success")
-	defer cancel()
+	baseName := "update_success"
+	for _, useSpiffeBundle := range []bool{true, false} {
+		testName := baseName
+		if useSpiffeBundle {
+			testName = testName + "_" + "withSpiffeBundle"
+		}
+		t.Run(testName, func(t *testing.T) {
+			dir, prov, distCh, cancel := initializeProvider(t, "update_success", useSpiffeBundle)
+			defer cancel()
 
-	// Make sure the provider is healthy and returns key material.
-	ctx, cc := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cc()
-	km1, err := prov.KeyMaterial(ctx)
-	if err != nil {
-		t.Fatalf("provider.KeyMaterial() failed: %v", err)
-	}
+			// Make sure the provider is healthy and returns key material.
+			ctx, cc := context.WithTimeout(context.Background(), defaultTestTimeout)
+			defer cc()
+			km1, err := prov.KeyMaterial(ctx)
+			if err != nil {
+				t.Fatalf("provider.KeyMaterial() failed: %v", err)
+			}
 
-	// Change only the root file.
-	createTmpFile(t, testdata.Path("x509/server_ca_cert.pem"), path.Join(dir, rootFile))
-	if _, err := distCh.Receive(ctx); err != nil {
-		t.Fatal("timeout waiting for new key material to be pushed to the distributor")
-	}
+			// Change only the root file.
+			if useSpiffeBundle {
+				createTmpFile(t, testdata.Path("spiffe/spiffebundle2.json"), path.Join(dir, spiffeBundleFile))
+			} else {
+				createTmpFile(t, testdata.Path("x509/server_ca_cert.pem"), path.Join(dir, rootFile))
+			}
+			if _, err := distCh.Receive(ctx); err != nil {
+				t.Fatal("timeout waiting for new key material to be pushed to the distributor")
+			}
 
-	// Make sure update is picked up.
-	km2, err := prov.KeyMaterial(ctx)
-	if err != nil {
-		t.Fatalf("provider.KeyMaterial() failed: %v", err)
-	}
-	if err := compareKeyMaterial(km1, km2); err == nil {
-		t.Fatal("expected provider to return new key material after update to underlying file")
-	}
+			// Make sure update is picked up.
+			km2, err := prov.KeyMaterial(ctx)
+			if err != nil {
+				t.Fatalf("provider.KeyMaterial() failed: %v", err)
+			}
+			if err := compareKeyMaterial(km1, km2); err == nil {
+				t.Fatal("expected provider to return new key material after update to underlying file")
+			}
 
-	// Change only cert/key files.
-	createTmpFile(t, testdata.Path("x509/client2_cert.pem"), path.Join(dir, certFile))
-	createTmpFile(t, testdata.Path("x509/client2_key.pem"), path.Join(dir, keyFile))
-	if _, err := distCh.Receive(ctx); err != nil {
-		t.Fatal("timeout waiting for new key material to be pushed to the distributor")
-	}
+			// Change only cert/key files.
+			createTmpFile(t, testdata.Path("x509/client2_cert.pem"), path.Join(dir, certFile))
+			createTmpFile(t, testdata.Path("x509/client2_key.pem"), path.Join(dir, keyFile))
+			if _, err := distCh.Receive(ctx); err != nil {
+				t.Fatal("timeout waiting for new key material to be pushed to the distributor")
+			}
 
-	// Make sure update is picked up.
-	km3, err := prov.KeyMaterial(ctx)
-	if err != nil {
-		t.Fatalf("provider.KeyMaterial() failed: %v", err)
-	}
-	if err := compareKeyMaterial(km2, km3); err == nil {
-		t.Fatal("expected provider to return new key material after update to underlying file")
+			// Make sure update is picked up.
+			km3, err := prov.KeyMaterial(ctx)
+			if err != nil {
+				t.Fatalf("provider.KeyMaterial() failed: %v", err)
+			}
+			if err := compareKeyMaterial(km2, km3); err == nil {
+				t.Fatal("expected provider to return new key material after update to underlying file")
+			}
+		})
 	}
 }
 
@@ -313,82 +349,102 @@ func (s) TestProvider_UpdateSuccess(t *testing.T) {
 // symlink is updates to point to new files. Verifies that the changes are
 // picked up by the provider.
 func (s) TestProvider_UpdateSuccessWithSymlink(t *testing.T) {
-	// Override the newDistributor to one which pushes on a channel that we
-	// can block on.
-	origDistributorFunc := newDistributor
-	distCh := testutils.NewChannel()
-	d := newWrappedDistributor(distCh)
-	newDistributor = func() distributor { return d }
-	defer func() { newDistributor = origDistributorFunc }()
-
-	// Create two tempDirs with different files.
-	dir1 := createTmpDirWithFiles(t, "update_with_symlink1_*", "x509/client1_cert.pem", "x509/client1_key.pem", "x509/client_ca_cert.pem")
-	dir2 := createTmpDirWithFiles(t, "update_with_symlink2_*", "x509/server1_cert.pem", "x509/server1_key.pem", "x509/server_ca_cert.pem")
-
-	// Create a symlink under a new tempdir, and make it point to dir1.
-	tmpdir, err := os.MkdirTemp("", "test_symlink_*")
-	if err != nil {
-		t.Fatalf("os.MkdirTemp() failed: %v", err)
-	}
-	symLinkName := path.Join(tmpdir, "test_symlink")
-	if err := os.Symlink(dir1, symLinkName); err != nil {
-		t.Fatalf("failed to create symlink to %q: %v", dir1, err)
-	}
-
-	// Create a provider which watches the files pointed to by the symlink.
-	opts := Options{
-		CertFile:        path.Join(symLinkName, certFile),
-		KeyFile:         path.Join(symLinkName, keyFile),
-		RootFile:        path.Join(symLinkName, rootFile),
-		RefreshDuration: defaultTestRefreshDuration,
-	}
-	prov, err := NewProvider(opts)
-	if err != nil {
-		t.Fatalf("NewProvider(%+v) failed: %v", opts, err)
-	}
-	defer prov.Close()
-
-	// Make sure the provider picks up the files and pushes the key material on
-	// to the distributors.
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cancel()
-	for i := 0; i < 2; i++ {
-		// Since we have root and identity certs, we need to make sure the
-		// update is pushed on both of them.
-		if _, err := distCh.Receive(ctx); err != nil {
-			t.Fatalf("timeout waiting for provider to read files and push key material to distributor: %v", err)
+	baseName := "update_with_symlink"
+	for _, useSpiffeBundle := range []bool{true, false} {
+		testName := baseName
+		if useSpiffeBundle {
+			testName = testName + "_" + "withSpiffeBundle"
 		}
-	}
-	km1, err := prov.KeyMaterial(ctx)
-	if err != nil {
-		t.Fatalf("provider.KeyMaterial() failed: %v", err)
-	}
+		t.Run(testName, func(t *testing.T) {
+			// Override the newDistributor to one which pushes on a channel that we
+			// can block on.
+			origDistributorFunc := newDistributor
+			distCh := testutils.NewChannel()
+			d := newWrappedDistributor(distCh)
+			newDistributor = func() distributor { return d }
+			defer func() { newDistributor = origDistributorFunc }()
 
-	// Update the symlink to point to dir2.
-	symLinkTmpName := path.Join(tmpdir, "test_symlink.tmp")
-	if err := os.Symlink(dir2, symLinkTmpName); err != nil {
-		t.Fatalf("failed to create symlink to %q: %v", dir2, err)
-	}
-	if err := os.Rename(symLinkTmpName, symLinkName); err != nil {
-		t.Fatalf("failed to update symlink: %v", err)
-	}
+			// Create two tempDirs with different files.
+			dir1 := createTmpDirWithFiles(t, "update_with_symlink1_*", "x509/client1_cert.pem", "x509/client1_key.pem", "x509/client_ca_cert.pem", "spiffe/spiffebundle.json")
+			dir2 := createTmpDirWithFiles(t, "update_with_symlink2_*", "x509/server1_cert.pem", "x509/server1_key.pem", "x509/server_ca_cert.pem", "spiffe/spiffebundle2.json")
 
-	// Make sure the provider picks up the new files and pushes the key material
-	// on to the distributors.
-	for i := 0; i < 2; i++ {
-		// Since we have root and identity certs, we need to make sure the
-		// update is pushed on both of them.
-		if _, err := distCh.Receive(ctx); err != nil {
-			t.Fatalf("timeout waiting for provider to read files and push key material to distributor: %v", err)
-		}
-	}
-	km2, err := prov.KeyMaterial(ctx)
-	if err != nil {
-		t.Fatalf("provider.KeyMaterial() failed: %v", err)
-	}
+			// Create a symlink under a new tempdir, and make it point to dir1.
+			tmpdir, err := os.MkdirTemp("", "test_symlink_*")
+			if err != nil {
+				t.Fatalf("os.MkdirTemp() failed: %v", err)
+			}
+			symLinkName := path.Join(tmpdir, "test_symlink")
+			if err := os.Symlink(dir1, symLinkName); err != nil {
+				t.Fatalf("failed to create symlink to %q: %v", dir1, err)
+			}
 
-	if err := compareKeyMaterial(km1, km2); err == nil {
-		t.Fatal("expected provider to return new key material after symlink update")
+			// Create a provider which watches the files pointed to by the symlink.
+			var opts Options
+			if useSpiffeBundle {
+				opts = Options{
+					CertFile:            path.Join(symLinkName, certFile),
+					KeyFile:             path.Join(symLinkName, keyFile),
+					RootFile:            path.Join(symLinkName, rootFile),
+					SpiffeBundleMapFile: path.Join(symLinkName, spiffeBundleFile),
+					RefreshDuration:     defaultTestRefreshDuration,
+				}
+			} else {
+				opts = Options{
+					CertFile:        path.Join(symLinkName, certFile),
+					KeyFile:         path.Join(symLinkName, keyFile),
+					RootFile:        path.Join(symLinkName, rootFile),
+					RefreshDuration: defaultTestRefreshDuration,
+				}
+			}
+			prov, err := NewProvider(opts)
+			if err != nil {
+				t.Fatalf("NewProvider(%+v) failed: %v", opts, err)
+			}
+			defer prov.Close()
+
+			// Make sure the provider picks up the files and pushes the key material on
+			// to the distributors.
+			ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+			defer cancel()
+			for i := 0; i < 2; i++ {
+				// Since we have root and identity certs, we need to make sure the
+				// update is pushed on both of them.
+				if _, err := distCh.Receive(ctx); err != nil {
+					t.Fatalf("timeout waiting for provider to read files and push key material to distributor: %v", err)
+				}
+			}
+			km1, err := prov.KeyMaterial(ctx)
+			if err != nil {
+				t.Fatalf("provider.KeyMaterial() failed: %v", err)
+			}
+
+			// Update the symlink to point to dir2.
+			symLinkTmpName := path.Join(tmpdir, "test_symlink.tmp")
+			if err := os.Symlink(dir2, symLinkTmpName); err != nil {
+				t.Fatalf("failed to create symlink to %q: %v", dir2, err)
+			}
+			if err := os.Rename(symLinkTmpName, symLinkName); err != nil {
+				t.Fatalf("failed to update symlink: %v", err)
+			}
+
+			// Make sure the provider picks up the new files and pushes the key material
+			// on to the distributors.
+			for i := 0; i < 2; i++ {
+				// Since we have root and identity certs, we need to make sure the
+				// update is pushed on both of them.
+				if _, err := distCh.Receive(ctx); err != nil {
+					t.Fatalf("timeout waiting for provider to read files and push key material to distributor: %v", err)
+				}
+			}
+			km2, err := prov.KeyMaterial(ctx)
+			if err != nil {
+				t.Fatalf("provider.KeyMaterial() failed: %v", err)
+			}
+
+			if err := compareKeyMaterial(km1, km2); err == nil {
+				t.Fatal("expected provider to return new key material after symlink update")
+			}
+		})
 	}
 }
 
@@ -397,7 +453,7 @@ func (s) TestProvider_UpdateSuccessWithSymlink(t *testing.T) {
 // distributor. Then the update succeeds, and the test verifies that the key
 // material is updated.
 func (s) TestProvider_UpdateFailure_ThenSuccess(t *testing.T) {
-	dir, prov, distCh, cancel := initializeProvider(t, "update_failure")
+	dir, prov, distCh, cancel := initializeProvider(t, "update_failure", false)
 	defer cancel()
 
 	// Make sure the provider is healthy and returns key material.
