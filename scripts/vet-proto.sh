02b6d9c09ba387ab970e2f5da1469c3f0c8570d0
Gregory Cooke <gregorycooke@google.com>
2025-02-27 22:02:32 +0000

    .

--- scripts/vet-proto.sh
+++ scripts/vet-proto.sh
@@ -32,7 +32,7 @@ elif [[ "$#" -ne 0 ]]; then
 fi
 
 # - Check that generated proto files are up to date.
-go ./... && git status --porcelain 2>&1 | fail_on_output || \
+go generate ./... && git status --porcelain 2>&1 | fail_on_output || \
 (git status; git --no-pager diff; exit 1)
 
 echo SUCCESS
