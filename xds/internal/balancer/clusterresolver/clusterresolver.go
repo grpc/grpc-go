0553aa0e4888723183402426b00804dbbdc9ac21
Arjan Bal <arjansbal@google.com>
2025-02-28 22:57:02 +0530

    Change error log to info

--- xds/internal/balancer/clusterresolver/clusterresolver.go
+++ xds/internal/balancer/clusterresolver/clusterresolver.go
@@ -327,7 +327,10 @@ func (b *clusterResolverBalancer) run() {
 				b.handleClientConnUpdate(update)
 			case exitIdle:
 				if b.child == nil {
-					b.logger.Errorf("xds: received ExitIdle with no child balancer")
+					// This is not necessarily an error. The EDS/DNS watch may
+					// not have  returned a list of endpoints yet, so the child
+					// may not be built.
+					b.logger.Infof("xds: received ExitIdle with no child balancer")
 					break
 				}
 				// This implementation assumes the child balancer supports
