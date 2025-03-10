fd16a34617abe7d55169e005e6eae3fbdfde5785
Gregory Cooke <gregorycooke@google.com>
2025-03-10 15:08:49 +0000

    mis-merged

--- internal/credentials/spiffe/spiffe.go
+++ internal/credentials/spiffe/spiffe.go
@@ -76,11 +76,7 @@ func SPIFFEBundleMapFromBytes(bundleMapBytes []byte) (SPIFFEBundleMap, error) {
 		}
 		bundle, err := spiffebundle.Parse(trustDomain, jsonBundle)
 		if err != nil {
-<<<<<<< HEAD
-			return nil, fmt.Errorf("failed to parse bundle in map: %v", err)
-=======
-			return nil, fmt.Errorf("spiffe: failed to parse bundle in map file (%v) for trust domain %v: %v", filePath, td, err)
->>>>>>> spiffe
+			return nil, fmt.Errorf("spiffe: failed to parse bundle for trust domain %v: %v", td, err)
 		}
 		bundleMap[trustDomainString] = bundle
 	}
