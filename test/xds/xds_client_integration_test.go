61e6084e2eadf122467999839358497f1baa0e5d
Gregory Cooke <gregorycooke@google.com>
2025-04-14 18:57:46 +0000

    progress

--- test/xds/xds_client_integration_test.go
+++ test/xds/xds_client_integration_test.go
@@ -20,17 +20,23 @@ package xds_test
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials/xds"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/internal/stubserver"
 	"google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/internal/testutils/xds/e2e"
 	"google.golang.org/grpc/internal/testutils/xds/e2e/setup"
+	"google.golang.org/grpc/testdata"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 	testpb "google.golang.org/grpc/interop/grpc_testing"
@@ -71,6 +77,101 @@ func (s) TestClientSideXDS(t *testing.T) {
 
 	// Create a ClientConn and make a successful RPC.
 	cc, err := grpc.NewClient(fmt.Sprintf("xds:///%s", serviceName), grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithResolvers(xdsResolver))
+	if err != nil {
+		t.Fatalf("failed to dial local test server: %v", err)
+	}
+	defer cc.Close()
+
+	client := testgrpc.NewTestServiceClient(cc)
+	if _, err := client.EmptyCall(ctx, &testpb.Empty{}, grpc.WaitForReady(true)); err != nil {
+		t.Fatalf("rpc EmptyCall() failed: %v", err)
+	}
+}
+
+// Helper function to create a real TLS client credentials which is used as
+// fallback credentials from multiple tests.
+func makeFallbackClientCreds(t *testing.T, useSPIFFECreds bool, isMTLS bool) credentials.TransportCredentials {
+	var creds credentials.TransportCredentials
+	var err error
+	if useSPIFFECreds {
+		if isMTLS {
+			b, err := os.ReadFile(testdata.Path("spiffe_end2end/ca.pem"))
+			if err != nil {
+				t.Fatal(err)
+			}
+			cp := x509.NewCertPool()
+			if !cp.AppendCertsFromPEM(b) {
+				t.Fatalf("failed to append certificates")
+			}
+			cert, err := tls.LoadX509KeyPair(testdata.Path("spiffe_end2end/client_spiffe.pem"), testdata.Path("spiffe_end2end/client.key"))
+			if err != nil {
+				t.Fatal(err)
+			}
+			creds = credentials.NewTLS(&tls.Config{
+				ServerName:   "x.test.example.com",
+				RootCAs:      cp,
+				Certificates: []tls.Certificate{cert},
+			})
+		} else {
+			creds, err = credentials.NewClientTLSFromFile(testdata.Path("spiffe_end2end/ca.pem"), "x.test.example.com")
+		}
+	} else {
+		if isMTLS {
+			b, err := os.ReadFile(testdata.Path("x509/server_ca_cert.pem"))
+			if err != nil {
+				t.Fatal(err)
+			}
+			cp := x509.NewCertPool()
+			if !cp.AppendCertsFromPEM(b) {
+				t.Fatalf("failed to append certificates")
+			}
+			cert, err := tls.LoadX509KeyPair(testdata.Path("x509/client1_cert.pem"), testdata.Path("x509/client1_key.pem"))
+			if err != nil {
+				t.Fatal(err)
+			}
+			creds = credentials.NewTLS(&tls.Config{
+				ServerName:   "x.test.example.com",
+				RootCAs:      cp,
+				Certificates: []tls.Certificate{cert},
+			})
+		} else {
+			creds, err = credentials.NewClientTLSFromFile(testdata.Path("x509/server_ca_cert.pem"), "x.test.example.com")
+		}
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	return creds
+}
+
+func (s) TestClientSideXDSSPIFFE(t *testing.T) {
+	managementServer, nodeID, _, xdsResolver := setup.ManagementServerAndResolver(t)
+
+	serverCreds := testutils.CreateServerTLSCredentialsCompatibleWithSPIFFE(t, tls.RequireAndVerifyClientCert)
+	server := stubserver.StartTestService(t, nil, grpc.Creds(serverCreds))
+	defer server.Stop()
+
+	const serviceName = "my-service-client-side-xds"
+	resources := e2e.DefaultClientResources(e2e.ResourceParams{
+		DialTarget: serviceName,
+		NodeID:     nodeID,
+		Host:       "localhost",
+		Port:       testutils.ParsePort(t, server.Address),
+		SecLevel:   e2e.SecurityLevelMTLS,
+	})
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+	if err := managementServer.Update(ctx, resources); err != nil {
+		t.Fatal(err)
+	}
+	opts := xds.ClientOptions{FallbackCreds: makeFallbackClientCreds(t, true, true)}
+	creds, err := xds.NewClientCredentials(opts)
+	if err != nil {
+		t.Fatalf("NewClientCredentials(%v) failed: %v", opts, err)
+	}
+
+	// Create a ClientConn and make a successful RPC.
+	cc, err := grpc.NewClient(fmt.Sprintf("xds:///%s", serviceName), grpc.WithTransportCredentials(creds), grpc.WithResolvers(xdsResolver))
 	if err != nil {
 		t.Fatalf("failed to dial local test server: %v", err)
 	}
