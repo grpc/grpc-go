cfaf50d335fe799a049b1f91a121b0f1a52b9119
janardhanvissa <47281167+janardhanvissa@users.noreply.github.com>
2025-05-12 10:57:47 +0530

    cleanup: replace dial with newclient (#8196)

--- credentials/insecure/insecure.go
+++ credentials/insecure/insecure.go
@@ -30,7 +30,7 @@ import (
 // NewCredentials returns a credentials which disables transport security.
 //
 // Note that using this credentials with per-RPC credentials which require
-// transport security is incompatible and will cause grpc.Dial() to fail.
+// transport security is incompatible and will cause RPCs to fail.
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
 }
