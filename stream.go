154642748b3bf6b65abad08412acfe3d06bb0d66
Pranjali-2501 <pranjalisaxena@google.com>
2025-05-12 04:47:47 +0000

    change variable name from recvMsg to recvFirstMsg

--- stream.go
+++ stream.go
@@ -542,7 +542,7 @@ type clientStream struct {
 
 	sentLast bool // sent an end stream
 
-	recvMsg bool // received msg from server
+	recvFirstMsg bool // received first msg from server
 
 	methodConfig *MethodConfig
 
@@ -1136,7 +1136,7 @@ func (a *csAttempt) recvMsg(m any, payInfo *payloadInfo) (err error) {
 			if statusErr := a.transportStream.Status().Err(); statusErr != nil {
 				return statusErr
 			}
-			if cs.desc.ClientStreams && !cs.desc.ServerStreams && !cs.recvMsg{
+			if cs.desc.ClientStreams && !cs.desc.ServerStreams && !cs.recvFirstMsg {
 				return status.Errorf(codes.Internal, "client streaming cardinality violation")
 			}
 			return io.EOF // indicates successful end of stream.
@@ -1145,7 +1145,7 @@ func (a *csAttempt) recvMsg(m any, payInfo *payloadInfo) (err error) {
 		return toRPCErr(err)
 	}
 	if cs.desc.ClientStreams {
-		cs.recvMsg = true
+		cs.recvFirstMsg = true
 	}
 	if a.trInfo != nil {
 		a.mu.Lock()
