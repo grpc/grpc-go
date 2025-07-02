aa57e6af6cbc8e1285315b338b092420f014c732
Easwar Swaminathan <easwars@google.com>
2025-07-02 05:24:14 -0700

    xds: cleanup internal testing functions for env vars that have long been removed (#8413)

--- test/xds/xds_rls_clusterspecifier_plugin_test.go
+++ test/xds/xds_rls_clusterspecifier_plugin_test.go
@@ -25,7 +25,6 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/internal"
 	"google.golang.org/grpc/internal/stubserver"
 	"google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/internal/testutils/rls"
@@ -100,9 +99,6 @@ func (s) TestRLSinxDS(t *testing.T) {
 }
 
 func testRLSinxDS(t *testing.T, lbPolicy e2e.LoadBalancingPolicy) {
-	internal.RegisterRLSClusterSpecifierPluginForTesting()
-	defer internal.UnregisterRLSClusterSpecifierPluginForTesting()
-
 	// Set up all components and configuration necessary - management server,
 	// xDS resolver, fake RLS Server, and xDS configuration which specifies an
 	// RLS Balancer that communicates to this set up fake RLS Server.
