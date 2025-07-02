06f350c28365134d6b099813726f5b0a91c33c09
Vinothkumar <vinothkumarr@google.com>
2025-07-02 17:40:05 +0000

    Fixed the server trace issues

--- stats/opentelemetry/trace.go
+++ stats/opentelemetry/trace.go
@@ -42,15 +42,20 @@ func populateSpan(rs stats.RPCStats, ai *attemptInfo) {
 		// Note: Go always added Client and FailFast attributes even though they are not
 		// defined by the OpenCensus gRPC spec. Thus, they are unimportant for
 		// correctness.
-		span.SetAttributes(
+		attrs := []attribute.KeyValue{
 			attribute.Bool("Client", rs.Client),
 			attribute.Bool("FailFast", rs.FailFast),
-			attribute.Int64("previous-rpc-attempts", int64(ai.previousRPCAttempts.Load())),
-			attribute.Bool("transparent-retry", rs.IsTransparentRetryAttempt),
-		)
+		}
+		if rs.Client {
+			attrs = append(attrs,
+				attribute.Int64("previous-rpc-attempts", int64(ai.previousRPCAttempts.Load())),
+				attribute.Bool("transparent-retry", rs.IsTransparentRetryAttempt),
+			)
+		}
+		span.SetAttributes(attrs...)
 		// Increment retry count for the next attempt if not a transparent
 		// retry.
-		if !rs.IsTransparentRetryAttempt {
+		if !rs.IsTransparentRetryAttempt && rs.Client {
 			ai.previousRPCAttempts.Add(1)
 		}
 	case *stats.PickerUpdated:
