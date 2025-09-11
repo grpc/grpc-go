8420f3ff9ce4617369e054cedb51fda6d45c3340
Arjan Singh Bal <46515553+arjan-bal@users.noreply.github.com>
2025-09-11 14:02:34 +0530

    transport: avoid slice reallocation during header creation (#8547)
    This PR improves the size estimate while pre-allocating `headerFields`
    to avoid reallocations, which pprof showed were responsible for ~4% of
    total memory allocations. This change improves performance, increasing
    QPS by 1% while reducing bytes/op by 4% and latencies by 0.3-4%.
    
    ## Tested
    ```sh
    go run benchmark/benchresult/main.go unary-before unary-after       
    unary-networkMode_Local-bufConn_false-keepalive_false-benchTime_1m0s-trace_false-latency_0s-kbps_0-MTU_0-maxConcurrentCalls_120-reqSiz
    e_1024B-respSize_1024B-compressor_off-channelz_false-preloader_false-clientReadBufferSize_-1-clientWriteBufferSize_-1-serverReadBuffer
    Size_-1-serverWriteBufferSize_-1-sleepBetweenRPCs_0s-connections_1-recvBufferPool_simple-sharedWriteBuffer_false
                   Title       Before        After Percentage
                TotalOps      6327736      6390728     1.00%
                 SendOps            0            0      NaN%
                 RecvOps            0            0      NaN%
                Bytes/op     13903.23     13354.55    -3.95%
               Allocs/op       156.22       155.23    -0.64%
                 ReqT/op 863946888.53 872547396.27     1.00%
                RespT/op 863946888.53 872547396.27     1.00%
                50th-Lat    1.00991ms   1.006914ms    -0.30%
                90th-Lat   1.678329ms   1.610331ms    -4.05%
                99th-Lat   2.517556ms   2.497122ms    -0.81%
                 Avg-Lat   1.136117ms   1.125311ms    -0.95%
               GoVersion     go1.24.4     go1.24.4
             GrpcVersion   1.76.0-dev   1.76.0-dev
    ```
    
    RELEASE NOTES:
    * client: Improve header slice length estimate to reduce re-allocations.

--- internal/transport/http2_client.go
+++ internal/transport/http2_client.go
@@ -556,6 +556,19 @@ func (t *http2Client) createHeaderFields(ctx context.Context, callHdr *CallHdr) 
 	// Make the slice of certain predictable size to reduce allocations made by append.
 	hfLen := 7 // :method, :scheme, :path, :authority, content-type, user-agent, te
 	hfLen += len(authData) + len(callAuthData)
+	registeredCompressors := t.registeredCompressors
+	if callHdr.PreviousAttempts > 0 {
+		hfLen++
+	}
+	if callHdr.SendCompress != "" {
+		hfLen++
+	}
+	if registeredCompressors != "" {
+		hfLen++
+	}
+	if _, ok := ctx.Deadline(); ok {
+		hfLen++
+	}
 	headerFields := make([]hpack.HeaderField, 0, hfLen)
 	headerFields = append(headerFields, hpack.HeaderField{Name: ":method", Value: "POST"})
 	headerFields = append(headerFields, hpack.HeaderField{Name: ":scheme", Value: t.scheme})
@@ -568,7 +581,6 @@ func (t *http2Client) createHeaderFields(ctx context.Context, callHdr *CallHdr) 
 		headerFields = append(headerFields, hpack.HeaderField{Name: "grpc-previous-rpc-attempts", Value: strconv.Itoa(callHdr.PreviousAttempts)})
 	}
 
-	registeredCompressors := t.registeredCompressors
 	if callHdr.SendCompress != "" {
 		headerFields = append(headerFields, hpack.HeaderField{Name: "grpc-encoding", Value: callHdr.SendCompress})
 		// Include the outgoing compressor name when compressor is not registered
