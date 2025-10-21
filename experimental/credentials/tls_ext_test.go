b59a01980965c4b8189936158b5c052e4c0649e1
Arjan Bal <arjansbal@google.com>
2025-10-21 23:21:41 +0530

    Revert "credentials/tls: Remove environment variable for disabling ALPN (#8660)"
    This reverts commit 0037c61d300991605f745bba4d145f406c6e392d.

--- experimental/credentials/tls_ext_test.go
+++ experimental/credentials/tls_ext_test.go
@@ -33,6 +33,7 @@ import (
 	"google.golang.org/grpc/codes"
 	credsstable "google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/experimental/credentials"
+	"google.golang.org/grpc/internal/envconfig"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/internal/stubserver"
 	"google.golang.org/grpc/status"
@@ -410,6 +411,12 @@ func (s) TestTLS_CipherSuitesOverridable(t *testing.T) {
 // correctly for a server that doesn't specify the NextProtos field and uses
 // GetConfigForClient to provide the TLS config during the handshake.
 func (s) TestTLS_ServerConfiguresALPNByDefault(t *testing.T) {
+	initialVal := envconfig.EnforceALPNEnabled
+	defer func() {
+		envconfig.EnforceALPNEnabled = initialVal
+	}()
+	envconfig.EnforceALPNEnabled = true
+
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
 
@@ -446,104 +453,152 @@ func (s) TestTLS_ServerConfiguresALPNByDefault(t *testing.T) {
 // TestTLS_DisabledALPNClient tests the behaviour of TransportCredentials when
 // connecting to a server that doesn't support ALPN.
 func (s) TestTLS_DisabledALPNClient(t *testing.T) {
-	listener, err := tls.Listen("tcp", "localhost:0", &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-		NextProtos:   []string{}, // Empty list indicates ALPN is disabled.
-	})
-	if err != nil {
-		t.Fatalf("Error starting TLS server: %v", err)
-	}
-
-	errCh := make(chan error, 1)
-	go func() {
-		conn, err := listener.Accept()
-		if err != nil {
-			errCh <- fmt.Errorf("listener.Accept returned error: %v", err)
-		} else {
-			// The first write to the TLS listener initiates the TLS handshake.
-			conn.Write([]byte("Hello, World!"))
-			conn.Close()
-		}
-		close(errCh)
+	initialVal := envconfig.EnforceALPNEnabled
+	defer func() {
+		envconfig.EnforceALPNEnabled = initialVal
 	}()
 
-	serverAddr := listener.Addr().String()
-	conn, err := net.Dial("tcp", serverAddr)
-	if err != nil {
-		t.Fatalf("net.Dial(%s) failed: %v", serverAddr, err)
-	}
-	defer conn.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cancel()
-
-	clientCfg := tls.Config{
-		ServerName: serverName,
-		RootCAs:    certPool,
-		NextProtos: []string{"h2"},
-	}
-	_, _, err = credentials.NewTLSWithALPNDisabled(&clientCfg).ClientHandshake(ctx, serverName, conn)
-
-	if gotErr, wantErr := (err != nil), false; gotErr != wantErr {
-		t.Errorf("ClientHandshake returned unexpected error: got=%v, want=%t", err, wantErr)
+	tests := []struct {
+		name         string
+		alpnEnforced bool
+		wantErr      bool
+	}{
+		{
+			name: "enforced",
+		},
+		{
+			name: "not_enforced",
+		},
 	}
 
-	select {
-	case err := <-errCh:
-		if err != nil {
-			t.Fatalf("Unexpected error received from server: %v", err)
-		}
-	case <-ctx.Done():
-		t.Fatalf("Timeout waiting for error from server")
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			envconfig.EnforceALPNEnabled = tc.alpnEnforced
+
+			listener, err := tls.Listen("tcp", "localhost:0", &tls.Config{
+				Certificates: []tls.Certificate{serverCert},
+				NextProtos:   []string{}, // Empty list indicates ALPN is disabled.
+			})
+			if err != nil {
+				t.Fatalf("Error starting TLS server: %v", err)
+			}
+
+			errCh := make(chan error, 1)
+			go func() {
+				conn, err := listener.Accept()
+				if err != nil {
+					errCh <- fmt.Errorf("listener.Accept returned error: %v", err)
+				} else {
+					// The first write to the TLS listener initiates the TLS handshake.
+					conn.Write([]byte("Hello, World!"))
+					conn.Close()
+				}
+				close(errCh)
+			}()
+
+			serverAddr := listener.Addr().String()
+			conn, err := net.Dial("tcp", serverAddr)
+			if err != nil {
+				t.Fatalf("net.Dial(%s) failed: %v", serverAddr, err)
+			}
+			defer conn.Close()
+
+			ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+			defer cancel()
+
+			clientCfg := tls.Config{
+				ServerName: serverName,
+				RootCAs:    certPool,
+				NextProtos: []string{"h2"},
+			}
+			_, _, err = credentials.NewTLSWithALPNDisabled(&clientCfg).ClientHandshake(ctx, serverName, conn)
+
+			if gotErr := (err != nil); gotErr != tc.wantErr {
+				t.Errorf("ClientHandshake returned unexpected error: got=%v, want=%t", err, tc.wantErr)
+			}
+
+			select {
+			case err := <-errCh:
+				if err != nil {
+					t.Fatalf("Unexpected error received from server: %v", err)
+				}
+			case <-ctx.Done():
+				t.Fatalf("Timeout waiting for error from server")
+			}
+		})
 	}
 }
 
 // TestTLS_DisabledALPNServer tests the behaviour of TransportCredentials when
 // accepting a request from a client that doesn't support ALPN.
 func (s) TestTLS_DisabledALPNServer(t *testing.T) {
-	listener, err := net.Listen("tcp", "localhost:0")
-	if err != nil {
-		t.Fatalf("Error starting server: %v", err)
-	}
-
-	errCh := make(chan error, 1)
-	go func() {
-		conn, err := listener.Accept()
-		if err != nil {
-			errCh <- fmt.Errorf("listener.Accept returned error: %v", err)
-			return
-		}
-		defer conn.Close()
-		serverCfg := tls.Config{
-			Certificates: []tls.Certificate{serverCert},
-			NextProtos:   []string{"h2"},
-		}
-		_, _, err = credentials.NewTLSWithALPNDisabled(&serverCfg).ServerHandshake(conn)
-		if gotErr, wantErr := (err != nil), false; gotErr != wantErr {
-			t.Errorf("ServerHandshake returned unexpected error: got=%v, want=%t", err, wantErr)
-		}
-		close(errCh)
+	initialVal := envconfig.EnforceALPNEnabled
+	defer func() {
+		envconfig.EnforceALPNEnabled = initialVal
 	}()
 
-	serverAddr := listener.Addr().String()
-	clientCfg := &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-		NextProtos:   []string{}, // Empty list indicates ALPN is disabled.
-		RootCAs:      certPool,
-		ServerName:   serverName,
+	tests := []struct {
+		name         string
+		alpnEnforced bool
+		wantErr      bool
+	}{
+		{
+			name: "enforced",
+		},
+		{
+			name: "not_enforced",
+		},
 	}
-	conn, err := tls.Dial("tcp", serverAddr, clientCfg)
-	if err != nil {
-		t.Fatalf("tls.Dial(%s) failed: %v", serverAddr, err)
-	}
-	defer conn.Close()
 
-	select {
-	case <-time.After(defaultTestTimeout):
-		t.Fatal("Timed out waiting for completion")
-	case err := <-errCh:
-		if err != nil {
-			t.Fatalf("Unexpected server error: %v", err)
-		}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			envconfig.EnforceALPNEnabled = tc.alpnEnforced
+
+			listener, err := net.Listen("tcp", "localhost:0")
+			if err != nil {
+				t.Fatalf("Error starting server: %v", err)
+			}
+
+			errCh := make(chan error, 1)
+			go func() {
+				conn, err := listener.Accept()
+				if err != nil {
+					errCh <- fmt.Errorf("listener.Accept returned error: %v", err)
+					return
+				}
+				defer conn.Close()
+				serverCfg := tls.Config{
+					Certificates: []tls.Certificate{serverCert},
+					NextProtos:   []string{"h2"},
+				}
+				_, _, err = credentials.NewTLSWithALPNDisabled(&serverCfg).ServerHandshake(conn)
+				if gotErr := (err != nil); gotErr != tc.wantErr {
+					t.Errorf("ServerHandshake returned unexpected error: got=%v, want=%t", err, tc.wantErr)
+				}
+				close(errCh)
+			}()
+
+			serverAddr := listener.Addr().String()
+			clientCfg := &tls.Config{
+				Certificates: []tls.Certificate{serverCert},
+				NextProtos:   []string{}, // Empty list indicates ALPN is disabled.
+				RootCAs:      certPool,
+				ServerName:   serverName,
+			}
+			conn, err := tls.Dial("tcp", serverAddr, clientCfg)
+			if err != nil {
+				t.Fatalf("tls.Dial(%s) failed: %v", serverAddr, err)
+			}
+			defer conn.Close()
+
+			select {
+			case <-time.After(defaultTestTimeout):
+				t.Fatal("Timed out waiting for completion")
+			case err := <-errCh:
+				if err != nil {
+					t.Fatalf("Unexpected server error: %v", err)
+				}
+			}
+		})
 	}
 }
