7a7b59627ba7ca0070c4306254970bd1244f1cbd
Madhav Bissa <mbissa@gmail.com>
2025-11-13 08:31:04 +0000

    keeping parsing error as Internal

--- internal/transport/transport_test.go
+++ internal/transport/transport_test.go
@@ -2681,7 +2681,7 @@ func (s) TestClientDecodeHeader(t *testing.T) {
 				},
 			},
 			wantStatus: status.New(
-				codes.Unknown,
+				codes.Internal,
 				"transport: malformed grpc-status: strconv.ParseInt: parsing \"xxxx\": invalid syntax",
 			),
 		},
@@ -2813,7 +2813,7 @@ func (s) TestClientDecodeTrailer(t *testing.T) {
 				},
 			},
 			wantEndStreamStatus: status.New(
-				codes.Unknown,
+				codes.Internal,
 				"transport: malformed grpc-status: strconv.ParseInt: parsing \"xxxx\": invalid syntax",
 			),
 		},
