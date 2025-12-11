b162fa92b7300f75c1a55eecb36c128e2ed2065e
Easwar Swaminathan <easwars@google.com>
2025-12-11 01:17:10 +0000

    get rid of e2e test and replace with unit and functional test

--- balancer/randomsubsetting/randomsubsetting_test.go
+++ balancer/randomsubsetting/randomsubsetting_test.go
@@ -22,26 +22,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"math"
 	"strings"
 	"testing"
 	"time"
 
 	xxhash "github.com/cespare/xxhash/v2"
 	"github.com/google/go-cmp/cmp"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/internal"
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/internal/balancer/stub"
 	"google.golang.org/grpc/internal/grpctest"
 	iserviceconfig "google.golang.org/grpc/internal/serviceconfig"
-	"google.golang.org/grpc/internal/stubserver"
-	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/resolver/manual"
 	"google.golang.org/grpc/serviceconfig"
 
-	testgrpc "google.golang.org/grpc/interop/grpc_testing"
-	testpb "google.golang.org/grpc/interop/grpc_testing"
+	_ "google.golang.org/grpc/balancer/roundrobin" // For round_robin LB policy in tests
 )
 
 var defaultTestTimeout = 5 * time.Second
@@ -198,167 +193,69 @@ func (s) TestCalculateSubset_EndpointsRetainHashValues(t *testing.T) {
 	}
 }
 
-func startTestServiceBackends(t *testing.T, num int) []resolver.Address {
-	t.Helper()
+func (s) TestSubsettingBalancer_DeterministicSubset(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
 
-	addresses := make([]resolver.Address, num)
-	for i := 0; i < num; i++ {
-		server := stubserver.StartTestService(t, nil)
-		t.Cleanup(server.Stop)
-		addresses[i] = resolver.Address{Addr: server.Address}
-	}
-	return addresses
-}
-
-func startTestSetupClients(t *testing.T, clientsCount int, subsetSize int, addresses []resolver.Address) []testgrpc.TestServiceClient {
-	t.Helper()
-
-	clients := make([]testgrpc.TestServiceClient, clientsCount)
-	ccs := make([]*grpc.ClientConn, clientsCount)
-	var err error
-
-	for i := 0; i < clientsCount; i++ {
-		mr := manual.NewBuilderWithScheme("subsetting-e2e")
-		jsonConfig := fmt.Sprintf(`
-		{
-		  "loadBalancingConfig": [
-			{
-			  "random_subsetting": {
-				"subsetSize": %d,
-				"childPolicy": [{"round_robin": {}}]
-			  }
+	// Register the stub balancer builder, which will be used as the child
+	// policy in the random_subsetting balancer.
+	updateCh := make(chan balancer.ClientConnState, 1)
+	stub.Register("stub-child-balancer", stub.BalancerFuncs{
+		UpdateClientConnState: func(_ *stub.BalancerData, s balancer.ClientConnState) error {
+			select {
+			case <-ctx.Done():
+			case updateCh <- s:
 			}
-		  ]
-		}`, subsetSize)
-
-		sc := internal.ParseServiceConfig.(func(string) *serviceconfig.ParseResult)(string(jsonConfig))
-		mr.InitialState(resolver.State{
-			Addresses:     addresses,
-			ServiceConfig: sc,
-		})
-
-		ccs[i], err = grpc.Dial(mr.Scheme()+":///", grpc.WithResolvers(mr), grpc.WithTransportCredentials(insecure.NewCredentials()))
-		if err != nil {
-			t.Fatalf("grpc.Dial() failed: %v", err)
-		}
-		clients[i] = testgrpc.NewTestServiceClient(ccs[i])
-	}
-
-	t.Cleanup(func() {
-		for _, cc := range ccs {
-			cc.Close()
-		}
+			return nil
+		},
 	})
-	return clients
-}
 
-func checkRoundRobinRPCs(ctx context.Context, t *testing.T, clients []testgrpc.TestServiceClient, subsetSize int, maxDiff int) {
-	clientsPerBackend := map[string]map[int]struct{}{}
+	// Create a random_subsetting balancer.
+	tcc := testutils.NewBalancerClientConn(t)
+	rsb := balancer.Get(Name).Build(tcc, balancer.BuildOptions{})
+	defer rsb.Close()
 
-	for clientIdx, client := range clients {
-		// make sure that every client send exactly 1 request to each server in its subset
-		for i := 0; i < subsetSize; i++ {
-			var peer peer.Peer
-			_, err := client.EmptyCall(ctx, &testpb.Empty{}, grpc.Peer(&peer))
-			if err != nil {
-				t.Fatalf("failed to call server: %v", err)
-			}
-			if peer.Addr != nil {
-				if m, ok := clientsPerBackend[peer.Addr.String()]; !ok {
-					clientsPerBackend[peer.Addr.String()] = map[int]struct{}{clientIdx: {}}
-				} else if _, ok := m[clientIdx]; !ok {
-					m[clientIdx] = struct{}{}
-				} else {
-					// The backend receives a second request from the same client. This could happen if the client have 1 backend in READY
-					// state while the other  are CONNECTING. In this case round_robbin will pick the same address twice.
-					// We are going to retry after short timeout.
-					time.Sleep(10 * time.Microsecond)
-					i--
-				}
-			} else {
-				t.Fatalf("peer.Addr == nil, peer: %v", peer)
-			}
+	// Prepare the configuration and resolver state to be passed to the
+	// random_subsetting balancer.
+	endpoints := makeEndpoints(10)
+	state := balancer.ClientConnState{
+		ResolverState: resolver.State{Endpoints: endpoints},
+		BalancerConfig: &lbConfig{
+			SubsetSize:  5,
+			ChildPolicy: &iserviceconfig.BalancerConfig{Name: "stub-child-balancer"},
+		},
+	}
+
+	// Send the resolver state to the random_subsetting balancer and verify that
+	// the child policy receives the expected number of endpoints.
+	if err := rsb.UpdateClientConnState(state); err != nil {
+		t.Fatalf("UpdateClientConnState failed: %v", err)
+	}
+
+	var wantEndpoints []resolver.Endpoint
+	select {
+	case s := <-updateCh:
+		if len(s.ResolverState.Endpoints) != 5 {
+			t.Fatalf("Child policy received %d endpoints, want 5", len(s.ResolverState.Endpoints))
 		}
+		// Store the subset for the next comparison.
+		wantEndpoints = s.ResolverState.Endpoints
+	case <-ctx.Done():
+		t.Fatal("Timed out waiting for child policy to receive an update")
 	}
 
-	minClientsPerBackend := math.MaxInt
-	maxClientsPerBackend := 0
-	for _, v := range clientsPerBackend {
-		if len(v) < minClientsPerBackend {
-			minClientsPerBackend = len(v)
+	// Call UpdateClientConnState again with the same configuration.
+	if err := rsb.UpdateClientConnState(state); err != nil {
+		t.Fatalf("Second UpdateClientConnState failed: %v", err)
+	}
+
+	// Verify that the child policy receives the same subset of endpoints.
+	select {
+	case s := <-updateCh:
+		if diff := cmp.Diff(wantEndpoints, s.ResolverState.Endpoints); diff != "" {
+			t.Fatalf("Child policy received a different subset of endpoints on second update, diff (-want +got):\n%s", diff)
 		}
-		if len(v) > maxClientsPerBackend {
-			maxClientsPerBackend = len(v)
-		}
-	}
-
-	if maxClientsPerBackend > minClientsPerBackend+maxDiff {
-		t.Fatalf("the difference between min and max clients per backend should be <= %d, clientsPerBackend: %v", maxDiff, clientsPerBackend)
-	}
-}
-
-func (s) TestSubsettingE2E(t *testing.T) {
-	// Save the original hash seed and restore it at the end of the test.
-	originalHashSeed := hashSeed
-	defer func() {
-		hashSeed = originalHashSeed
-	}()
-
-	// Set a fixed hash seed to make the test deterministic.
-	hashSeed = func() uint64 { return 1 }
-	tests := []struct {
-		name       string
-		subsetSize int
-		clients    int
-		backends   int
-		maxDiff    int
-	}{
-		{
-			name:       "backends could be evenly distributed between small number of clients",
-			backends:   3,
-			clients:    2,
-			subsetSize: 2,
-			maxDiff:    1,
-		},
-		{
-			name:       "backends could be evenly distributed between clients",
-			backends:   12,
-			clients:    8,
-			subsetSize: 3,
-			maxDiff:    3,
-		},
-		{
-			name:       "backends could NOT be evenly distributed between clients",
-			backends:   37,
-			clients:    22,
-			subsetSize: 5,
-			maxDiff:    15,
-		},
-		{
-			name:       "backends %% subsetSize == 0, but there are not enough clients to fill the last round",
-			backends:   20,
-			clients:    7,
-			subsetSize: 5,
-			maxDiff:    20,
-		},
-		{
-			name:       "last round is completely filled, but there are some excluded backends on every round",
-			backends:   21,
-			clients:    15,
-			subsetSize: 5,
-			maxDiff:    4,
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			addresses := startTestServiceBackends(t, test.backends)
-
-			clients := startTestSetupClients(t, test.clients, test.subsetSize, addresses)
-
-			ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
-			defer cancel()
-
-			checkRoundRobinRPCs(ctx, t, clients, test.subsetSize, test.maxDiff)
-		})
+	case <-ctx.Done():
+		t.Fatal("Timed out waiting for second child policy update")
 	}
 }
