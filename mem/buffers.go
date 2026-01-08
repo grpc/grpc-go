c90acd2eb2105d0a03ce25eadbf8ff720e0854c4
Arjan Bal <arjansbal@google.com>
2026-01-08 16:02:03 +0530

    use atomic op for sanity check

--- mem/buffers.go
+++ mem/buffers.go
@@ -71,17 +71,8 @@ func IsBelowBufferPoolingThreshold(size int) bool {
 	return size <= bufferPoolingThreshold
 }
 
-type atomicInt32 struct {
-	atomic.Int32
-	// initialized enables sanity checks without the overhead of atomic
-	// operations. This field is not safe for concurrent access and is used in a
-	// best-effort manner for assertion purposes only. It does not play a role
-	// in the concurrent logic of reference counting.
-	initialized bool
-}
-
 type buffer struct {
-	refs atomicInt32
+	refs atomic.Int32
 	data []byte
 
 	// rootBuf is the buffer responsible for returning origData to the pool
@@ -122,8 +113,7 @@ func NewBuffer(data *[]byte, pool BufferPool) Buffer {
 	b.data = *data
 	b.pool = pool
 	b.rootBuf = b
-	b.refs.Add(1)
-	b.refs.initialized = true
+	b.refs.Store(1)
 	return b
 }
 
@@ -146,33 +136,27 @@ func Copy(data []byte, pool BufferPool) Buffer {
 }
 
 func (b *buffer) ReadOnlyData() []byte {
-	if !b.refs.initialized {
+	if b.refs.Load() <= 0 {
 		panic("Cannot read freed buffer")
 	}
 	return b.data
 }
 
 func (b *buffer) Ref() {
-	if !b.refs.initialized {
+	if b.refs.Add(1) <= 1 {
 		panic("Cannot ref freed buffer")
 	}
-	b.refs.Add(1)
 }
 
 func (b *buffer) Free() {
-	if !b.refs.initialized {
-		panic("Cannot free freed buffer")
-	}
-
 	refs := b.refs.Add(-1)
-	if refs > 0 {
-		return
-	}
 	if refs < 0 {
 		panic("Cannot free freed buffer")
 	}
+	if refs > 0 {
+		return
+	}
 
-	b.refs.initialized = false
 	b.data = nil
 	if b.rootBuf == b {
 		// This buffer is the owner of the data slice and its ref count reached
@@ -197,16 +181,14 @@ func (b *buffer) Len() int {
 }
 
 func (b *buffer) split(n int) (Buffer, Buffer) {
-	if !b.refs.initialized {
+	if b.rootBuf == nil || b.rootBuf.refs.Add(1) <= 1 {
 		panic("Cannot split freed buffer")
 	}
 
-	b.rootBuf.Ref()
 	split := newBuffer()
 	split.data = b.data[n:]
 	split.rootBuf = b.rootBuf
-	split.refs.initialized = true
-	split.refs.Add(1)
+	split.refs.Store(1)
 
 	b.data = b.data[:n]
 
@@ -214,7 +196,7 @@ func (b *buffer) split(n int) (Buffer, Buffer) {
 }
 
 func (b *buffer) read(buf []byte) (int, Buffer) {
-	if !b.refs.initialized {
+	if b.refs.Load() <= 0 {
 		panic("Cannot read freed buffer")
 	}
 
