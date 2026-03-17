8091efdc91033d101ce4dbaa99ccb44a2df106df
Easwar Swaminathan <easwars@google.com>
2026-03-17 16:35:32 -0700

    grpc: enforce strict path checking for incoming requests on the server (#8985)
    RELEASE NOTES:
    * server: fix an authorization bypass where malformed :path headers
    (missing the leading slash) could bypass path-based restricted "deny"
    rules in interceptors like `grpc/authz`. Any request with a
    non-canonical path is now immediately rejected with an `Unimplemented`
    error.

--- internal/envconfig/envconfig.go
+++ internal/envconfig/envconfig.go
@@ -103,6 +103,22 @@ var (
 	// environment variable "GRPC_GO_EXPERIMENTAL_XDS_RESOURCE_PANIC_RECOVERY"
 	// to "false".
 	XDSRecoverPanicInResourceParsing = boolFromEnv("GRPC_GO_EXPERIMENTAL_XDS_RESOURCE_PANIC_RECOVERY", true)
+
+	// DisableStrictPathChecking indicates whether strict path checking is
+	// disabled. This feature can be disabled by setting the environment
+	// variable GRPC_GO_EXPERIMENTAL_DISABLE_STRICT_PATH_CHECKING to "true".
+	//
+	// When strict path checking is enabled, gRPC will reject requests with
+	// paths that do not conform to the gRPC over HTTP/2 specification found at
+	// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md.
+	//
+	// When disabled, gRPC will allow paths that do not contain a leading slash.
+	// Enabling strict path checking is recommended for security reasons, as it
+	// prevents potential path traversal vulnerabilities.
+	//
+	// A future release will remove this environment variable, enabling strict
+	// path checking behavior unconditionally.
+	DisableStrictPathChecking = boolFromEnv("GRPC_GO_EXPERIMENTAL_DISABLE_STRICT_PATH_CHECKING", false)
 )
 
 func boolFromEnv(envVar string, def bool) bool {
