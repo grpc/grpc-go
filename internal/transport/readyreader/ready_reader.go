934dc2f8efb9c9c993891561b877f37ba642b29a
Arjan Bal <arjansbal@google.com>
2026-04-10 21:54:32 +0530

    add buffered reader

--- internal/transport/readyreader/ready_reader.go
+++ internal/transport/readyreader/ready_reader.go
@@ -20,8 +20,8 @@
 package readyreader
 
 import (
+	"errors"
 	"io"
-	"net"
 	"syscall"
 
 	"google.golang.org/grpc/mem"
@@ -85,12 +85,12 @@ func (c *nonBlockingReader) ReadOnReady(bufSize int, pool mem.BufferPool) (buf *
 }
 
 type blockingReader struct {
-	conn net.Conn
+	reader io.Reader
 }
 
 func (c *blockingReader) ReadOnReady(bufSize int, pool mem.BufferPool) (*[]byte, int, error) {
 	buf := pool.Get(bufSize)
-	n, err := c.conn.Read(*buf)
+	n, err := c.reader.Read(*buf)
 	if err != nil {
 		pool.Put(buf)
 		return nil, 0, err
@@ -98,16 +98,126 @@ func (c *blockingReader) ReadOnReady(bufSize int, pool mem.BufferPool) (*[]byte,
 	return buf, n, nil
 }
 
-// New detects if [syscall.RawConn] is available for non-memory-pinning reads.
-// If [syscall.RawConn] is unavailable, it falls back to using the simpler
-// [net.Conn] interface for reads.
-func New(conn net.Conn) Reader {
-	sysConn, ok := conn.(syscall.Conn)
-	if !ok || !isRawConnSupported() {
-		return &blockingReader{conn: conn}
+// NewNonBlocking returns a ReadyReader if the passed reader supports
+// non-memory-pinning reads, else nil.
+func NewNonBlocking(r io.Reader) Reader {
+	if !isRawConnSupported() {
+		return nil
+	}
+	sysConn, ok := r.(syscall.Conn)
+	if !ok {
+		return nil
 	}
 	if raw, err := sysConn.SyscallConn(); err == nil {
 		return &nonBlockingReader{raw: raw}
 	}
-	return &blockingReader{conn: conn}
+	return nil
 }
+
+// New detects if [syscall.RawConn] is available for non-memory-pinning reads.
+// If [syscall.RawConn] is unavailable, it falls back to using the simpler
+// [net.Conn] interface for reads.
+func New(r io.Reader) Reader {
+	if r := NewNonBlocking(r); r != nil {
+		return r
+	}
+	return &blockingReader{reader: r}
+}
+
+// bufReadyReader implements buffering for a ReadyReader object.
+// A new bufReadyReader is created by calling [newBufReadyReader].
+type bufReadyReader struct {
+	buf       *[]byte
+	pool      mem.BufferPool
+	bufSize   int
+	rd        Reader // reader provided by the caller
+	r, w      int    // buf read and write positions
+	err       error
+	constPool constBufferPool // stored as a field to avoid heap allocations.
+}
+
+// NewBuffered returns a new [bufferedReadyReader] whose buffer has the
+// specified size. If the argument.
+func NewBuffered(rd Reader, size int, pool mem.BufferPool) io.Reader {
+	r := &bufReadyReader{
+		rd:      rd,
+		pool:    pool,
+		bufSize: size,
+	}
+	return r
+}
+
+var errNegativeRead = errors.New("transport: reader returned negative count from Read")
+
+func (b *bufReadyReader) readErr() error {
+	err := b.err
+	b.err = nil
+	return err
+}
+
+func (b *bufReadyReader) buffered() int { return b.w - b.r }
+
+// Read reads data into p.
+// It returns the number of bytes read into p.
+// The bytes are taken from at most one Read on the underlying [ReadyReader],
+// hence n may be less than len(p).
+// If the underlying [ReadyReader] can return a non-zero count with io.EOF,
+// then this Read method can do so as well; see the [io.Reader] docs.
+func (b *bufReadyReader) Read(p []byte) (n int, err error) {
+	n = len(p)
+	if n == 0 {
+		if b.buffered() > 0 {
+			return 0, nil
+		}
+		return 0, b.readErr()
+	}
+	if b.r == b.w {
+		if b.err != nil {
+			return 0, b.readErr()
+		}
+		if len(p) >= b.bufSize {
+			// Large read, empty buffer.
+			// Read directly into p to avoid copy.
+			b.constPool.buffer = p
+			_, n, err := b.rd.ReadOnReady(len(p), &b.constPool)
+			b.err = err
+			if n < 0 {
+				panic(errNegativeRead)
+			}
+			return n, b.readErr()
+		}
+		// One read.
+		b.r = 0
+		b.w = 0
+		b.buf, n, b.err = b.rd.ReadOnReady(b.bufSize, b.pool)
+		if n < 0 {
+			panic(errNegativeRead)
+		}
+		if n == 0 {
+			return 0, b.readErr()
+		}
+		b.w += n
+	}
+
+	// copy as much as we can
+	// b.buf must be non-nil since b.r != b.w.
+	buf := *b.buf
+	n = copy(p, buf[b.r:b.w])
+	b.r += n
+	if b.r == b.w {
+		// Consumed entire buffer, release it.
+		b.pool.Put(b.buf)
+		b.buf = nil
+	}
+	return n, nil
+}
+
+type constBufferPool struct {
+	buffer []byte
+}
+
+func (p *constBufferPool) Get(int) *[]byte {
+	return &p.buffer
+}
+
+func (p *constBufferPool) Put(*[]byte) {}
