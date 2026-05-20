02e93c318a6bc9e62111e4df9e44da48cb4e3ffe
eshitachandwani <emchandwani@google.com>
2026-05-20 11:08:44 +0530

    review comments

--- internal/xds/matcher/string_matcher_test.go
+++ internal/xds/matcher/string_matcher_test.go
@@ -337,7 +337,7 @@ func TestCompileSafeRegex(t *testing.T) {
 		t.Run(test.desc, func(t *testing.T) {
 			re, err := CompileSafeRegex(test.pattern)
 			if (err != nil) != test.wantErr {
-				t.Fatalf("CompileSafeRegex(%q) returned error %q, want %q", test.pattern, err, test.wantErr)
+				t.Fatalf("CompileSafeRegex(%q) returned error %q, wantErr=%v", test.pattern, err, test.wantErr)
 			}
 			if err == nil {
 				if re.String() != test.wantRegex {
