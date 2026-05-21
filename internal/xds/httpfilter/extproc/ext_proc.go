037c6efe40c3fb4c6aebfbd158ae4cb592a1bff3
eshitachandwani <59800922+eshitachandwani@users.noreply.github.com>
2026-05-21 09:23:45 +0530

    extproc: register filter and parse base and override config (#9073)
    This PR is part of implementing [A93:
    xds-ext-proc](https://github.com/grpc/proposal/blob/master/A93-xds-ext-proc.md)
    
    This PR adds the ext_proc filter and the builder that implements the
    [builder](https://github.com/grpc/grpc-go/blob/06fc26a196350499dd0cf2dabdd5cf11bbee01a7/internal/xds/httpfilter/httpfilter.go#L48)
    interface. That includes parsing and validating the base and the
    override config. The registration of the filter is under the
    `GRPC_EXPERIMENTAL_XDS_EXT_PROC_ON_CLIENT` flag.
    
    #ext-proc-a93
    
    RELEASE NOTES: None

---
+++ internal/xds/httpfilter/extproc/ext_proc.go
@@ -0,0 +1,179 @@
+/*
+ *
+ * Copyright 2026 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package extproc implements the Envoy external processing HTTP filter.
+package extproc
+
+import (
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc/internal/envconfig"
+	"google.golang.org/grpc/internal/optional"
+	"google.golang.org/grpc/internal/xds/httpfilter"
+	"google.golang.org/grpc/internal/xds/matcher"
+	"google.golang.org/grpc/internal/xds/xdsclient/xdsresource"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+
+	v3corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	v3procfilterpb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/ext_proc/v3"
+)
+
+func init() {
+	if envconfig.XDSClientExtProcEnabled {
+		httpfilter.Register(builder{})
+	}
+}
+
+var parseGRPCServiceConfig = func(*v3corepb.GrpcService) (xdsresource.GRPCServiceConfig, error) {
+	return xdsresource.GRPCServiceConfig{}, fmt.Errorf("parseGRPCServiceConfig not implemented")
+}
+
+const defaultDeferredCloseTimeout = 5 * time.Second
+
+type builder struct{}
+
+func (builder) TypeURLs() []string {
+	return []string{
+		"type.googleapis.com/envoy.extensions.filters.http.ext_proc.v3.ExternalProcessor",
+		"type.googleapis.com/envoy.extensions.filters.http.ext_proc.v3.ExtProcPerRoute",
+	}
+}
+
+// validateBodyProcessingMode ensures that the body processing mode is either
+// NONE or GRPC.
+func validateBodyProcessingMode(mode *v3procfilterpb.ProcessingMode) error {
+	if m := mode.GetRequestBodyMode(); m != v3procfilterpb.ProcessingMode_NONE && m != v3procfilterpb.ProcessingMode_GRPC {
+		return fmt.Errorf("extproc: invalid request body mode %v: want %q or %q", m, "NONE", "GRPC")
+	}
+	if m := mode.GetResponseBodyMode(); m != v3procfilterpb.ProcessingMode_NONE && m != v3procfilterpb.ProcessingMode_GRPC {
+		return fmt.Errorf("extproc: invalid response body mode %v: want %q or %q", m, "NONE", "GRPC")
+	}
+	return nil
+}
+
+func (builder) ParseFilterConfig(cfg proto.Message) (httpfilter.FilterConfig, error) {
+	m, ok := cfg.(*anypb.Any)
+	if !ok {
+		return nil, fmt.Errorf("extproc: error parsing config %v: unknown type %T, want *anypb.Any", cfg, cfg)
+	}
+	msg := new(v3procfilterpb.ExternalProcessor)
+	if err := m.UnmarshalTo(msg); err != nil {
+		return nil, fmt.Errorf("extproc: failed to unmarshal config %v: %v", cfg, err)
+	}
+	if msg.GetProcessingMode() == nil {
+		return nil, fmt.Errorf("extproc: missing processing_mode in config %v", cfg)
+	}
+	if err := validateBodyProcessingMode(msg.GetProcessingMode()); err != nil {
+		return nil, err
+	}
+
+	if msg.GetGrpcService() == nil {
+		return nil, fmt.Errorf("extproc: empty grpc_service provided in config %v", cfg)
+	}
+	server, err := parseGRPCServiceConfig(msg.GetGrpcService())
+	if err != nil {
+		return nil, fmt.Errorf("extproc: failed to parse grpc_service %v", err)
+	}
+
+	mutationRules, err := httpfilter.HeaderMutationRulesFromProto(msg.GetMutationRules())
+	if err != nil {
+		return nil, err
+	}
+
+	var allowedHeaders, disallowedHeaders []matcher.StringMatcher
+	if allowed := msg.GetForwardRules().GetAllowedHeaders(); allowed != nil {
+		allowedHeaders, err = httpfilter.ConvertStringMatchers(allowed.GetPatterns())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if disallowed := msg.GetForwardRules().GetDisallowedHeaders(); disallowed != nil {
+		disallowedHeaders, err = httpfilter.ConvertStringMatchers(disallowed.GetPatterns())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	deferredCloseTimeout := defaultDeferredCloseTimeout
+	if msg.GetDeferredCloseTimeout() != nil {
+		deferredCloseTimeout = msg.GetDeferredCloseTimeout().AsDuration()
+	}
+
+	return baseConfig{
+		processingModes:          processingModesFromProto(msg.GetProcessingMode()),
+		requestAttributes:        msg.GetRequestAttributes(),
+		responseAttributes:       msg.GetResponseAttributes(),
+		disableImmediateResponse: msg.GetDisableImmediateResponse(),
+		observabilityMode:        msg.GetObservabilityMode(),
+		failureModeAllow:         msg.GetFailureModeAllow(),
+		server:                   server,
+		mutationRules:            mutationRules,
+		allowedHeaders:           allowedHeaders,
+		disallowedHeaders:        disallowedHeaders,
+		deferredCloseTimeout:     deferredCloseTimeout,
+	}, nil
+}
+
+func (builder) ParseFilterConfigOverride(ov proto.Message) (httpfilter.FilterConfig, error) {
+	m, ok := ov.(*anypb.Any)
+	if !ok {
+		return nil, fmt.Errorf("extproc: error parsing override %v: unknown type %T, want *anypb.Any", ov, ov)
+	}
+	msg := new(v3procfilterpb.ExtProcPerRoute)
+	if err := m.UnmarshalTo(msg); err != nil {
+		return nil, fmt.Errorf("extproc: failed to unmarshal override %v: %v", ov, err)
+	}
+	override := msg.GetOverrides()
+
+	var processingModesOpt optional.Optional[processingModes]
+	if pm := override.GetProcessingMode(); pm != nil {
+		if err := validateBodyProcessingMode(pm); err != nil {
+			return nil, err
+		}
+		processingModesOpt = optional.New(processingModesFromProto(pm))
+	}
+
+	var serverOpt optional.Optional[xdsresource.GRPCServiceConfig]
+	if override.GetGrpcService() != nil {
+		server, err := parseGRPCServiceConfig(override.GetGrpcService())
+		if err != nil {
+			return nil, fmt.Errorf("extproc: failed to parse grpc_service: %v", err)
+		}
+		serverOpt = optional.New(server)
+	}
+
+	var failureModeAllowOpt optional.Optional[bool]
+	if override.GetFailureModeAllow() != nil {
+		failureModeAllowOpt = optional.New(override.GetFailureModeAllow().GetValue())
+	}
+
+	return overrideConfig{
+		server:             serverOpt,
+		processingModes:    processingModesOpt,
+		failureModeAllow:   failureModeAllowOpt,
+		requestAttributes:  override.GetRequestAttributes(),
+		responseAttributes: override.GetResponseAttributes(),
+	}, nil
+}
+
+func (builder) IsTerminal() bool {
+	return false
+}
