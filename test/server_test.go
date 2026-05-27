0b6c4d9c250b3b6a7ee44b218677e1cc490ed81e
goingforstudying-ctrl <goingforstudying@gmail.com>
2026-05-27 15:50:05 -0400

    server: Set a pprof label on new stream goroutines (#9082)
    Fixes #9010
    
    To make stack-traces and some profiles more useful, change sets
    goroutine labels indicating which gRPC method is being handled.
    
    Goroutine labels are inherited by child goroutines, so this provides
    useful context in profiles and traces for work that's been farmed out to
    child goroutines.
    
    These currently show up in three places:
     - trace labels in pprof CPU profiles
    - trace labels in runtime/pprof (and http/pprof) debug=0 goroutine
    profiles (which are pprof format)
     - debug=1 aggregated text-format goroutine profiles
    
    For Go 1.27, golang/go#76349 adds goroutine labels to tracebacks and by
    extension debug=2 pprof text-based profiles for go 1.27+ modules.
    
    The naming of the goroutine label currently matches the opentelemetry
    RPC method tag, and has some similarities to the current proposal for
    goroutine tag naming for tests in golang/go#75047. I.e. this uses
    `grpc.method`.
    
    This change avoids setting anything on the client side due to the lower
    utility and goroutine lifetime issues.
    
    Include a `GRPC_GO_SERVER_GOROUTINE_LABELS` environment variable to
    allow users to easily opt-in of setting these goroutine labels. The
    value the form `grpc.method=true` to enable a specific goroutine label,
    and has special values of `all` and `none` which enable and disable all
    registered goroutine labels respectively.
    
    RELEASE NOTES:
    * server: Set runtime/pprof goroutine labels for incoming method
    streams. This may be enabled with the
    `GRPC_GO_SERVER_GOROUTINE_LABELS=grpc.method=true` environment variable
    for just the new goroutine label or
    `GRPC_GO_SERVER_GOROUTINE_LABELS=all` to enable all goroutine labels
    that might be added later. `none` is available for blanket disabling, as
    well.

--- test/server_test.go
+++ test/server_test.go
@@ -21,12 +21,15 @@ package test
 import (
 	"context"
 	"io"
+	"runtime/pprof"
 	"sync/atomic"
 	"testing"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/internal/envconfig"
 	"google.golang.org/grpc/internal/stubserver"
+	"google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/status"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
@@ -68,6 +71,101 @@ func (s) TestServerReturningContextError(t *testing.T) {
 		t.Fatalf("ss.Client.FullDuplexCall().Recv() got error %v; want <status with Code()=DeadlineExceeded>", err)
 	}
 
+}
+
+func pprofCtxCollectLabels(ctx context.Context) map[string]string {
+	seenLabels := map[string]string{}
+	pprof.ForLabels(ctx, func(k, val string) bool {
+		seenLabels[k] = val
+		return true
+	})
+	return seenLabels
+}
+
+// TestServerSetGoroutineLabelsInContext verifies that when enabled, the
+// grpc.method runtime/pprof goroutine label gets set in the context that's
+// passed to the handlers.
+func (s) TestServerSetGoroutineLabelsInContext(t *testing.T) {
+	testutils.SetEnvConfig(t, &envconfig.LabelServerGoroutines, envconfig.GoroutineLabelServerMethod)
+	ss := &stubserver.StubServer{
+		EmptyCallF: func(ctx context.Context, _ *testpb.Empty) (*testpb.Empty, error) {
+			ctxLabels := pprofCtxCollectLabels(ctx)
+			if val, ok := ctxLabels["grpc.method"]; !ok {
+				t.Errorf("missing \"grpc.method\" label; found labels: %v", ctxLabels)
+			} else if wantVal := "/grpc.testing.TestService/EmptyCall"; val != wantVal {
+				t.Errorf("unexpected value for \"grpc.method\" label %q; want %q", ctxLabels["grpc.method"], wantVal)
+			}
+			return &testpb.Empty{}, nil
+		},
+		FullDuplexCallF: func(stream testgrpc.TestService_FullDuplexCallServer) error {
+			ctxLabels := pprofCtxCollectLabels(stream.Context())
+			if val, ok := ctxLabels["grpc.method"]; !ok {
+				t.Errorf("missing \"grpc.method\" label; found labels: %v", ctxLabels)
+			} else if wantVal := "/grpc.testing.TestService/FullDuplexCall"; val != wantVal {
+				t.Errorf("unexpected value for \"grpc.method\" label %q; want %q", ctxLabels["grpc.method"], wantVal)
+			}
+			return nil
+		},
+	}
+	if err := ss.Start(nil); err != nil {
+		t.Fatalf("Error starting endpoint server: %v", err)
+	}
+	defer ss.Stop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+	if _, err := ss.Client.EmptyCall(ctx, &testpb.Empty{}); err != nil {
+		t.Fatalf("ss.Client.EmptyCall() got error %v; want OK", err)
+	}
+
+	stream, err := ss.Client.FullDuplexCall(ctx)
+	if err != nil {
+		t.Fatalf("unexpected error starting the stream: %v", err)
+	}
+	if _, err = stream.Recv(); err != io.EOF {
+		t.Fatalf("ss.Client.FullDuplexCall().Recv() got error %v; want io.EOF", err)
+	}
+}
+
+// TestServerSetGoroutineLabelsInContextEnvVarDisabled verifies that when
+// disable, the grpc.method runtime/pprof goroutine label does _not_ get set in
+// the context that's passed to the handlers.
+func (s) TestServerSetGoroutineLabelsInContextEnvVarDisabled(t *testing.T) {
+	testutils.SetEnvConfig(t, &envconfig.LabelServerGoroutines, 0)
+	ss := &stubserver.StubServer{
+		EmptyCallF: func(ctx context.Context, _ *testpb.Empty) (*testpb.Empty, error) {
+			ctxLabels := pprofCtxCollectLabels(ctx)
+			if val, ok := ctxLabels["grpc.method"]; ok {
+				t.Errorf("\"grpc.method\" label set with value %q; found labels: %v", val, ctxLabels)
+			}
+			return &testpb.Empty{}, nil
+		},
+		FullDuplexCallF: func(stream testgrpc.TestService_FullDuplexCallServer) error {
+			ctxLabels := pprofCtxCollectLabels(stream.Context())
+			if val, ok := ctxLabels["grpc.method"]; ok {
+				t.Errorf("\"grpc.method\" label set with value %q; found labels: %v", val, ctxLabels)
+			}
+			return nil
+		},
+	}
+	if err := ss.Start(nil); err != nil {
+		t.Fatalf("Error starting endpoint server: %v", err)
+	}
+	defer ss.Stop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+	if _, err := ss.Client.EmptyCall(ctx, &testpb.Empty{}); err != nil {
+		t.Fatalf("ss.Client.EmptyCall() got error %v; want OK", err)
+	}
+
+	stream, err := ss.Client.FullDuplexCall(ctx)
+	if err != nil {
+		t.Fatalf("unexpected error starting the stream: %v", err)
+	}
+	if _, err = stream.Recv(); err != io.EOF {
+		t.Fatalf("ss.Client.FullDuplexCall().Recv() got error %v; want io.EOF", err)
+	}
 }
 
 func (s) TestChainUnaryServerInterceptor(t *testing.T) {
