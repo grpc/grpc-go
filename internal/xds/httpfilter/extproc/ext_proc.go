9fcdb794135189e255d0f8690c44ee256bd12cdb
Madhav Bissa <mbissa@gmail.com>
2026-06-03 18:53:06 +0000

    extproc: Split v3procservicepb import into pb and grpc

--- internal/xds/httpfilter/extproc/ext_proc.go
+++ internal/xds/httpfilter/extproc/ext_proc.go
@@ -35,6 +35,7 @@ import (
 
 	v3corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	v3procfilterpb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/ext_proc/v3"
+	v3procservicegrpc "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
 	v3procservicepb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
 )
 
@@ -45,6 +46,8 @@ func init() {
 }
 
 var (
+	_ = v3procservicepb.ProcessingRequest{}
+
 	parseGRPCServiceConfig = func(*v3corepb.GrpcService) (xdsresource.GRPCServiceConfig, error) {
 		return xdsresource.GRPCServiceConfig{}, fmt.Errorf("parseGRPCServiceConfig not implemented")
 	}
@@ -219,7 +222,7 @@ func (clientFilter) BuildClientInterceptor(base, override httpfilter.FilterConfi
 	}
 	return &clientInterceptor{
 		config:    config,
-		extClient: v3procservicepb.NewExternalProcessorClient(cc),
+		extClient: v3procservicegrpc.NewExternalProcessorClient(cc),
 		cancel:    cancel,
 	}, nil
 }
@@ -227,7 +230,7 @@ func (clientFilter) BuildClientInterceptor(base, override httpfilter.FilterConfi
 type clientInterceptor struct {
 	resolver.ClientInterceptor
 	config    baseConfig
-	extClient v3procservicepb.ExternalProcessorClient
+	extClient v3procservicegrpc.ExternalProcessorClient
 	cancel    func() error
 }
 
