53130467b7c2b90c39b0f95d6cc0f25cd4fda2c3
goingforstudying-ctrl <goingforstudying@gmail.com>
2026-06-20 12:15:25 -0400

    fix: align test method spacing for gofmt compliance
    Signed-off-by: goingforstudying-ctrl <goingforstudying-ctrl@users.noreply.github.com>

--- maxsubconns_test.go
+++ maxsubconns_test.go
@@ -183,8 +183,8 @@ func (b *testMaxSubConnsBalancer) UpdateClientConnState(s balancer.ClientConnSta
 
 func (b *testMaxSubConnsBalancer) ResolverError(_ error)                                          {}
 func (b *testMaxSubConnsBalancer) UpdateSubConnState(_ balancer.SubConn, _ balancer.SubConnState) {}
-func (b *testMaxSubConnsBalancer) Close()                                                           {}
-func (b *testMaxSubConnsBalancer) ExitIdle()                                                         {}
+func (b *testMaxSubConnsBalancer) Close()                                                         {}
+func (b *testMaxSubConnsBalancer) ExitIdle()                                                      {}
 
 // testMaxSubConnsPicker is a test picker.
 type testMaxSubConnsPicker struct{}
