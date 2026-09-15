24ec7eed4bf57b19ccd48f2dc47bcc7d42b15311
Kartavyasonar <sonarkartavya@gmail.com>
2026-09-15 10:17:11 +0530

    roundrobin: standardize and V(2)-gate log messages
    Signed-off-by: Kartavyasonar <sonarkartavya@gmail.com>

--- balancer/roundrobin/roundrobin.go
+++ balancer/roundrobin/roundrobin.go
@@ -53,7 +53,9 @@ func (bb builder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) bala
 		Balancer: endpointsharding.NewBalancer(cc, opts, childBuilder, endpointsharding.Options{}),
 	}
 	bal.logger = internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf("[%p] ", bal))
-	bal.logger.Infof("Created")
+	if bal.logger.V(2) {
+		bal.logger.Infof("Created")
+	}
 	return bal
 }
 
@@ -64,9 +66,26 @@ type rrBalancer struct {
 }
 
 func (b *rrBalancer) UpdateClientConnState(ccs balancer.ClientConnState) error {
+	if b.logger.V(2) {
+		b.logger.Infof("Received new resolver state with %d endpoints", len(ccs.ResolverState.Endpoints))
+	}
 	return b.Balancer.UpdateClientConnState(balancer.ClientConnState{
 		// Enable the health listener in pickfirst children for client side health
 		// checks and outlier detection, if configured.
 		ResolverState: pickfirst.EnableHealthListener(ccs.ResolverState),
 	})
+}
+
+func (b *rrBalancer) ResolverError(err error) {
+	if b.logger.V(2) {
+		b.logger.Infof("Received resolver error: %v", err)
+	}
+	b.Balancer.ResolverError(err)
+}
+
+func (b *rrBalancer) Close() {
+	if b.logger.V(2) {
+		b.logger.Infof("Shutdown")
+	}
+	b.Balancer.Close()
 }
