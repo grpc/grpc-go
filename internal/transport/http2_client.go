e454badf0d52dd6acf92b623492b4e856f6adda6
Easwar Swaminathan <easwars@google.com>
2026-09-23 22:37:51 +0000

    transport: remove read throttling due to controlBuf size, on the client

--- internal/transport/http2_client.go
+++ internal/transport/http2_client.go
@@ -1727,9 +1727,16 @@ func (t *http2Client) reader(errCh chan<- error) {
 		atomic.StoreInt64(&t.lastRead, time.Now().UnixNano())
 	}
 
-	// loop to keep reading incoming messages on this transport.
+	// Loop to keep reading incoming messages on this transport. Never throttle
+	// the reader based on the number of items in the controlBuf. We do not
+	// expect the client to be vulnerable to a malicious server that causes the
+	// controlBuf to grow unbounded. Instead throttling the client can lead to a
+	// deadlock as the server may be throttling as well, and in that case,
+	// neither side will be able to make progress. The client is expected to be
+	// able to read incoming messages from the server at all times, and the
+	// controlBuf is expected to be drained by the loopyWriter in a timely
+	// manner.
 	for {
-		t.controlBuf.throttle()
 		frame, err := t.framer.readFrame()
 		if t.keepaliveEnabled {
 			atomic.StoreInt64(&t.lastRead, time.Now().UnixNano())
