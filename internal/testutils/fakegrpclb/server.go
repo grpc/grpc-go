aa7d753bdba6c048a784858d5b6971fe42049598
eshitachandwani <emchandwani@google.com>
2026-09-25 14:05:55 +0530

    change %q for int to %d

--- internal/testutils/fakegrpclb/server.go
+++ internal/testutils/fakegrpclb/server.go
@@ -103,7 +103,7 @@ func NewServer(params ServerParams) (*Server, error) {
 
 	lis, err := net.Listen("tcp", "localhost:"+strconv.Itoa(params.ListenPort))
 	if err != nil {
-		return nil, fmt.Errorf("failed to listen on port %q: %v", params.ListenPort, err)
+		return nil, fmt.Errorf("failed to listen on port %d: %v", params.ListenPort, err)
 	}
 
 	return &Server{
