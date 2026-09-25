56bace4892ca33e957c38b7dc60850566be7f9fb
eshitachandwani <emchandwani@google.com>
2026-09-25 19:14:58 +0530

    http2 deprecation fix

--- test/end2end_test.go
+++ test/end2end_test.go
@@ -669,16 +669,16 @@ func (te *test) listenAndServe(ts testgrpc.TestServiceServer, listen func(networ
 		if err != nil {
 			te.t.Fatal("tls.LoadX509KeyPair(server1.pem, server1.key) failed: ", err)
 		}
+		p := &http.Protocols{}
+		p.SetHTTP2(true)
 		hs := &http.Server{
 			Handler:   s,
 			TLSConfig: &tls.Config{Certificates: []tls.Certificate{cert}},
-		}
-		if err := http2.ConfigureServer(hs, &http2.Server{MaxConcurrentStreams: te.maxStream}); err != nil {
-			te.t.Fatal("http2.ConfigureServer(_, _) failed: ", err)
+			HTTP2:     &http.HTTP2Config{MaxConcurrentStreams: int(te.maxStream)},
+			Protocols: p,
 		}
 		te.srv = wrapHS{hs}
-		tlsListener := tls.NewListener(lis, hs.TLSConfig)
-		go hs.Serve(tlsListener)
+		go hs.ServeTLS(lis, "", "")
 		return lis
 	}
 
